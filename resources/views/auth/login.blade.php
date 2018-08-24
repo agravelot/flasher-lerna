62d7bf865edb30f6f07ff0f235bd898ba3fc0288
Unknown <antoine.gravelot@hotmail.fr>
2018-08-24 22:14:23 +0200

    twig: login: new ui basd on bulma

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,103 +2,57 @@
 
 @section('content')
 
-    <section class="hero is-primary">
+    <section class="hero is-light is-fullheight">
         <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
-                    Login
-                </h1>
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Login</h3>
+                    <p class="subtitle has-text-grey">Please login to proceed.</p>
+                    <div class="box">
+                        <figure class="avatar">
+                            <img src="https://placehold.it/128x128">
+                        </figure>
+                        <form class="login-form" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" id="email" type="email" name="email"
+                                           placeholder="Your Email" autofocus="" value="{{ old('email') }}">
+                                </div>
+                                @if ($errors->has('email'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('email') }}
+                                    </p>
+                                @endif
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" id="password" type="password" name="password"
+                                           placeholder="Your Password">
+                                </div>
+                                @if ($errors->has('password'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('password') }}
+                                    </p>
+                                @endif
+                            </div>
+                            <div class="field">
+                                <label class="checkbox">
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    Remember me
+                                </label>
+                            </div>
+                            <button class="button is-block is-info is-large is-fullwidth" type="submit">Login</button>
+                        </form>
+                    </div>
+                    <p class="has-text-grey">
+                        <a href="{{ route('register') }}">Sign Up</a> &nbsp;·&nbsp;
+                        <a href="{{ route('password.request') }}">Forgot Password</a>
+                    </p>
+                </div>
             </div>
         </div>
     </section>
 
-    <div class="columns is-marginless is-centered">
-        <div class="column is-5">
-            <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title">Login</p>
-                </header>
-
-                <div class="card-content">
-                    <form class="login-form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="field is-horizontal">
-                            <div class="field-label">
-                                <label class="label">E-Mail Address</label>
-                            </div>
-
-                            <div class="field-body">
-                                <div class="field">
-                                    <p class="control">
-                                        <input class="input" id="email" type="email" name="email"
-                                               value="{{ old('email') }}" required autofocus>
-                                    </p>
-
-                                    @if ($errors->has('email'))
-                                        <p class="help is-danger">
-                                            {{ $errors->first('email') }}
-                                        </p>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="field is-horizontal">
-                            <div class="field-label">
-                                <label class="label">Password</label>
-                            </div>
-
-                            <div class="field-body">
-                                <div class="field">
-                                    <p class="control">
-                                        <input class="input" id="password" type="password" name="password" required>
-                                    </p>
-
-                                    @if ($errors->has('password'))
-                                        <p class="help is-danger">
-                                            {{ $errors->first('password') }}
-                                        </p>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="field is-horizontal">
-                            <div class="field-label"></div>
-
-                            <div class="field-body">
-                                <div class="field">
-                                    <p class="control">
-                                        <label class="checkbox">
-                                            <input type="checkbox"
-                                                   name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                        </label>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="field is-horizontal">
-                            <div class="field-label"></div>
-
-                            <div class="field-body">
-                                <div class="field is-grouped">
-                                    <div class="control">
-                                        <button type="submit" class="button is-primary">Login</button>
-                                    </div>
-
-                                    <div class="control">
-                                        <a href="{{ route('password.request') }}">
-                                            Forgot Your Password?
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
