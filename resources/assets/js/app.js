0db05c2a6f0ef6cf9619ff9ea438a2413facd1f6
Unknown <antoine.gravelot@hotmail.fr>
2018-08-27 20:21:54 +0200

    assets: clean unnecessary

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,19 +7,19 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+// window.Vue = require('vue');
+//
+// /**
+//  * Next, we will create a fresh Vue application instance and attach it to
+//  * the page. Then, you may begin adding components to this application
+//  * or customize the JavaScript scaffolding to fit your unique needs.
+//  */
+//
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//
+// const app = new Vue({
+//     el: '#app'
+// });
 
 
 // Bulma NavBar Burger Script
@@ -51,3 +51,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 require('./bulma-extensions');
+
