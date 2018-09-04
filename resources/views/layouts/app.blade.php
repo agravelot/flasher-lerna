1b8dbc955211e60bc60771e45c87505b8af0a2c1
Unknown <antoine.gravelot@hotmail.fr>
2018-09-04 22:53:57 +0200

    view: layouts: put blade code in partials

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,93 +14,12 @@
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<div id="app">
-    <nav class="navbar has-shadow">
-        <div class="container">
-            <div class="navbar-brand">
-                <a href="{{ url('/') }}" class="navbar-item">{{ config('app.name', 'Laravel') }}</a>
-            </div>
 
+@include('layouts.partials._navbar')
 
-            <div class="navbar-burger burger" data-target="navMenu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+@yield('content')
 
-            <div class="navbar-menu" id="navMenu">
-                <div class="navbar-start">
-                    <a class="navbar-item" href="{{ route('posts.index') }}">Posts</a>
-                    <a class="navbar-item" href="{{ route('albums.index') }}">Albums</a>
-                    <a class="navbar-item" href="{{ route('goldenbook.index') }}">Golden book</a>
-                </div>
-
-                <a class="navbar-item" href="#" target="_blank">
-                      <span class="icon">
-                            <i class="fab fa-facebook-f" aria-hidden="true"></i>
-                      </span>
-                </a>
-                <a class="navbar-item" href="#" target="_blank">
-                      <span class="icon">
-                            <i class="fab fa-instagram" aria-hidden="true"></i>
-                      </span>
-                </a>
-
-
-                <div class="navbar-end">
-                    <a class="navbar-item" href="#">About</a>
-                    <a class="navbar-item" href="#">Contact</a>
-                    @guest()
-                        <div class="navbar-item has-dropdown is-hoverable">
-                            <a class="navbar-link" href="#">
-                                <span class="icon">
-                                  <i class="fas fa-globe-africa"></i>
-                                </span>
-                                <span>FR</span>
-                            </a>
-
-                            <div class="navbar-dropdown is-boxed is-right">
-                                <a class="navbar-item" href="#">
-                                    FR
-                                </a>
-                                <a class="navbar-item" href="#">
-                                    EN
-                                </a>
-                            </div>
-                        </div>
-
-                    @else
-                        <div class="navbar-item has-dropdown is-hoverable">
-                            <a class="navbar-link" href="#">
-                                <span class="icon">
-                                  <i class="fas fa-user"></i>
-                                </span>
-                                <span>{{ Auth::user()->name }}</span>
-                            </a>
-
-                            <div class="navbar-dropdown is-boxed is-right">
-                                <a class="navbar-item" href="#">
-                                    Settings
-                                </a>
-                                <hr class="navbar-divider">
-                                <a class="navbar-item has-text-danger" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </div>
-                        </div>
-                    @endguest
-                </div>
-            </div>
-        </div>
-    </nav>
-    @yield('content')
-</div>
+@include('layouts.partials._footer')
 
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>
+            <strong>Picblog</strong> by <a href="https://jgthms.com">Antoine GRAVELOT</a>
+        </p>
+    </div>
+</footer>
