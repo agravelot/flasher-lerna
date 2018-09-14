2393a2d65250e6e159c646366646ad332b7d321b
Nevax <antoine.gravelot@hotmail.fr>
2018-09-14 18:06:38 +0200

    UsersTableSeeeder: clean code and rely more from parent

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
-use Carbon\Carbon;
+use App\Models\Album;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,31 +14,27 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $admin = factory(App\Models\User::class, 1)
+        $admin = factory(User::class, 1)
             ->create([
                 'name' => 'admin',
                 'email' => 'admin@gmail.com',
                 'password' => bcrypt('secret'),
                 'role' => 'admin',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-                'email_verified_at' => Carbon::now(),
-                'remember_token' => str_random(10),
             ])
             ->each(function ($u) {
-                $u->albums()->save(factory(App\Models\Album::class)->make());
+                $u->albums()->save(factory(Album::class)->make());
             })
             ->each(function ($u) {
-                $u->posts()->save(factory(\App\Models\Post::class)->make());
+                $u->posts()->save(factory(Post::class)->make());
             });
 
-        $users = factory(App\Models\User::class, 10)
+        $users = factory(User::class, 10)
             ->create()
             ->each(function ($u) {
-                $u->albums()->save(factory(App\Models\Album::class)->make());
+                $u->albums()->save(factory(Album::class)->make());
             })
             ->each(function ($u) {
-                $u->posts()->save(factory(\App\Models\Post::class)->make());
+                $u->posts()->save(factory(Post::class)->make());
             });
     }
 }
