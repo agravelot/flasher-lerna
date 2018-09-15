9d7e38085bf6f0fdc522fe8e50a9cd8a7be93cc0
Nevax <antoine.gravelot@hotmail.fr>
2018-09-15 14:30:12 +0200

    add new cards

--- resources/views/albums/_feilds.blade.php
+++ resources/views/albums/_feilds.blade.php
@@ -5,102 +5,107 @@
         {{ method_field('PATCH') }}
     @endif
 
-    <div class="field is-horizontal">
-        <div class="field-label">
-            <label class="label">Title</label>
-        </div>
 
-        <div class="field-body">
-            <div class="field">
-                <p class="control">
-                    <input class="input" id="title" type="text" name="title"
-                           value="{{ old('title', isset($album->title) ? $album->title : null) }}"
-                           required autofocus>
-                </p>
+    <div class="columns">
+        <div class="column is-two-thirds">
+            <div class="card">
+                <div class="card-content">
+                    <div class="field is-horizontal">
+                        <div class="field-label">
+                            <label class="label">Title</label>
+                        </div>
 
-                @if ($errors->has('title'))
-                    <p class="help is-danger">
-                        {{ $errors->first('title') }}
-                    </p>
-                @endif
-            </div>
-        </div>
-    </div>
+                        <div class="field-body">
+                            <div class="field">
+                                <p class="control">
+                                    <input class="input" id="title" type="text" name="title"
+                                           value="{{ old('title', isset($album->title) ? $album->title : null) }}"
+                                           required autofocus>
+                                </p>
 
-    <div class="field is-horizontal">
-        <div class="field-label">
-            <label class="label">Active</label>
-        </div>
+                                @if ($errors->has('title'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('title') }}
+                                    </p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
 
-        <div class="field-body">
-            <div class="field">
-                <p class="control">
-                    <input type="hidden" name="active" value="0">
-                    <input type="checkbox" name="active"
-                           @if(old('active', isset($album->active) ? $album->active : false)) checked @endif>
-                </p>
+                    <div class="field is-horizontal">
+                        <div class="field-label">
+                            <label class="label">Active</label>
+                        </div>
 
-                @if ($errors->has('active'))
-                    <p class="help is-danger">
-                        {{ $errors->first('active') }}
-                    </p>
-                @endif
-            </div>
-        </div>
-    </div>
+                        <div class="field-body">
+                            <div class="field">
+                                <p class="control">
+                                    <input type="hidden" name="active" value="0">
+                                    <input type="checkbox" name="active"
+                                           @if(old('active', isset($album->active) ? $album->active : false)) checked @endif>
+                                </p>
 
-    <div class="field is-horizontal">
-        <div class="field-label">
-            <label class="label">Password</label>
-        </div>
+                                @if ($errors->has('active'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('active') }}
+                                    </p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
 
-        <div class="field-body">
-            <div class="field">
-                <p class="control">
-                    <input class="input" id="password" type="password" name="password">
-                </p>
+                    <div class="field is-horizontal">
+                        <div class="field-label">
+                            <label class="label">Password</label>
+                        </div>
 
-                @if ($errors->has('password'))
-                    <p class="help is-danger">
-                        {{ $errors->first('password') }}
-                    </p>
-                @endif
-            </div>
-        </div>
-    </div>
+                        <div class="field-body">
+                            <div class="field">
+                                <p class="control">
+                                    <input class="input" id="password" type="password" name="password">
+                                </p>
 
-    <div class="field is-horizontal">
-        <div class="field-label">
-            <label class="label">Body</label>
-        </div>
+                                @if ($errors->has('password'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('password') }}
+                                    </p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
 
-        <div class="field-body">
-            <div class="field">
-                <p class="control">
+                    <div class="field is-horizontal">
+                        <div class="field-label">
+                            <label class="label">Body</label>
+                        </div>
+
+                        <div class="field-body">
+                            <div class="field">
+                                <p class="control">
                     <textarea class="textarea" id="body" type="text" name="body"
                               rows="10">{{ old('body', isset($album->body) ? $album->body : null) }}</textarea>
-                </p>
+                                </p>
 
-                @if ($errors->has('body'))
-                    <p class="help is-danger">
-                        {{ $errors->first('body') }}
-                    </p>
-                @endif
-            </div>
-        </div>
-    </div>
+                                @if ($errors->has('body'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('body') }}
+                                    </p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="field is-horizontal">
-        <div class="field-label">
-            <label class="label">Pictures</label>
-        </div>
+                    <div class="field is-horizontal">
+                        <div class="field-label">
+                            <label class="label">Pictures</label>
+                        </div>
 
-        <div class="field-body">
-            <div class="field">
-                <div class="file">
-                    <label class="file-label">
-                        <input class="file-input" type="file" name="pictures[]" multiple>
-                        <span class="file-cta">
+                        <div class="field-body">
+                            <div class="field">
+                                <div class="file">
+                                    <label class="file-label">
+                                        <input class="file-input" type="file" name="pictures[]" multiple>
+                                        <span class="file-cta">
                             <span class="file-icon">
                                 <i class="fas fa-upload"></i>
                             </span>
@@ -108,43 +113,61 @@
                                 Choose your files…
                           </span>
                         </span>
-                    </label>
-                </div>
+                                    </label>
+                                </div>
 
-                @if ($errors->has('pictures'))
-                    <p class="help is-danger">
-                        {{ $errors->first('pictures') }}
-                    </p>
-                @endif
-            </div>
-        </div>
-    </div>
+                                @if ($errors->has('pictures'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('pictures') }}
+                                    </p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="field is-horizontal">
-        <div class="field-label">
-            <label class="label">Cosplayers</label>
-        </div>
-        <div class="field-body">
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="tags" placeholder="Add Cosplayers" value="Tag1,Tag2,Tag3">
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="field is-horizontal">
-        <div class="field-label">
-            <!-- Left empty for spacing -->
-        </div>
-        <div class="field-body">
-            <div class="field">
-                <div class="control">
-                    <button class="button is-primary">
-                        Send
-                    </button>
+                    <div class="field is-horizontal">
+                        <div class="field-label">
+                            <label class="label">Cosplayers</label>
+                        </div>
+                        <div class="field-body">
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input" type="tags" placeholder="Add Cosplayers"
+                                           value="Tag1,Tag2,Tag3">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
-    </div>
+        <div class="column is-one-third">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-header-title">
+                        Publish
+                    </div>
+                </div>
+                <div class="card-content">
+                    <div class="control">
+                        <label class="radio">
+                            <input type="radio" name="foobar" checked>
+                            Publish
+                        </label>
+                        <label class="radio">
+                            <input type="radio" name="foobar">
+                            Draft
+                        </label>
+                    </div>
+                </div>
+                <footer class="card-footer">
+                    <div class="control card-footer-item">
+                        <button class="button is-primary">
+                            Send
+                        </button>
+                    </div>
+                </footer>
+            </div>
+        </div>
 </form>
