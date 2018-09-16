6a7cf875f4fb539e0f459f0bea8fbf87e9949530
Nevax <antoine.gravelot@hotmail.fr>
2018-09-16 15:19:54 +0200

    add ide helper for dev env

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Comment
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $user_id
+ * @property int $commentable_id
+ * @property string $commentable_type
+ * @property string $body
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $commentable
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Comment whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Comment whereCommentableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Comment whereCommentableType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Comment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Comment whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Comment whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Comment extends Model
 {
     /**
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Post
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $title
+ * @property string|null $seo_title
+ * @property string $body
+ * @property int $active
+ * @property int $user_id
+ * @property string|null $image
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Category[] $categories
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Comment[] $comments
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereSeoTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Post extends Model
 {
     /**
