485bf1e632244430012fd4607c53e3b2b9ee8af2
Nevax <antoine.gravelot@hotmail.fr>
2018-09-17 22:29:27 +0200

    ContactFactory: seed name instead of username

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Contact::class, function (Faker $faker) {
     return [
-        'name' => $faker->userName,
+        'name' => $faker->name,
         'email' => $faker->email,
         'message' => $faker->paragraph,
         'created_at' => Carbon::now(),
