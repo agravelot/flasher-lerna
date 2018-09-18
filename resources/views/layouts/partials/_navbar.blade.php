e5201dead0613e424bc2c757e85acc670a14d192
Nevax <antoine.gravelot@hotmail.fr>
2018-09-18 19:52:50 +0200

    navbar is fixed top

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar has-shadow">
+<nav class="navbar is-fixed-top has-shadow is-transparent">
     <div class="container">
         <div class="navbar-brand">
             <a href="{{ url('/') }}" class="navbar-item">{{ config('app.name', 'PicBlog') }}</a>
