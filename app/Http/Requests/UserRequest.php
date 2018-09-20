5db20ae33f1936fb0e0ecf4059b16277b1f7c0b7
Nevax <antoine.gravelot@hotmail.fr>
2018-09-20 13:29:04 +0200

    UserRequest: name and email must be unique

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -11,9 +11,11 @@ class UserRequest extends Request
      */
     public function rules()
     {
+        $id = $this->route('user');
+
         return [
-            'name' => 'required|string|max:255|unique:users',
-            'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required|string|max:255|unique:users,name,' . $id,
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
             'password' => 'required|string|min:6|confirmed',
         ];
     }
