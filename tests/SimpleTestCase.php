ef26b609d21e08f2b19167d3ae220ac0a1bc97a7
Nevax <antoine.gravelot@hotmail.fr>
2018-09-26 19:57:16 +0200

    tests: add some testing helpers

---
+++ tests/SimpleTestCase.php
@@ -0,0 +1,7 @@
+<?php
+namespace Tests;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+abstract class SimpleTestCase extends BaseTestCase
+{
+    //
+}
---
+++ tests/WithClearQueryLog.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+trait WithClearQueryLog
+{
+    /**
+     * @var \Illuminate\Database\Connection
+     */
+    protected $db;
+
+    protected function setUpQueryLog()
+    {
+        $this->db = $this->app->make('db');
+        $this->db->flushQueryLog();
+        $this->db->enableQueryLog();
+    }
+
+    protected function tearDownQueryLog()
+    {
+        $this->db->flushQueryLog();
+        $this->db->disableQueryLog();
+    }
+}
