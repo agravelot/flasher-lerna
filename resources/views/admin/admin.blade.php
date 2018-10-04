143960b870fa4adfa22c7fa21d4793210060627e
Nevax <antoine.gravelot@hotmail.fr>
2018-10-04 15:51:28 +0200

    admin: menu: fixed left menu

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="columns">
-        @include('admin.partials._menu')
+    <div class="columns is-fullheight">
+        <div class="column is-2 is-sidebar-menu">
+            @include('admin.partials._menu')
+        </div>
         <div class="column is-10">
             @include('layouts.partials._messages')
             @yield('admin-content')
