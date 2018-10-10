89ee395b69dc8b473a320a73ef53687f666feb15
Nevax <antoine.gravelot@hotmail.fr>
2018-10-10 19:20:50 +0200

    update foreign keys migration

--- database/migrations/2018_09_13_201524_create_foreign_keys.php
+++ database/migrations/2018_09_13_201524_create_foreign_keys.php
@@ -15,46 +15,36 @@ class CreateForeignKeys extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users')
-                ->onDelete('restrict')
-                ->onUpdate('restrict');
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
-//        Schema::table('post_tag', function (Blueprint $table) {
-//            $table->foreign('post_id')->references('id')->on('posts')
-//                ->onDelete('cascade')
-//                ->onUpdate('cascade');
-//        });
-//        Schema::table('post_tag', function (Blueprint $table) {
-//            $table->foreign('tag_id')->references('id')->on('tags')
-//                ->onDelete('cascade')
-//                ->onUpdate('cascade');
-//        });
+
         Schema::table('comments', function (Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users')
-                ->onDelete('restrict')
-                ->onUpdate('restrict');
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
-//        Schema::table('comments', function (Blueprint $table) {
-//            $table->foreign('post_id')->references('id')->on('posts')
-//                ->onDelete('cascade')
-//                ->onUpdate('cascade');
-//        });
-//        Schema::table('category_post', function (Blueprint $table) {
-//            $table->foreign('category_id')->references('id')->on('categories')
-//                ->onDelete('cascade')
-//                ->onUpdate('cascade');
-//        });
-//        Schema::table('category_post', function (Blueprint $table) {
-//            $table->foreign('post_id')->references('id')->on('posts')
-//                ->onDelete('cascade')
-//                ->onUpdate('cascade');
-//        });
+
         Schema::table('album_cosplayer', function (Blueprint $table) {
             $table->foreign('album_id')->references('id')->on('albums')
                 ->onDelete('set null')
                 ->onUpdate('set null');
         });
+
         Schema::table('album_cosplayer', function (Blueprint $table) {
             $table->foreign('cosplayer_id')->references('id')->on('cosplayers')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+        });
+
+        Schema::table('categorizables', function (Blueprint $table) {
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+        });
+
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')
                 ->onDelete('set null')
                 ->onUpdate('set null');
         });
@@ -70,32 +60,25 @@ class CreateForeignKeys extends Migration
         Schema::table('posts', function (Blueprint $table) {
             $table->dropForeign('posts_user_id_foreign');
         });
-//        Schema::table('post_tag', function (Blueprint $table) {
-//            $table->dropForeign('post_tag_post_id_foreign');
-//        });
-//        Schema::table('post_tag', function (Blueprint $table) {
-//            $table->dropForeign('post_tag_tag_id_foreign');
-//        });
+
         Schema::table('comments', function (Blueprint $table) {
             $table->dropForeign('comments_user_id_foreign');
         });
-//        Schema::table('comments', function (Blueprint $table) {
-//            $table->dropForeign('comments_post_id_foreign');
-//        });
-//        Schema::table('comments', function (Blueprint $table) {
-//            $table->dropForeign('comments_parent_id_foreign');
-//        });
-//        Schema::table('category_post', function (Blueprint $table) {
-//            $table->dropForeign('category_post_category_id_foreign');
-//        });
-//        Schema::table('category_post', function (Blueprint $table) {
-//            $table->dropForeign('category_post_post_id_foreign');
-//        });
+
         Schema::table('album_cosplayer', function (Blueprint $table) {
             $table->dropForeign('album_cosplayer_album_id_foreign');
         });
-//        Schema::table('album_cosplayer', function (Blueprint $table) {
-//            $table->dropForeign('album_id_cosplayer_id_foreign');
-//        });
+
+        Schema::table('album_cosplayer', function (Blueprint $table) {
+            $table->dropForeign('album_cosplayer_cosplayer_id_foreign');
+        });
+
+        Schema::table('categorizables', function (Blueprint $table) {
+            $table->dropForeign('categorizables_category_id_foreign');
+        });
+
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->dropForeign('contacts_user_id_foreign');
+        });
     }
 }
