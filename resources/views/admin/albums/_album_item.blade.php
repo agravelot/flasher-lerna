9e6e6dc61299d4ccf368fd3b2530641533ce7661
Nevax <antoine.gravelot@hotmail.fr>
2018-10-11 17:25:54 +0200

    glued buttons in admin panel

--- resources/views/admin/albums/_album_item.blade.php
+++ resources/views/admin/albums/_album_item.blade.php
@@ -1,18 +1,18 @@
 <tr>
-    <td width="5%">
+    <td width="2%">
         <span class="icon is-small">
             <span class="has-margin-sm">{{ $album->pictures->count() }}</span>
             <i class="far fa-images"></i>
         </span>
     </td>
-    <td width="5%">
+    <td width="2%">
         @if ($album->publish)
             <span class="icon is-small has-text-success">
                 <i class="fas fa-check"></i>
             </span>
         @endif
     </td>
-    <td width="5%">
+    <td width="2%">
         @if ($album->password)
             <span class="icon is-small has-text-warning">
                 <i class="fas fa-lock"></i>
@@ -22,14 +22,14 @@
     <td>
         <a href="{{ route('admin.albums.show', ['album' => $album]) }}">{{ $album->title }}</a>
     </td>
-    <td>
+    <td width="2%">
         <a href="{{ route('admin.albums.edit', ['album' => $album]) }}">
             <span class="icon has-text-info">
                 <i class="far fa-edit"></i>
             </span>
         </a>
     </td>
-    <td>
+    <td width="2%">
         <form action="{{ route('admin.albums.destroy', ['album' => $album]) }}" method="POST">
             {{ method_field('DELETE') }}
             @csrf
@@ -38,7 +38,6 @@
                     <i class="far fa-trash-alt"></i>
                 </span>
             </button>
-
         </form>
     </td>
 </tr>
--- resources/views/admin/cosplayers/_cosplayer_item.blade.php
+++ resources/views/admin/cosplayers/_cosplayer_item.blade.php
@@ -1,20 +1,20 @@
 <tr>
     <td width="5%">
         <span class="icon is-small">
-            <i class="fas fa-user"></i>
+            <i class="fas fa-user-tag"></i>
         </span>
     </td>
     <td>
         <a href="{{ route('admin.cosplayers.show', ['cosplayer' => $cosplayer]) }}">{{ $cosplayer->name }}</a>
     </td>
-    <td>
+    <td width="2%">
         <a href="{{ route('admin.cosplayers.edit', ['cosplayer' => $cosplayer]) }}">
             <span class="icon has-text-info">
                 <i class="far fa-edit"></i>
             </span>
         </a>
     </td>
-    <td>
+    <td width="2%">
         <form action="{{ route('admin.cosplayers.destroy', ['cosplayer' => $cosplayer]) }}" method="POST">
             {{ method_field('DELETE') }}
             @csrf
