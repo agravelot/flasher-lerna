4bb219ae0898d317a9c5cd7bc8f0c7e7ebe4523b
Nevax <antoine.gravelot@hotmail.fr>
2018-10-11 22:07:05 +0200

    Front: PostController: remove unused functions

--- app/Http/Controllers/Front/PostController.php
+++ app/Http/Controllers/Front/PostController.php
@@ -15,33 +15,11 @@ class PostController extends Controller
      */
     public function index()
     {
-
         $posts = Post::all();
 
         return view('posts.index', ['posts' => $posts]);
-
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('posts.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -51,40 +29,6 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return view('posts.show');
+        return view('posts.show', ['post' => $post]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        return view('posts.edit');
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \App\Models\Post $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
-}
