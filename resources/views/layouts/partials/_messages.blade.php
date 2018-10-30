6e313b03952467974b574e4785b4d42e2a39d151
Nevax <antoine.gravelot@hotmail.fr>
2018-10-30 18:27:04 +0100

    messages for all pages

--- resources/views/layouts/partials/_messages.blade.php
+++ resources/views/layouts/partials/_messages.blade.php
@@ -1,4 +1,12 @@
 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+
+    @foreach ($errors->all() as $error)
+        <p class="notification is-danger">
+            <button class="delete"></button>
+            {{ $error }}
+        </p>
+    @endforeach
+
     @if(Session::has($msg))
         <p class="notification is-{{ $msg }}">
             <button class="delete"></button>
