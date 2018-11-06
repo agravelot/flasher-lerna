9b5f89e51b4d0a8df633a84817444d2db2c3a8ae
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-11-06 17:29:33 +0100

    goldenbook can be created and stored

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::namespace('Front')->group(function () {
     Route::resource('posts', 'PostController', ['only' => ['index', 'show']]);
     Route::resource('albums', 'AlbumController', ['only' => ['index', 'show']]);
     Route::get('/albums/{slug}/download', 'AlbumController@download')->name('album_download');
-    Route::resource('goldenbook', 'GoldenBookController', ['only' => ['index', 'show']]);
+    Route::resource('goldenbook', 'GoldenBookController', ['only' => ['index', 'create', 'store']]);
     Route::resource('cosplayers', 'CosplayerController', ['only' => ['index', 'show']]);
     Route::resource('categories', 'CategoryController', ['only' => ['index', 'show']]);
     Route::resource('contact', 'ContactController', ['only' => ['create', 'store']]);
