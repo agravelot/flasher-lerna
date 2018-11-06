671cbb6f4ceaca525cc2fd9c4852a083e70614f0
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-11-06 17:34:37 +0100

    show albums of categories

--- resources/views/categories/partials/_show_item.blade.php
+++ resources/views/categories/partials/_show_item.blade.php
@@ -5,36 +5,18 @@
     <div class="column is-8 is-offset-2">
         <div class="card article">
             <div class="card-content">
-                <div class="media">
-                    <div class="media-content has-text-centered">
-                        <p class="title article-title">  {{-- {{ $album->title }}--}}</p>
-                        <div class="tags has-addons level-item">
-                            {{--<span class="tag is-rounded is-info">{{'@' . $album->user->name}}</span>--}}
-                            {{--<span class="tag is-rounded">{{ $album->created_at->toFormattedDateString() }}</span>--}}
-                        </div>
-                    </div>
-                </div>
                 <div class="content article-body">
                     <p class="has-text-justified">{!! $category->description  !!}</p>
-                </div>
-                {{--<div class="tags">--}}
-                    {{--@foreach($category->categories as $category)--}}
-                        {{--<span class="tag">{{ $category->name }}</span>--}}
-                    {{--@endforeach--}}
-                {{--</div>--}}
-                <div class="columns is-multiline is-mobile">
-                    {{--@for ($i = 1; $i <= 10; $i++)--}}
-                    {{--<figure class="image column is-2-desktop is-3-tablet is-3-mobile">--}}
-                    {{--<img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">--}}
-                    {{--<p class="has-text-centered"><a href="#">Cosplayer {{ $i }}</a></p>--}}
-                    {{--</figure>--}}
-                    {{--@endfor--}}
-
                 </div>
             </div>
         </div>
     </div>
 
-    @each('albums.partials._index_item', $category->albums, 'album', 'layouts.partials._empty')
+    <div class="container is-centered">
+        <div class="columns is-multiline is-centered">
+            @each('albums.partials._index_item', $category->albums, 'album', 'layouts.partials._empty')
+        </div>
+        {{--{{ $category->albums->links() }}--}}
+    </div>
 
 </div>
