8451700733e19aa3f1c3d38fed916a1d70367921
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-11-06 13:15:17 +0100

    refactor user request

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -9,17 +9,12 @@ class UserRequest extends Request
     {
         $id = $this->route('user');
 
-        $RULE_UPDATE = [
+        return[
             'name' => 'required|string|min:2|max:255|unique:users,name,' . $id,
             'email' => 'required|string|email|min:2|max:255|unique:users,email,' . $id,
             'password' => 'nullable|string|min:6|confirmed',
             'cosplayer' => 'nullable|sometimes|integer|exists:cosplayers,id',
+            'g-recaptcha-response' => auth()->check() && auth()->user()->isAdmin() ? '' : 'required|captcha'
         ];
-
-        if (! auth()->check() || ! auth()->user()->isAdmin()) {
-            $captcha = ['g-recaptcha-response' => 'required|captcha'];
-            return array_merge($RULE_UPDATE, $captcha);
-        }
-        return $RULE_UPDATE;
     }
 }
