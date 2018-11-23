bc877bed6f8ca6ea6c967b6e5ec9734d7bc93954
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-11-23 15:05:44 +0100

    fix contact request wrong feild 'body' > 'message'

--- app/Http/Requests/ContactStoreRequest.php
+++ app/Http/Requests/ContactStoreRequest.php
@@ -15,7 +15,7 @@ class ContactStoreRequest extends Request
         return [
             'name' => 'string|required|min:3|max:255',
             'email' => 'email|min:5|max:142',
-            'body' => 'required|min:42|max:65000',
+            'message' => 'required|min:42|max:65000',
             'g-recaptcha-response' => 'required|captcha'
         ];
     }
