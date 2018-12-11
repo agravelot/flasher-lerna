e37d91f07a8a70a354188dff64ffd4406a108a7e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-12-11 21:52:38 +0100

    docker: get env when generate env files

--- docker/generate_env.sh
+++ docker/generate_env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source init_variables.sh $1
+
 envsubst < docker/db/.env.example > docker/db/.env
 envsubst < docker/nginx/.env.example > docker/nginx/.env
 envsubst < .env.production > docker/php-fpm/.env
