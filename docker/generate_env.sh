e127daa9e40d4faae87fb1cd4bb06a37dd9a9734
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-12-11 22:10:44 +0100

    docker: script: fix source path

--- docker/generate_env.sh
+++ docker/generate_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source init_variables.sh $1
+source docker/init_variables.sh $1
 
 envsubst < docker/db/.env.example > docker/db/.env
 envsubst < docker/nginx/.env.example > docker/nginx/.env
