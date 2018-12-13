aac51924ea641a057d09edfd55358304aa96331e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-12-13 21:28:03 +0100

    seeder: album: create all type of public/private

--- database/seeds/AlbumsTableSeeder.php
+++ database/seeds/AlbumsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class AlbumsTableSeeder extends Seeder
@@ -12,10 +13,13 @@ class AlbumsTableSeeder extends Seeder
      */
     public function run()
     {
-        User::all()->each(function ($user)  {
+        User::all()->each(function (User $user) {
             $user->albums()->saveMany(
-                factory(App\Models\Album::class, 2)
-                    ->make()
+                collect()
+                    ->concat(factory(App\Models\Album::class, 2)->state('password')->state('published')->make())
+                    ->concat(factory(App\Models\Album::class, 2)->state('password')->state('unpublished')->make())
+                    ->concat(factory(App\Models\Album::class, 2)->state('passwordLess')->state('unpublished')->make())
+                    ->concat(factory(App\Models\Album::class, 2)->state('passwordLess')->state('published')->make())
             );
         });
     }
