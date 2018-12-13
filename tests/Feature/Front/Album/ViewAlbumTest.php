e1a9532cd7113dc8a9b954efbadf3f6e5b39cc27
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-12-13 20:16:34 +0100

    test: feature: add some tests for album show

---
+++ tests/Feature/Front/Album/ViewAlbumTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Album;
+use App\Models\Category;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ViewAlbumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_user_can_view_album()
+    {
+        $user = factory(User::class)->create();
+
+        $category = factory(Category::class)->create([
+            'name' => 'Category name'
+        ]);
+
+        /** @var Album $album */
+        $album = factory(Album::class)->create([
+            'title' => 'Test title',
+            'body' => 'Some test body, this is a good day!',
+            'published_at' => Carbon::now(),
+            'password' => null,
+            'user_id' => $user->id,
+        ]);
+        $album->categories()->attach($category);
+
+        $response = $this->get('/albums/' . $album->slug);
+
+        $response->assertStatus(200);
+        $response->assertSee('Test title');
+        $response->assertSee('Some test body, this is a good day!');
+        $response->assertSee('Category name');
+    }
+
+    public function test_user_cannot_view_private_album_listing()
+    {
+        $user = factory(User::class)->create();
+
+        /** @var Album $album */
+        $album = factory(Album::class)->create([
+            'title' => 'Test title private',
+            'body' => 'Some test body, this is a good day!',
+            'published_at' => null,
+            'password' => null,
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get('/albums/' . $album->slug);
+
+        $response->assertStatus(404);
+    }
+}
