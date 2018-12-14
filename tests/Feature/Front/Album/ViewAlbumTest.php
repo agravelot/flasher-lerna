497f14060300358aa57cf65629ca5c04d11c1203
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-12-14 12:02:49 +0100

    album test: should get 403 error when not authorized and rename tests

--- tests/Feature/Front/Album/ViewAlbumTest.php
+++ tests/Feature/Front/Album/ViewAlbumTest.php
@@ -12,7 +12,24 @@ class ViewAlbumTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function test_user_can_view_a_published_album()
+    public function test_guest_can_view_a_published_album()
+    {
+        $user = factory(User::class)->create();
+
+        $album = factory(Album::class)->states(['published', 'passwordLess'])->create([
+            'title' => 'Test title',
+            'body' => 'Some test body, this is a good day!',
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get('/albums/' . $album->slug);
+
+        $response->assertStatus(200);
+        $response->assertSee('Test title');
+        $response->assertSee('Some test body, this is a good day!');
+    }
+
+    public function test_guest_can_view_a_published_album_with_category()
     {
         $user = factory(User::class)->create();
 
@@ -20,7 +37,6 @@ class ViewAlbumTest extends TestCase
             'name' => 'Category name',
         ]);
 
-        /** @var Album $album */
         $album = factory(Album::class)->states(['published', 'passwordLess'])->create([
             'title' => 'Test title',
             'body' => 'Some test body, this is a good day!',
@@ -36,17 +52,58 @@ class ViewAlbumTest extends TestCase
         $response->assertSee('Category name');
     }
 
-    public function test_user_cannot_view_private_album_listing()
+    public function test_guest_can_view_a_published_album_with_two_categories()
     {
         $user = factory(User::class)->create();
 
-        /** @var Album $album */
+        $categoryA = factory(Category::class)->create([
+            'name' => 'Category name',
+        ]);
+
+        $categoryB = factory(Category::class)->create([
+            'name' => 'Category name two',
+        ]);
+
+        $album = factory(Album::class)->states(['published', 'passwordLess'])->create([
+            'title' => 'Test title',
+            'body' => 'Some test body, this is a good day!',
+            'user_id' => $user->id,
+        ]);
+
+        $album->categories()->attach([$categoryA->id, $categoryB->id]);
+
+        $response = $this->get('/albums/' . $album->slug);
+
+        $response->assertStatus(200);
+        $response->assertSee('Test title');
+        $response->assertSee('Some test body, this is a good day!');
+        $response->assertSee('Category name');
+        $response->assertSee('Category name two');
+    }
+
+    public function test_guest_cannot_view_unpublished_album_listing()
+    {
+        $user = factory(User::class)->create();
+
         $album = factory(Album::class)->states(['unpublished', 'passwordLess'])->create([
             'user_id' => $user->id,
         ]);
 
         $response = $this->get('/albums/' . $album->slug);
 
-        $response->assertStatus(404);
+        $response->assertStatus(403);
+    }
+
+    public function test_guest_cannot_view_album_with_password_listing()
+    {
+        $user = factory(User::class)->create();
+
+        $album = factory(Album::class)->states(['published', 'password'])->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get('/albums/' . $album->slug);
+
+        $response->assertStatus(403);
     }
 }
