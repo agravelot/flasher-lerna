f843227a54df246bd0db261b7ac2dabdfd15de59
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-12-14 12:43:38 +0100

    integration: test add album index

---
+++ tests/Feature/Front/Album/IndexAlbumTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Front\Album;
+
+use App\Models\Album;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class IndexAlbumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_guest_can_view_published_albums()
+    {
+        $albums = factory(Album::class, 2)->states(['published', 'passwordLess', 'withUser'])->create();
+
+        $response = $this->get('/albums');
+
+        $response->assertStatus(200);
+        $response->assertSee($albums[0]->title);
+        $response->assertDontSee($albums[0]->body);
+        $response->assertSee($albums[1]->title);
+        $response->assertDontSee($albums[1]->body);
+    }
+
+    public function test_guest_can_not_view_unpublished_albums()
+    {
+        $albums = factory(Album::class, 2)->states(['unpublished', 'passwordLess', 'withUser'])->create();
+
+        $response = $this->get('/albums');
+
+        $response->assertStatus(200);
+        $response->assertDontSee($albums[0]->title);
+        $response->assertDontSee($albums[0]->body);
+        $response->assertDontSee($albums[1]->title);
+        $response->assertDontSee($albums[1]->body);
+    }
+
+
+    public function test_guest_can_not_view_albums_with_password()
+    {
+        $albums = factory(Album::class, 2)->states(['published', 'password', 'withUser'])->create();
+
+        $response = $this->get('/albums');
+
+        $response->assertStatus(200);
+        $response->assertDontSee($albums[0]->title);
+        $response->assertDontSee($albums[0]->body);
+        $response->assertDontSee($albums[1]->title);
+        $response->assertDontSee($albums[1]->body);
+    }
+
+    public function test_guest_can_not_view_unpublished_albums_with_password()
+    {
+        $albums = factory(Album::class, 2)->states(['unpublished', 'password', 'withUser'])->create();
+
+        $response = $this->get('/albums');
+
+        $response->assertStatus(200);
+        $response->assertDontSee($albums[0]->title);
+        $response->assertDontSee($albums[0]->body);
+        $response->assertDontSee($albums[1]->title);
+        $response->assertDontSee($albums[1]->body);
+    }
+}
