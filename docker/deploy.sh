10f5d6631933ee4e404e82848fac79eb0d30e27b
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-12-14 13:12:28 +0100

    docker deploy: no need to run artisan up since it's a new container

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -30,7 +30,7 @@ echo " * PUTTING LARAVEL IN MAINTENANCE MODE"
 docker-compose exec php echo Hello world && docker-compose exec php php artisan down --message="We'll be back soon" --retry=60 || echo "Container is not running"
 echo " * UPDATING RUNNING CONTAINERS"
 docker-compose -f docker-compose.yml up -d --remove-orphans
-echo " * LEAVING MAINTENANCE MODE"
-docker-compose exec php php artisan up
+#echo " * LEAVING MAINTENANCE MODE"
+#docker-compose exec php php artisan up
 #echo " * CLEANING OLD IMAGES"
 #ssh -t ${REMOTE} -p $CI_DEPLOY_SSH_PORT "docker-clean images"
