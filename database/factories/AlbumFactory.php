ca65357947fe1d09b869b4bcb33a67566d2306a3
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2018-12-30 18:24:58 +0100

    album factory: do not define timestamps since it's automatically filled

--- database/factories/AlbumFactory.php
+++ database/factories/AlbumFactory.php
@@ -14,14 +14,12 @@ $factory->define(App\Models\Album::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence,
         'body' => $faker->paragraph($faker->numberBetween(42, 420)),
-        'created_at' => $faker->dateTimeThisYear(),
-        'updated_at' => Carbon::now(),
     ];
 });
 
-$factory->state(\App\Models\Album::class, 'published', function (Faker $faker) {
+$factory->state(\App\Models\Album::class, 'published', function () {
     return [
-        'published_at' => $faker->dateTimeThisYear(),
+        'published_at' => true,
     ];
 });
 
