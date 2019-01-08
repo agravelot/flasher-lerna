903fe272149ba56a3cf8482c98009a0963e31231
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-01-08 23:14:33 +0100

    show messages on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="container">
+        @include('layouts.partials._messages')
         <div class="columns is-marginless is-centered">
             <div class="column is-7">
                 <nav class="card">
