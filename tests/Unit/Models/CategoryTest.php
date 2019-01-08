75e1d4cc4dcae471bb632e1bfd6481af1ddc94d8
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-01-08 20:08:51 +0100

    add category unit test

--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -10,27 +10,55 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Category;
+use Illuminate\Database\QueryException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\ModelTestCase;
 
 class CategoryTest extends ModelTestCase
 {
-    use WithFaker;
+    use WithFaker, RefreshDatabase;
+
+    public function test_can_not_create_two_categories_with_the_same_name()
+    {
+        $categories = factory(Category::class, 2)->make(['name' => 'A name']);
+
+        try {
+            $categories->each(function (Category $category) {
+                $category->save();
+            });
+        } catch (QueryException $e) {
+            $this->assertSame(1, Category::count());
+
+            return;
+        }
+
+        $this->fail('We cannot create two categories with the same name');
+    }
+
+    public function test_can_create_a_category_with_an_empty_description()
+    {
+        $category = factory(Category::class)->make(['description' => null]);
+
+        $category->save();
+
+        $this->assertSame(1, Category::count());
+    }
 
     public function testRouteKeyName()
     {
-        $post = new Category();
+        $category = new Category();
         $slug = $this->faker->slug;
-        $routeKey = $post->getRouteKeyName();
-        $post->$$routeKey = $slug;
+        $routeKey = $category->getRouteKeyName();
+        $category->$$routeKey = $slug;
 
         $this->assertSame($$routeKey, $slug);
     }
 
     public function testSlugAsRouteKeyName()
     {
-        $post = new Category();
-        $routeKey = $post->getRouteKeyName();
+        $category = new Category();
+        $routeKey = $category->getRouteKeyName();
 
         $excepted = 'slug';
 
@@ -39,8 +67,8 @@ class CategoryTest extends ModelTestCase
 
     public function testSlugSourceAsTitle()
     {
-        $post = new Category();
-        $slugSource = $post->sluggable()['slug']['source'];
+        $category = new Category();
+        $slugSource = $category->sluggable()['slug']['source'];
 
         $excepted = 'name';
 
