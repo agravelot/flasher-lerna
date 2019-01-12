b309fb09b1a730a20ebebd02ce55e67c4203e14e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-01-12 17:58:07 +0100

    add album observer to event provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,6 +9,8 @@
 
 namespace App\Providers;
 
+use App\Models\Album;
+use App\Observers\AlbumObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -36,5 +38,6 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
+        Album::observe(AlbumObserver::class);
     }
 }
