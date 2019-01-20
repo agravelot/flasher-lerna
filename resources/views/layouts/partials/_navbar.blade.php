ab89b60fa2f0004d0c216bd17d524a260662916b
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-01-20 18:30:34 +0100

    navbar is no more fixed top

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar is-fixed-top has-shadow" role="navigation" aria-label="dropdown navigation">
+<nav class="navbar has-shadow" role="navigation" aria-label="dropdown navigation">
     <div class="container">
         <div class="navbar-brand">
             <a href="{{ url('/') }}" class="navbar-item">{{ config('app.name', 'PicBlog') }}</a>
