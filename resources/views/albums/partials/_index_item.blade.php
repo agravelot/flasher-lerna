92c779fbbfa063e6f09678d4a35d9cc578ee1e3a
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-01-20 16:32:32 +0100

    add hover effect for albums

--- resources/views/albums/partials/_index_item.blade.php
+++ resources/views/albums/partials/_index_item.blade.php
@@ -1,6 +1,6 @@
 <div class="column is-half-tablet is-one-third-widescreen">
     <a href="{{ route('albums.show', ['album' => $album]) }}">
-        <div class="card">
+        <div class="card album">
             <div class="card-image">
                 <figure class="image">
                     <img src="{{ $album->getFirstMediaUrl('pictures', 'thumb')  }}" alt="">
