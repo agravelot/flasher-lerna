fa721c2bcf967807673c15a46b9a41f8342c6c88
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-01-22 20:24:15 +0100

    add basic french translation

---
+++ resources/lang/fr/pagination.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * (c) Antoine GRAVELOT <antoine.gravelot@hotmail.fr> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Antoine Gravelot <agravelot@orma.fr>
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; Précédent',
+    'next' => 'Suivant &raquo;',
+];
