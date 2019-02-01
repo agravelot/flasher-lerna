5625b648ab91e1fd202668224a67c8594a79a74e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-01 13:55:03 +0100

    fix aria for social medias

--- resources/views/layouts/partials/_navbar_socials.blade.php
+++ resources/views/layouts/partials/_navbar_socials.blade.php
@@ -1,17 +1,17 @@
 @if (env('FACEBOOK_URL'))
     <a class="navbar-item {{ $desktop ? 'is-hidden-touch' : 'is-hidden-desktop' }}" href="{{ env('FACEBOOK_URL') }}"
-       target="_blank" rel="noreferrer">
+       target="_blank" rel="noreferrer" aria-label="facebook">
         <span class="icon" style="color: #3b5998;">
-            <i class="fab fa-facebook-f" aria-label="facebook"></i>
+            <i class="fab fa-facebook-f"></i>
         </span>
     </a>
 @endif
 
 @if (env('INSTAGRAM_URL'))
     <a class="navbar-item {{ $desktop ? 'is-hidden-touch' : 'is-hidden-desktop' }}" href="{{ env('INSTAGRAM_URL') }}"
-       target="_blank" rel="noreferrer">
+       target="_blank" rel="noreferrer" aria-label="instagram">
         <span class="icon" style="color: #f71a5d;">
-            <i class="fab fa-instagram" aria-label="instagram"></i>
+            <i class="fab fa-instagram"></i>
         </span>
     </a>
 @endif
