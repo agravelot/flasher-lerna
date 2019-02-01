05a912d7bf89531fa84044b407aa249525315a85
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-01 13:13:10 +0100

    use modal fx js instead of custom one

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 
 require('./fontawsome');
+require('bulma-modal-fx/src/_js/modal-fx');
 
 // Bulma NavBar Burger Script
 document.addEventListener('DOMContentLoaded', function() {
@@ -31,50 +32,4 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
-
-    // Modals
-    let rootEl = document.documentElement;
-    let $modals = getAll('.modal');
-    let $modalButtons = getAll('.modal-button');
-    let $modalCloses = getAll(
-        '.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button'
-    );
-
-    if ($modalButtons.length > 0) {
-        $modalButtons.forEach(function($el) {
-            $el.addEventListener('click', function() {
-                let target = $el.dataset.target;
-                openModal(target);
-            });
-        });
-    }
-
-    if ($modalCloses.length > 0) {
-        $modalCloses.forEach(function($el) {
-            $el.addEventListener('click', function() {
-                closeModals();
-            });
-        });
-    }
-
-    function openModal(target) {
-        let $target = document.getElementById(target);
-        rootEl.classList.add('is-clipped');
-        $target.classList.add('is-active');
-    }
-
-    function closeModals() {
-        rootEl.classList.remove('is-clipped');
-        $modals.forEach(function($el) {
-            $el.classList.remove('is-active');
-        });
-    }
-
-    document.addEventListener('keydown', function(event) {
-        let e = event || window.event;
-        if (e.keyCode === 27) {
-            closeModals();
-            //closeDropdowns();
-        }
-    });
 });
