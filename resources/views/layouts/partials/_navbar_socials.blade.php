4ca025af294e7c6364305b1338187e7324d0b895
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-01 13:14:20 +0100

    update social icons aria-label

--- resources/views/layouts/partials/_navbar_socials.blade.php
+++ resources/views/layouts/partials/_navbar_socials.blade.php
@@ -2,7 +2,7 @@
     <a class="navbar-item {{ $desktop ? 'is-hidden-touch' : 'is-hidden-desktop' }}" href="{{ env('FACEBOOK_URL') }}"
        target="_blank" rel="noreferrer">
         <span class="icon" style="color: #3b5998;">
-            <i class="fab fa-facebook-f" aria-hidden="true"></i>
+            <i class="fab fa-facebook-f" aria-label="facebook"></i>
         </span>
     </a>
 @endif
@@ -11,7 +11,7 @@
     <a class="navbar-item {{ $desktop ? 'is-hidden-touch' : 'is-hidden-desktop' }}" href="{{ env('INSTAGRAM_URL') }}"
        target="_blank" rel="noreferrer">
         <span class="icon" style="color: #f71a5d;">
-            <i class="fab fa-instagram" aria-hidden="true"></i>
+            <i class="fab fa-instagram" aria-label="instagram"></i>
         </span>
     </a>
 @endif
