ee0f63b9a4c4b220ac7c1fb8b1c26d803dd4f51a
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-07 13:32:55 +0100

    add social media docblock

--- app/Models/SocialMedia.php
+++ app/Models/SocialMedia.php
@@ -11,6 +11,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\SocialMedia.
+ *
+ * @property int                             $id
+ * @property string                          $name
+ * @property string                          $url
+ * @property string                          $icon
+ * @property string                          $color
+ * @property int                             $active
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia active()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia whereColor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia whereIcon($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SocialMedia whereUrl($value)
+ * @mixin \Eloquent
+ */
 class SocialMedia extends Model
 {
     protected $fillable = ['name', 'icon', 'url', 'color', 'active'];
