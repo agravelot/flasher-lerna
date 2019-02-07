75111f83cf57174d22fa99d9d3d9196aa680e4b7
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-07 23:53:43 +0100

    support buttons on multiple lines

--- resources/views/admin/partials/_edit_delete_buttons.blade.php
+++ resources/views/admin/partials/_edit_delete_buttons.blade.php
@@ -1,8 +1,4 @@
-<div class="field is-grouped">
-    <div class="control">
-        @include('admin.partials._edit_button', compact('route'))
-    </div>
-    <div class="control">
-        @include('admin.partials._delete_button', compact('key'))
-    </div>
+<div class="buttons">
+    @include('admin.partials._edit_button', compact('route'))
+    @include('admin.partials._delete_button', compact('key'))
 </div>
