cfe6feffec87600bb46a92fae80319ce632df305
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-08 13:50:44 +0100

    clean

--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
         App\Providers\HorizonServiceProvider::class,
         // App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \App\Providers\ComposerServiceProvider::class,
+        App\Providers\ComposerServiceProvider::class,
 
         /*
         * Package Service Providers...
