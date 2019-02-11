0eb8d80d1f75d06fc1c95346c8e675c2bbe0b376
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-11 22:12:13 +0100

    clean up js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,35 +1,19 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+// Bulma NavBar Burger Script
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const navbarBurgers = document.getElementsByClassName('navbar-burger');
+
+    Array.from(navbarBurgers).forEach((el) => {
+        el.addEventListener('click', function() {
+            // Get the target from the "data-target" attribute
+            let target = document.getElementById(el.dataset.target);
+
+            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            target.classList.toggle('is-active');
+        });
+    });
+});
 
 require('./fontawsome');
 require('bulma-modal-fx/src/_js/modal-fx');
-
-// Bulma NavBar Burger Script
-document.addEventListener('DOMContentLoaded', function() {
-    //Functions
-    function getAll(selector) {
-        return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
-    }
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = getAll('.navbar-burger');
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
-        $navbarBurgers.forEach(function($el) {
-            $el.addEventListener('click', function() {
-                // Get the target from the "data-target" attribute
-                let target = $el.dataset.target;
-                let $target = document.getElementById(target);
-
-                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-                $el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-            });
-        });
-    }
-});
