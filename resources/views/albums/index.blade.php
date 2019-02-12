25f42a261318d32bb351f5faf0eed034d12a55d5
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-12 18:51:57 +0100

    add albums subtitle

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="container">
         <h1 class="title">{{ __('My albums') }}</h1>
-
+        <h2 class="subtitle">{{ __('Discover my albums') }}</h2>
+        
         <div class="masonry">
             @each('albums.partials._index_item', $albums, 'album', 'layouts.partials._empty')
         </div>
