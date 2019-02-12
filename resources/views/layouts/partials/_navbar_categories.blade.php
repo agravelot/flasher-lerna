742666d1753076c0f146e411d15c7fb21a4fd34f
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-12 22:02:10 +0100

    do not show categories if it doesn't exist

--- resources/views/layouts/partials/_navbar_categories.blade.php
+++ resources/views/layouts/partials/_navbar_categories.blade.php
@@ -1,12 +1,14 @@
-<div class="navbar-item has-dropdown is-hoverable">
-    <a class="navbar-link" href="{{ route('categories.index') }}">
-        {{ __('Categories') }}
-    </a>
-    <div class="navbar-dropdown is-boxed">
-        @foreach($categories as $category)
-            <a class="navbar-item" href="{{ route('categories.show', compact('category')) }}">
-                {{ $category->name }}
-            </a>
-        @endforeach
+@if(!$categories->isEmpty())
+    <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link" href="{{ route('categories.index') }}">
+            {{ __('Categories') }}
+        </a>
+        <div class="navbar-dropdown is-boxed">
+            @foreach($categories as $category)
+                <a class="navbar-item" href="{{ route('categories.show', compact('category')) }}">
+                    {{ $category->name }}
+                </a>
+            @endforeach
+        </div>
     </div>
-</div>
+@endif
