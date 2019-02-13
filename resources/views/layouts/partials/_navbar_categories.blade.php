e91c8305f2bb0296412c42a3b9d3d6ab9c9fa45c
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-13 15:28:44 +0100

    add show all categories in navbar

--- resources/views/layouts/partials/_navbar_categories.blade.php
+++ resources/views/layouts/partials/_navbar_categories.blade.php
@@ -4,6 +4,10 @@
             {{ __('Categories') }}
         </a>
         <div class="navbar-dropdown is-boxed">
+            <a class="navbar-item" href="{{ route('categories.index') }}">
+                {{ __('Show all categories') }}
+            </a>
+            <hr class="navbar-divider">
             @foreach($categories as $category)
                 <a class="navbar-item" href="{{ route('categories.show', compact('category')) }}">
                     {{ $category->name }}
