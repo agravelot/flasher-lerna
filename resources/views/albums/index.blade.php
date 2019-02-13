0e81c3b0ade36fde7eba24ebd0b64a82534949ad
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-13 15:52:22 +0100

    fix empty albums index

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -5,12 +5,12 @@
         <h1 class="title">{{ __('My albums') }}</h1>
         <h2 class="subtitle">{{ __('Discover my albums') }}</h2>
 
-        @if($albums)
+        @if($albums->isEmpty())
+            @include('layouts.partials._empty')
+        @else
             <div class="masonry">
                 @each('albums.partials._index_item', $albums, 'album')
             </div>
-        @else
-            @include('layouts.partials._empty')
         @endif
 
         {{ $albums->links() }}
