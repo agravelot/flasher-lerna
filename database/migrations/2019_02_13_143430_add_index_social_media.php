9e0f53adcda7b22e8588d75a123abe9614fec785
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-14 17:08:05 +0100

    reformat

--- database/migrations/2019_02_13_143430_add_index_social_media.php
+++ database/migrations/2019_02_13_143430_add_index_social_media.php
@@ -1,33 +1,34 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+/*
+ * (c) Antoine GRAVELOT <antoine.gravelot@hotmail.fr> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Antoine Gravelot <agravelot@hotmail.fr>
+ */
+
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddIndexSocialMedia extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
-        Schema::table('social_media', function(Blueprint $table)
-        {
+        Schema::table('social_media', function (Blueprint $table) {
             $table->index('active');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
-        Schema::table('social_media', function(Blueprint $table)
-        {
+        Schema::table('social_media', function (Blueprint $table) {
             $table->dropIndex('social_media_active_index');
         });
     }
