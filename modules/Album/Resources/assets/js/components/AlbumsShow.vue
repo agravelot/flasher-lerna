37ccb39c06e522d80e5bcd1c9cbcb155bcec8d89
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-22 11:03:15 +0100

    update album show

--- modules/Album/Resources/assets/js/components/AlbumsShow.vue
+++ modules/Album/Resources/assets/js/components/AlbumsShow.vue
@@ -13,23 +13,14 @@ export default {
         };
     },
     created() {
-        this.fetchAlbum('et-et-distinctio-occaecati-aliquam-veritatis-itaque');
+        this.fetchAlbum();
     },
     // mounted() {
     //     this.fetchAlbum();
     // },
-    filters: {
-        /**
-         * Filter to truncate string, accepts a length parameter
-         */
-        truncate(value, length) {
-            return value.length > length ? value.substr(0, length) + '...' : value;
-        },
-    },
     methods: {
-        fetchAlbum(slug) {
-
-            Vue.axios.get(`/api/admin/albums/${slug}`, {
+        fetchAlbum() {
+            Vue.axios.get(`/api/admin/albums/${this.$route.params.slug}`, {
                 })
                 .then(res => res.data)
                 .then(res => {
@@ -40,25 +31,10 @@ export default {
                     throw err;
                 });
         },
-
-        confirmDeleteSelectedAlbums() {
-            this.$dialog.confirm({
-                title: 'Deleting Albums',
-                message:
-                    'Are you sure you want to <b>delete</b> these albums? This action cannot be undone.',
-                confirmText: 'Delete Albums',
-                type: 'is-danger',
-                hasIcon: true,
-                onConfirm: () => {
-                    this.checkedRows.forEach(el => this.deleteAlbum(el));
-                    this.$toast.open('Albums deleted!');
-                },
-            });
-        },
         /**
          * Delete album from slug
          */
-        deleteAlbum(album) {
+        deleteSelectedAlbum(album) {
             Vue.axios
                 .delete(`/api/admin/albums/${album.slug}`)
                 .then(res => {
