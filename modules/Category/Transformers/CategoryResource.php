7e9f6eb32bac41d0178309a8553f9da14652aae0
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-23 22:19:08 +0100

    add cosplayer module

---
+++ modules/Category/Transformers/CategoryResource.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * (c) Antoine GRAVELOT <antoine.gravelot@hotmail.fr> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Antoine Gravelot <agravelot@hotmail.fr>
+ */
+
+namespace Modules\Category\Transformers;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class CategoryResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
