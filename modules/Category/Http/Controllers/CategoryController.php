0f160ff289e894d6e84dabeb1c181f462830f0e8
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-02-23 22:18:32 +0100

    use category resource

--- modules/Category/Http/Controllers/CategoryController.php
+++ modules/Category/Http/Controllers/CategoryController.php
@@ -13,17 +13,18 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Modules\Category\Transformers\CategoryResource;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return Category[]|\Illuminate\Database\Eloquent\Collection
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        return Category::all();
+        return CategoryResource::collection(Category::all());
     }
 
     /**
