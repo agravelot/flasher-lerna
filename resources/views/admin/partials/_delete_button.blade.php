5cd8cb40b3bc2e182fc06a329baa79a5a9f5a367
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-03-05 21:49:28 +0100

    cleanup

--- resources/views/admin/partials/_delete_button.blade.php
+++ resources/views/admin/partials/_delete_button.blade.php
@@ -1,6 +1,6 @@
 <a class="button modal-button" data-target="modal-delete-{{ $key }}">
-                    <span class="icon has-text-danger">
-                        <i class="fas fa-trash-alt"></i>
-                    </span>
+    <span class="icon has-text-danger">
+        <i class="fas fa-trash-alt"></i>
+    </span>
     <span>{{ __('Delete') }}</span>
 </a>
