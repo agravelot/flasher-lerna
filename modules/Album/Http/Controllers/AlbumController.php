b08397294b2b9e2388a36d0cbdcfdbfffd344638
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-03-07 20:42:43 +0100

    clean up controller

--- modules/Album/Http/Controllers/AlbumController.php
+++ modules/Album/Http/Controllers/AlbumController.php
@@ -23,115 +23,28 @@ class AlbumController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     *
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        $this->authorize('index', Album::class);
-
         return AlbumIndexResource::collection(
             QueryBuilder::for(PublicAlbum::class)->with('media')->latest()->paginate(10)
         );
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param AlbumRequest $request
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     *
-     * @return AlbumIndexResource
-     */
-    public function store(AlbumRequest $request)
-    {
-        $this->authorize('create', Album::class);
-        $validated = $request->validated();
-        /** @var Album $album */
-        $album = Album::create($validated);
-
-        if (Arr::exists($validated, 'categories')) {
-            $album->categories()->sync($validated['categories'], false);
-        }
-
-        if (Arr::exists($validated, 'cosplayers')) {
-            $album->cosplayers()->sync($validated['cosplayers'], false);
-        }
-
-        return new AlbumIndexResource($album);
-    }
-
-    /**
      * Display the specified resource.
      *
-     *
      * @param string $slug
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
      *
      * @return AlbumShowResource
      */
     public function show(string $slug)
     {
-        $this->authorize('view', Album::class);
-        $album = Album::with(['cosplayers.media'])
+        $album = PublicAlbum::with(['cosplayers.media'])
             ->whereSlug($slug)
             ->firstOrFail();
-        $this->authorize('view', $album);
 
         return new AlbumShowResource($album);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param AlbumRequest $request
-     * @param string       $slug
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     *
-     * @return AlbumIndexResource
-     */
-    public function update(AlbumRequest $request, string $slug)
-    {
-        $this->authorize('update', Album::class);
-        $album = Album::whereSlug($slug)
-            ->firstOrFail();
-        $this->authorize('update', $album);
-        $validated = $request->validated();
-        $album->update($validated);
-
-        if (Arr::exists($validated, 'categories')) {
-            $album->categories()->sync($request->input('categories'), false);
-        }
-
-        if (Arr::exists($validated, 'cosplayers')) {
-            $album->cosplayers()->sync($request->input('cosplayers'), false);
-        }
-
-        return new AlbumIndexResource($album);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param string $slug
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     * @throws \Exception
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy(string $slug)
-    {
-        $this->authorize('delete', Album::class);
-        $album = Album::whereSlug($slug)
-            ->firstOrFail();
-        $this->authorize('delete', $album);
-        $album->delete();
-
-        return response()->json(null, 204);
     }
 }
