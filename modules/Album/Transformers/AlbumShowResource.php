2128e352b3f0c2c040ff09437fcd116280c5bf3e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-03-14 11:21:06 +0100

    refactor policy check

--- modules/Album/Transformers/AlbumShowResource.php
+++ modules/Album/Transformers/AlbumShowResource.php
@@ -16,6 +16,18 @@ use Illuminate\Support\Facades\Auth;
 class AlbumShowResource extends Resource
 {
     /**
+     * Check if the user has the ability to according to the policy
+     *
+     * @param string $permission
+     * @param Album  $album
+     * @return bool
+     */
+    private function checkCan(string $permission, Album $album)
+    {
+        return Auth::guard('api')->check() && Auth::guard('api')->user()->can($permission, $album);
+    }
+
+    /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request
@@ -35,11 +47,11 @@ class AlbumShowResource extends Resource
             'categories' => $this->categories,
             'cosplayers' => $this->cosplayer,
             'download' => $this->when(
-                Auth::guard('api')->check() && Auth::guard('api')->user()->can('download', Album::findOrFail($this->id)),
+                $this->checkCan('download', Album::findOrFail($this->id)),
                 route('download-albums.show', ['slug' => $this->slug])
             ),
             'edit' => $this->when(
-                Auth::guard('api')->check() && Auth::guard('api')->user()->can('update', Album::findOrFail($this->id)),
+                $this->checkCan('update', Album::findOrFail($this->id)),
                 route('admin.albums.edit', ['slug' => $this->slug])
             ),
         ];
