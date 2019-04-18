c119e86a818e695131646df5be5a64483532af4a
Antoine GRAVELOT <agravelot@orma.fr>
2019-04-18 11:54:44 +0200

    add more album tests

---
+++ modules/Album/Tests/Feature/Http/Controller/Api/Album/IndexAlbumTest.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ * (c) Antoine GRAVELOT <antoine.gravelot@hotmail.fr> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Antoine Gravelot <agravelot@hotmail.fr>
+ */
+
+namespace Modules\Album\Tests\Feature\Http\Controller\Api\Album;
+
+use App\Models\Album;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexAlbumTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_admin_can_view_published_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->states(['published' , 'passwordLess'])->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200);
+        $this->assertSame($album->title, $response->decodeResponseJson('data')[0]['title']);
+    }
+
+    public function test_admin_can_not_view_unpublished_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('unpublished')->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200)
+            ->assertJson(['data' => []]);
+    }
+
+    public function test_admin_can_not_view_secured_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('password')->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200)
+            ->assertJson(['data' => []]);
+    }
+
+    public function test_user_can_view_index()
+    {
+        $this->actingAsUser();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_can_view_published_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->states(['published', 'passwordLess'])->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200);
+        $this->assertSame($album->title, $response->decodeResponseJson('data')[0]['title']);
+    }
+
+    public function test_user_can_not_view_unpublished_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('unpublished')->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200)
+            ->assertJson(['data' => []]);
+    }
+
+    public function test_user_can_not_view_secured_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('password')->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200)
+            ->assertJson(['data' => []]);
+    }
+
+    public function test_guest_can_not_view_index()
+    {
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_guest_can_view_published_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->states(['published', 'passwordLess'])->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200);
+        $this->assertSame($album->title, $response->decodeResponseJson('data')[0]['title']);
+    }
+
+    public function test_guest_can_not_view_unpublished_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('unpublished')->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200)
+            ->assertJson(['data' => []]);
+    }
+
+    public function test_guest_can_not_view_secured_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('password')->create();
+
+        $response = $this->json('get', '/api/albums');
+
+        $response->assertStatus(200)
+            ->assertJson(['data' => []]);
+    }
+}
---
+++ modules/Album/Tests/Feature/Http/Controller/Api/Album/ShowAlbumTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * (c) Antoine GRAVELOT <antoine.gravelot@hotmail.fr> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Antoine Gravelot <agravelot@hotmail.fr>
+ */
+
+namespace Modules\Album\Tests\Feature\Http\Controller\Api\Album;
+
+use App\Models\Album;
+use App\Models\PublicAlbum;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestResponse;
+use Tests\TestCase;
+
+class ShowAlbumTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_admin_can_view_public_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(PublicAlbum::class)->state('withUser')->create();
+
+        $response = $this->showAlbum($album);
+
+        $response->assertStatus(200)
+            ->assertSee($album->title);
+    }
+
+    private function showAlbum(Album $album): TestResponse
+    {
+        return $this->json('get', "/api/albums/{$album->slug}");
+    }
+
+    public function test_admin_can_not_view_unpublished_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('unpublished')->create();
+
+        $response = $this->showAlbum($album);
+
+        $response->assertStatus(404);
+    }
+
+    public function test_admin_can_not_view_secured_albums()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('password')->create();
+
+        $response = $this->showAlbum($album);
+
+        $response->assertStatus(404);
+    }
+
+    public function test_admin_get_404_on_bad_slug()
+    {
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('password')->create();
+
+        $album->slug = 'random-slug-0000';
+        $response = $this->showAlbum($album);
+
+        $response->assertStatus(404);
+    }
+
+    public function test_user_can_view_public_album()
+    {
+        $this->actingAsUser();
+        $album = factory(PublicAlbum::class)->state('withUser')->create();
+
+        $response = $this->showAlbum($album);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_guest_can_view_public_album()
+    {
+        $album = factory(PublicAlbum::class)->state('withUser')->create();
+
+        $response = $this->showAlbum($album);
+
+        $response->assertStatus(200);
+    }
+}
