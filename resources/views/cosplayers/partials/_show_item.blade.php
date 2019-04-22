d2dc102a82144559e9b6df8c1279740f04d8d5c3
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-04-22 10:32:22 +0200

    mask cosplayer description if empty

--- resources/views/cosplayers/partials/_show_item.blade.php
+++ resources/views/cosplayers/partials/_show_item.blade.php
@@ -3,33 +3,27 @@
     @include('cosplayers.partials._cosplayer_avatar', compact('cosplayer'))
     <h1 class="has-text-centered title is-2">{{ $cosplayer->name }}</h1>
 
-
-    <div class="column is-8 is-offset-2">
-        <div class="card article">
-            <div class="card-content">
-                <div class="media">
-                    <div class="media-content has-text-centered">
-                        <p class="title article-title">  {{-- {{ $album->title }}--}}</p>
-                        <div class="tags has-addons level-item">
-                            {{--<span class="tag is-rounded is-info">{{'@' . $album->user->name}}</span>--}}
-                            {{--<span class="tag is-rounded">{{ $album->created_at->toFormattedDateString() }}</span>--}}
-                        </div>
+    @if ($cosplayer->description && $cosplayer->categories)
+        <div class="column is-8 is-offset-2">
+            <div class="card article">
+                <div class="card-content">
+                    <div class="content article-body">
+                        <p class="has-text-justified">{!! $cosplayer->description  !!}</p>
                     </div>
-                </div>
-                <div class="content article-body">
-                    <p class="has-text-justified">{!! $cosplayer->description  !!}</p>
-                </div>
-                <div class="tags">
-                    @each('categories.partials._category_tag', $cosplayer->categories, 'category')
+                    <div class="tags">
+                        @each('categories.partials._category_tag', $cosplayer->categories, 'category')
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
+
 
     <div class="container is-centered">
         <div class="masonry">
-        @each('albums.partials._index_item', $cosplayer->publicAlbums, 'album', 'layouts.partials._empty')
+            @each('albums.partials._index_item', $cosplayer->publicAlbums, 'album', 'layouts.partials._empty')
         </div>
-        {{--{{ $cosplayer->albums->links() }}--}}
+{{--        TODO add pagination--}}
+{{--        {{ $cosplayer->albums->links() }}--}}
     </div>
 </div>
