7d9d76da895a6ed140816477d53394eba53393d6
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-04-27 01:45:45 +0200

    add cosplayers title

--- resources/views/cosplayers/index.blade.php
+++ resources/views/cosplayers/index.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 
+@section('pageTitle', __('Cosplayers'))
+
 @section('content')
     @include('layouts.partials._messages')
     <div class="container is-centered">
+        <h1 class="title">{{ __('Cosplayers') }}</h1>
         <div class="columns is-multiline is-centered">
             @each('cosplayers.partials._cosplayer_item', $cosplayers, 'cosplayer', 'layouts.partials._empty')
         </div>
