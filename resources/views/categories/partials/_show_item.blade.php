07c183d40d8f1f64b7f202cba94377f80ae41969
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-04-27 16:57:29 +0200

    remove useless offset and cards

--- resources/views/categories/partials/_show_item.blade.php
+++ resources/views/categories/partials/_show_item.blade.php
@@ -3,15 +3,7 @@
     <h1 class="title is-2 has-text-centered">{{ $category->name }}</h1>
 
     @if ($category->description)
-        <div class="column is-8 is-offset-2">
-            <div class="card article">
-                <div class="card-content">
-                    <div class="content article-body">
-                        <p class="has-text-justified">{!! $category->description  !!}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <p class="has-text-justified">{!! $category->description  !!}</p>
     @endif
 
     @include('albums.partials._index_item', compact('albums'))
--- resources/views/cosplayers/partials/_show_item.blade.php
+++ resources/views/cosplayers/partials/_show_item.blade.php
@@ -3,18 +3,12 @@
     @include('cosplayers.partials._cosplayer_avatar', compact('cosplayer'))
     <h1 class="has-text-centered title is-2">{{ $cosplayer->name }}</h1>
 
-    @if ($cosplayer->description && $cosplayer->categories)
-        <div class="column is-8 is-offset-2">
-            <div class="card article">
-                <div class="card-content">
-                    <div class="content article-body">
-                        <p class="has-text-justified">{!! $cosplayer->description  !!}</p>
-                    </div>
-                    <div class="tags">
-                        @each('categories.partials._category_tag', $cosplayer->categories, 'category')
-                    </div>
-                </div>
-            </div>
+    @if ($cosplayer->description)
+        <p class="has-text-justified">{!! $cosplayer->description  !!}</p>
+    @endif
+    @if ($cosplayer->categories)
+        <div class="tags">
+            @each('categories.partials._category_tag', $cosplayer->categories, 'category')
         </div>
     @endif
 
