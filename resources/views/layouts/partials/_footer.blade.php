5b4394f831ab0fbb6ced8e8e4eedfa347340e671
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-04-27 12:58:43 +0200

    update footer

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -3,6 +3,6 @@
         <i class="fas fa-code" style="color: dodgerblue;"></i>
         {{ __('with') }}
         <i class="fas fa-heart" style="color:red;"></i>
-        {{ __('by') }} <a href="https://gitlab.com/Nevax">Antoine GRAVELOT</a>
+        {{ __('by') }} <a href="https://gitlab.com/Nevax">Antoine Gravelot</a>
     </p>
 </footer>
