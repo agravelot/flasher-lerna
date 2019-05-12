6c541bd57f617f65a109bb5917843f138d5711f4
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-05-12 16:50:27 +0200

    rework design

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -3,10 +3,13 @@
 @section('pageTitle', __('My albums'))
 
 @section('content')
-    <div class="container">
-        <h1 class="title">{{ __(settings()->get('homepage_title')) }}</h1>
-        <h2 class="subtitle">{{ __('Discover my albums') }}</h2>
+    <section class="section">
+        <div class="container">
+{{--            <h1 class="title">{{ __(settings()->get('homepage_title')) }}</h1>--}}
+{{--            <h2 class="subtitle">{{ __('Discover my albums') }}</h2>--}}
 
-        @include('albums.partials._index_item', compact('albums'))
-    </div>
+            @include('albums.partials._index_item', compact('albums'))
+        </div>
+    </section>
+
 @endsection
--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -1,58 +1,160 @@
-<nav class="navbar" role="navigation" aria-label="dropdown navigation">
-    <div class="container">
-        <div class="navbar-brand">
-            <a href="{{ url('/') }}" class="navbar-item">{{ settings()->get('app_name', config('app.name', 'Flasher')) }}</a>
+<section class="hero is-medium"
+         style="background: url(https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/48995967_679931189069097_5062173863815675904_o.jpg?_nc_cat=105&_nc_ht=scontent-cdg2-1.xx&oh=41dd0a533d0ff6c589c02626ef7ae977&oe=5D5C4F67);
+         background-size: contain;">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+        <nav class="navbar" role="navigation" aria-label="dropdown navigation">
+            <div class="container">
+                <div class="navbar-brand">
 
-            @include('layouts.partials._navbar_socials')
+                    <a href="{{ url('/') }}" class="navbar-item">{{ settings()->get('app_name', config('app.name', 'Flasher')) }}</a>
 
-            <a role="button" class="navbar-burger burger" aria-label="menu" data-target="navMenu" aria-expanded="false">
-                <span></span>
-                <span></span>
-                <span></span>
-            </a>
-        </div>
+                    @include('layouts.partials._navbar_socials')
 
-        <div class="navbar-menu" id="navMenu">
-            <div class="navbar-end">
-                <a class="navbar-item {{ Request::is('/') ? 'is-active' : '' }}"
-                   href="{{ url('/') }}">{{ __('Home') }}</a>
-                @include('layouts.partials._navbar_categories')
-                <a class="navbar-item {{ Request::is('goldenbook*') ? 'is-active' : '' }}"
-                   href="{{ route('goldenbook.index') }}">{{ __('Golden book') }}</a>
-{{--                <a class="navbar-item {{ Request::is('about') ? 'is-active' : '' }}" href="#">{{ __('About') }}</a>--}}
-                <a class="navbar-item {{ Request::is('contact*') ? 'is-active' : '' }}"
-                   href="{{ route('contact.index') }}">{{ __('Contact') }}</a>
-                @auth()
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link" href="{{ route('admin.dashboard') }}">
+                    <a role="button" class="navbar-burger burger" aria-label="menu" data-target="navMenu" aria-expanded="false">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
+                </div>
+                <div id="navMenu" class="navbar-menu">
+                    <div class="navbar-end">
+                        <a class="navbar-item {{ Request::is('/') ? 'is-active' : '' }}"
+                           href="{{ url('/') }}">{{ __('Home') }}</a>
+                        @include('layouts.partials._navbar_categories')
+                        <a class="navbar-item {{ Request::is('goldenbook*') ? 'is-active' : '' }}"
+                           href="{{ route('goldenbook.index') }}">{{ __('Golden book') }}</a>
+                        {{--                <a class="navbar-item {{ Request::is('about') ? 'is-active' : '' }}" href="#">{{ __('About') }}</a>--}}
+                        <a class="navbar-item {{ Request::is('contact*') ? 'is-active' : '' }}"
+                           href="{{ route('contact.index') }}">{{ __('Contact') }}</a>
+                        @auth()
+                            <div class="navbar-item has-dropdown is-hoverable">
+                                <a class="navbar-link" href="{{ route('admin.dashboard') }}">
                             <span class="icon">
                                 <i class="fas fa-user"></i>
                             </span>
-                            <span>{{ Auth::user()->name }}</span>
-                        </a>
-
-                        <div class="navbar-dropdown is-boxed is-right">
-
-                            @can('dashboard')
-                                <a class="navbar-item  {{ Request::is('admin*') ? 'is-active' : '' }} "
-                                   href="{{ route('admin.dashboard') }}">
-                                    {{ __('Admin') }}
+                                    <span>{{ Auth::user()->name }}</span>
                                 </a>
-                            @endcan
-                            <hr class="navbar-divider">
-                            <a class="navbar-item has-text-danger"
-                               onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                  style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
+                                <div class="navbar-dropdown is-boxed is-right">
+
+                                    @can('dashboard')
+                                        <a class="navbar-item  {{ Request::is('admin*') ? 'is-active' : '' }} "
+                                           href="{{ route('admin.dashboard') }}">
+                                            {{ __('Admin') }}
+                                        </a>
+                                    @endcan
+                                    <hr class="navbar-divider">
+                                    <a class="navbar-item has-text-danger"
+                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </div>
+                        @endguest
+{{--                        <span class="navbar-item">--}}
+{{--                          <a class="button is-primary is-inverted">--}}
+{{--                            <span class="icon">--}}
+{{--                              <i class="fab fa-github"></i>--}}
+{{--                            </span>--}}
+{{--                            <span>Download</span>--}}
+{{--                          </a>--}}
+{{--                        </span>--}}
                     </div>
-                @endguest
+                </div>
             </div>
+        </nav>
+    </div>
+
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <h1 class="title">
+                @yield('pageTitle')
+            </h1>
+{{--            <h2 class="subtitle">--}}
+{{--                Subtitle--}}
+{{--            </h2>--}}
         </div>
     </div>
-</nav>
+
+    <!-- Hero footer: will stick at the bottom -->
+{{--    <div class="hero-foot">--}}
+{{--        <nav class="tabs">--}}
+{{--            <div class="container">--}}
+{{--                <ul>--}}
+{{--                    <li class="is-active"><a>Overview</a></li>--}}
+{{--                    <li><a>Modifiers</a></li>--}}
+{{--                    <li><a>Grid</a></li>--}}
+{{--                    <li><a>Elements</a></li>--}}
+{{--                    <li><a>Components</a></li>--}}
+{{--                    <li><a>Layout</a></li>--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--        </nav>--}}
+{{--    </div>--}}
+</section>
+
+{{--<nav class="navbar" role="navigation" aria-label="dropdown navigation">--}}
+{{--    <div class="container">--}}
+{{--        <div class="navbar-brand">--}}
+{{--            <a href="{{ url('/') }}" class="navbar-item">{{ settings()->get('app_name', config('app.name', 'Flasher')) }}</a>--}}
+
+{{--            @include('layouts.partials._navbar_socials')--}}
+
+{{--            <a role="button" class="navbar-burger burger" aria-label="menu" data-target="navMenu" aria-expanded="false">--}}
+{{--                <span></span>--}}
+{{--                <span></span>--}}
+{{--                <span></span>--}}
+{{--            </a>--}}
+{{--        </div>--}}
+
+{{--        <div class="navbar-menu" id="navMenu">--}}
+{{--            <div class="navbar-end">--}}
+{{--                <a class="navbar-item {{ Request::is('/') ? 'is-active' : '' }}"--}}
+{{--                   href="{{ url('/') }}">{{ __('Home') }}</a>--}}
+{{--                @include('layouts.partials._navbar_categories')--}}
+{{--                <a class="navbar-item {{ Request::is('goldenbook*') ? 'is-active' : '' }}"--}}
+{{--                   href="{{ route('goldenbook.index') }}">{{ __('Golden book') }}</a>--}}
+{{--                <a class="navbar-item {{ Request::is('about') ? 'is-active' : '' }}" href="#">{{ __('About') }}</a>--}}
+{{--                <a class="navbar-item {{ Request::is('contact*') ? 'is-active' : '' }}"--}}
+{{--                   href="{{ route('contact.index') }}">{{ __('Contact') }}</a>--}}
+{{--                @auth()--}}
+{{--                    <div class="navbar-item has-dropdown is-hoverable">--}}
+{{--                        <a class="navbar-link" href="{{ route('admin.dashboard') }}">--}}
+{{--                            <span class="icon">--}}
+{{--                                <i class="fas fa-user"></i>--}}
+{{--                            </span>--}}
+{{--                            <span>{{ Auth::user()->name }}</span>--}}
+{{--                        </a>--}}
+
+{{--                        <div class="navbar-dropdown is-boxed is-right">--}}
+
+{{--                            @can('dashboard')--}}
+{{--                                <a class="navbar-item  {{ Request::is('admin*') ? 'is-active' : '' }} "--}}
+{{--                                   href="{{ route('admin.dashboard') }}">--}}
+{{--                                    {{ __('Admin') }}--}}
+{{--                                </a>--}}
+{{--                            @endcan--}}
+{{--                            <hr class="navbar-divider">--}}
+{{--                            <a class="navbar-item has-text-danger"--}}
+{{--                               onclick="event.preventDefault();document.getElementById('logout-form').submit();">--}}
+{{--                                {{ __('Logout') }}--}}
+{{--                            </a>--}}
+
+{{--                            <form id="logout-form" action="{{ route('logout') }}" method="POST"--}}
+{{--                                  style="display: none;">--}}
+{{--                                @csrf--}}
+{{--                            </form>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                @endguest--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</nav>--}}
