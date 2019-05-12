c0d423a1eba4d9cf0138d9d204ebc8aa25757322
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-05-12 18:50:55 +0200

    remove telescope in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->isLocal()) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
         }
     }
 }
