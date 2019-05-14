9f21cb1ab006fd8e510c379fbe168594df09918d
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-05-14 23:05:20 +0200

    add footer links

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,5 +1,34 @@
 <footer class="footer">
     <div class="content has-text-centered">
+        <div class="columns is-mobile is-centered">
+            <div class="column is-narrow">
+                <div class="has-text-centered">
+                    <a href="{{ url('/') }}">{{ __('Home') }}</a>
+                </div>
+            </div>
+            <div class="column is-narrow">
+                <div class="has-text-centered">
+                    <a href="{{ route('albums.index')  }}">{{ __('Albums') }}</a>
+                </div>
+            </div>
+            <div class="column is-narrow">
+                <div class="has-text-centered">
+                    <a href="{{ route('categories.index')  }}">{{ __('Categories') }}</a>
+                </div>
+            </div>
+            <div class="column is-narrow">
+                <div class="has-text-centered">
+                    <a href="{{ route('goldenbook.index')  }}">{{ __('Golden book') }}</a>
+                </div>
+            </div>
+            <div class="column is-narrow">
+                <div class="has-text-centered">
+                    <a href="{{ route('contact.index')  }}">{{ __('Contact') }}</a>
+                </div>
+            </div>
+        </div>
+
+
         {!! settings()->get('footer_content') !!}
         <div class="has-text-centered">
             <i class="fas fa-code" style="color: dodgerblue;"></i>
