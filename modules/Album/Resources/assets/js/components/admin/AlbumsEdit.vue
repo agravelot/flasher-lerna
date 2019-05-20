e987096fb0af8049e5e73ab6ba5dfecf50a3366e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-05-20 14:10:02 +0200

    add card for album edit

--- modules/Album/Resources/assets/js/components/admin/AlbumsEdit.vue
+++ modules/Album/Resources/assets/js/components/admin/AlbumsEdit.vue
@@ -1,98 +1,103 @@
 <template>
-    <div>
+    <section>
         <h1 class="title">Update album</h1>
-        <b-tabs type="is-boxed" size="is-medium" class="block">
-            <b-tab-item label="Album" icon-pack="fas" icon="info">
-                <form @submit.prevent="updateAlbum">
 
-                    <b-field label="Title"
-                             :type="errors.title ? 'is-danger' : ''"
-                             :message="errors.title ? errors.title[0] : null">
-                        <b-input type="text"
-                                 v-model="album.title"
-                                 maxlength="30">
-                        </b-input>
-                    </b-field>
+        <div class="card">
+            <div class="card-content">
+                <b-tabs type="is-boxed" size="is-medium" class="block">
+                    <b-tab-item label="Album" icon-pack="fas" icon="info">
+                        <form @submit.prevent="updateAlbum">
 
-                    <quill-editor v-model="album.body" ref="myQuillEditor" :options="editorOption"></quill-editor>
+                            <b-field label="Title"
+                                     :type="errors.title ? 'is-danger' : ''"
+                                     :message="errors.title ? errors.title[0] : null">
+                                <b-input type="text"
+                                         v-model="album.title"
+                                         maxlength="30">
+                                </b-input>
+                            </b-field>
 
-                    <b-field label="Enter some categories"
-                             :type="errors.categories ? 'is-danger' : ''"
-                             :message="errors.categories ? errors.categories[0] : null">
-                        <b-taginput
-                                v-model="album.categories"
-                                :data="filteredCategories"
-                                autocomplete
-                                :allow-new="false"
-                                field="name"
-                                placeholder="Add a category"
-                                icon-pack="fas"
-                                icon="tag"
-                                @typing="getFilteredCategories">
-                        </b-taginput>
-                    </b-field>
+                            <quill-editor v-model="album.body" ref="myQuillEditor" :options="editorOption"></quill-editor>
 
-                    <b-field label="Enter some cosplayers"
-                             :type="errors.cosplayers ? 'is-danger' : ''"
-                             :message="errors.cosplayers ? errors.cosplayers[0] : null">
-                        <b-taginput
-                                v-model="album.cosplayers"
-                                :data="filteredCosplayers"
-                                autocomplete
-                                :allow-new="false"
-                                field="name"
-                                placeholder="Add a cosplayer"
-                                icon="user-tag"
-                                @typing="getFilteredCosplayers">
-                        </b-taginput>
-                    </b-field>
+                            <b-field label="Enter some categories"
+                                     :type="errors.categories ? 'is-danger' : ''"
+                                     :message="errors.categories ? errors.categories[0] : null">
+                                <b-taginput
+                                        v-model="album.categories"
+                                        :data="filteredCategories"
+                                        autocomplete
+                                        :allow-new="false"
+                                        field="name"
+                                        placeholder="Add a category"
+                                        icon-pack="fas"
+                                        icon="tag"
+                                        @typing="getFilteredCategories">
+                                </b-taginput>
+                            </b-field>
 
-                    <b-field label="Should this album be published?"
-                             :type="errors.published_at ? 'is-danger' : ''"
-                             :message="errors.published_at ? errors.published_at[0] : null">
-                        <div class="field">
-                            <b-switch v-model="album.published_at"
-                                      :true-value="album.published_at || new Date().toISOString()"
-                                      :false-value=null>
-                                {{ album.published_at ? 'Yes' : 'No' }}
-                            </b-switch>
+                            <b-field label="Enter some cosplayers"
+                                     :type="errors.cosplayers ? 'is-danger' : ''"
+                                     :message="errors.cosplayers ? errors.cosplayers[0] : null">
+                                <b-taginput
+                                        v-model="album.cosplayers"
+                                        :data="filteredCosplayers"
+                                        autocomplete
+                                        :allow-new="false"
+                                        field="name"
+                                        placeholder="Add a cosplayer"
+                                        icon="user-tag"
+                                        @typing="getFilteredCosplayers">
+                                </b-taginput>
+                            </b-field>
+
+                            <b-field label="Should this album be published?"
+                                     :type="errors.published_at ? 'is-danger' : ''"
+                                     :message="errors.published_at ? errors.published_at[0] : null">
+                                <div class="field">
+                                    <b-switch v-model="album.published_at"
+                                              :true-value="album.published_at || new Date().toISOString()"
+                                              :false-value=null>
+                                        {{ album.published_at ? 'Yes' : 'No' }}
+                                    </b-switch>
+                                </div>
+                            </b-field>
+
+                            <b-field label="Should it be accessible publicly?"
+                                     :type="errors.private ? 'is-danger' : ''"
+                                     :message="errors.private ? errors.private[0] : null">
+                                <div class="field">
+                                    <b-switch v-model.numeric="album.private"
+                                              :true-value=0
+                                              :false-value=1>
+                                        {{ album.private ? 'No' : 'Yes' }}
+                                    </b-switch>
+                                </div>
+                            </b-field>
+
+                            <button class="button is-primary">Update</button>
+                        </form>
+
+                    </b-tab-item>
+                    <b-tab-item label="Pictures" icon-pack="fas" icon="images">
+                        <vue-dropzone ref="myVueDropzone" :options="dropzoneOptions" v-on:vdropzone-sending="sendingEvent"
+                                      v-on:vdropzone-complete="refreshMedias" class="has-margin-bottom-md"></vue-dropzone>
+                        <div class="columns is-multiline">
+                            <div v-for="picture in album.medias" class="column is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
+                                <img :src="picture.thumb" :alt="picture.name">
+                                <a class="button has-text-danger" @click="deleteAlbumPicture(album.slug, picture.id)">
+                                    Delete
+                                </a>
+                            </div>
                         </div>
-                    </b-field>
+                    </b-tab-item>
+                    <b-tab-item label="Prévualisation" icon-pack="fas" icon="eye">
+                        <album-show-gallery></album-show-gallery>
+                    </b-tab-item>
+                </b-tabs>
+            </div>
+        </div>
 
-                    <b-field label="Should it be accessible publicly?"
-                             :type="errors.private ? 'is-danger' : ''"
-                             :message="errors.private ? errors.private[0] : null">
-                        <div class="field">
-                            <b-switch v-model.numeric="album.private"
-                                      :true-value=0
-                                      :false-value=1>
-                                {{ album.private ? 'No' : 'Yes' }}
-                            </b-switch>
-                        </div>
-                    </b-field>
-
-                    <button class="button is-primary">Update</button>
-                </form>
-
-            </b-tab-item>
-            <b-tab-item label="Pictures" icon-pack="fas" icon="images">
-                <vue-dropzone ref="myVueDropzone" :options="dropzoneOptions" v-on:vdropzone-sending="sendingEvent"
-                              v-on:vdropzone-complete="refreshMedias" class="has-margin-bottom-md"></vue-dropzone>
-                <div class="columns is-multiline">
-                    <div v-for="picture in album.medias" class="column is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
-                        <img :src="picture.thumb" :alt="picture.name">
-                        <a class="button has-text-danger" @click="deleteAlbumPicture(album.slug, picture.id)">
-                            Delete
-                        </a>
-                    </div>
-                </div>
-            </b-tab-item>
-            <b-tab-item label="Prévualisation" icon-pack="fas" icon="eye">
-                <album-show-gallery></album-show-gallery>
-            </b-tab-item>
-        </b-tabs>
-
-    </div>
+    </section>
 </template>
 
 <script lang="ts">
