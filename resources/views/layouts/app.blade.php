f3a8962fe8a94ca56441de18723b8c7067478012
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-05-22 08:57:33 +0200

    update page home page title check

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('pageTitle')
-        - {{ settings()->has('app_name') ? settings()->get('app_name'): config('app.name', 'Flasher') }}</title>
+        - {{ settings()->get('app_name', config('app.name', 'Flasher')) }}</title>
 
     <meta name="description" content="@yield('seo_description', settings()->get('seo_description'))"/>
 
