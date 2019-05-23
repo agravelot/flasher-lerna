1f523dc92c92d58889d71033d60f6c1ddeb59ddd
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-05-23 11:00:36 +0200

    add purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,39 +15,37 @@ mix.mergeManifest();
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.ts('resources/js/app.ts', 'public/js')
+    .ts('resources/js/admin/admin.ts', 'public/js')
+    .js('resources/js/admin/tinymce.js', 'public/js')
+;
 
 mix.sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/error-illustrated.scss', 'public/css')
     .version()
-    // .purgeCss(
-    //     {
-    //         // Your custom globs are merged with the default globs. If you need to
-    //         // fully replace the globs, use the underlying `paths` option instead.
-    //         globs: [
-    //             path.join(__dirname, 'modules/**/*'),
-    //         ],
-    //
-    //     }
-    // )
-;
-
-mix.ts('resources/js/app.ts', 'public/js')
-    .ts('resources/js/admin/admin.ts', 'public/js')
-    .js('resources/js/admin/tinymce.js', 'public/js')
-    .version()
-    // .extract([
-    //     'vue',
-    //     'buefy',
-    //     'axios',
-    //     'vue-axios',
-    //     '@fortawesome/fontawesome-svg-core',
-    //     '@fortawesome/free-brands-svg-icons',
-    //     '@fortawesome/free-regular-svg-icons',
-    //     '@fortawesome/free-solid-svg-icons',
-    //     // 'bulma-modal-fx',
-    //     'dropzone',
-    //     'vue2-dropzone',
-    // ])
+    .purgeCss(
+        {
+            // Your custom globs are merged with the default globs. If you need to
+            // fully replace the globs, use the underlying `paths` option instead.
+            globs: [
+                path.join(__dirname, 'modules/**/*'),
+                path.join(__dirname, 'node_modules/buefy/**/*'),
+            ],
+        }
+    )
+// .extract([
+//     'vue',
+//     'buefy',
+//     'axios',
+//     'vue-axios',
+//     '@fortawesome/fontawesome-svg-core',
+//     '@fortawesome/free-brands-svg-icons',
+//     '@fortawesome/free-regular-svg-icons',
+//     '@fortawesome/free-solid-svg-icons',
+//     // 'bulma-modal-fx',
+//     'dropzone',
+//     'vue2-dropzone',
+// ])
 ;
 
 mix.options({
