bc33f499b325e49f946c1e128f45a98c1a7399d6
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-06-05 01:53:17 +0200

    reformat

--- resources/js/admin/Buefy.vue
+++ resources/js/admin/Buefy.vue
@@ -1,22 +1,22 @@
 <script lang="ts">
-    import Vue from 'vue';
-    import Buefy from 'buefy';
+import Vue from 'vue';
+import Buefy from 'buefy';
 
-    Vue.use(Buefy, {
-        defaultIconPack: 'fas',
-    });
+Vue.use(Buefy, {
+    defaultIconPack: 'fas',
+});
 
-    export default class VueBuefy extends Vue {
-        $dialog: any;
-        $loading: any;
-        $modal: any;
-        $snackbar: any;
-        $toast: any;
-        $refs: any;
-    }
+export default class VueBuefy extends Vue {
+    $dialog: any;
+    $loading: any;
+    $modal: any;
+    $snackbar: any;
+    $toast: any;
+    $refs: any;
+}
 </script>
 
 <style lang="scss">
-    @import "../../../resources/sass/app";
-    @import "~buefy/src/scss/buefy";
+@import '../../../resources/sass/app';
+@import '~buefy/src/scss/buefy';
 </style>
--- resources/js/admin/router.ts
+++ resources/js/admin/router.ts
@@ -7,57 +7,86 @@ const routes: Array<any> = [
     {
         path: '/admin',
         name: 'admin.dashboard',
-        component: () => import(/* webpackChunkName: "dashboard" */  '../../../modules/Dashboard/Resources/assets/js/Dashboard.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "dashboard" */ '../../../modules/Dashboard/Resources/assets/js/Dashboard.vue'
+            ),
     },
     // Albums
     {
         path: '/admin/albums',
         name: 'admin.albums.index',
-        component: () => import(/* webpackChunkName: "albumsIndex" */  '../../../modules/Album/Resources/assets/js/components/admin/AlbumsIndex.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "albumsIndex" */ '../../../modules/Album/Resources/assets/js/components/admin/AlbumsIndex.vue'
+            ),
     },
     {
         path: '/admin/albums/create',
         name: 'admin.albums.create',
-        component: () => import(/* webpackChunkName: "albumsCreate" */  '../../../modules/Album/Resources/assets/js/components/admin/AlbumsCreate.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "albumsCreate" */ '../../../modules/Album/Resources/assets/js/components/admin/AlbumsCreate.vue'
+            ),
     },
     {
         path: '/admin/albums/:slug',
         name: 'admin.albums.show',
-        component: () => import(/* webpackChunkName: "albumsShow" */  '../../../modules/Album/Resources/assets/js/components/admin/AlbumsShowGallery.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "albumsShow" */ '../../../modules/Album/Resources/assets/js/components/admin/AlbumsShowGallery.vue'
+            ),
     },
     {
         path: '/admin/albums/:slug/edit',
         name: 'admin.albums.edit',
-        component: () => import(/* webpackChunkName: "albumsEdit" */  '../../../modules/Album/Resources/assets/js/components/admin/AlbumsEdit.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "albumsEdit" */ '../../../modules/Album/Resources/assets/js/components/admin/AlbumsEdit.vue'
+            ),
     },
     //Cosplayers
     {
         path: '/admin/cosplayers',
         name: 'admin.cosplayers.index',
-        component: () => import(/* webpackChunkName: "cosplayersIndex" */  '../../../modules/Cosplayer/Resources/assets/js/components/admin/CosplayersIndex.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "cosplayersIndex" */ '../../../modules/Cosplayer/Resources/assets/js/components/admin/CosplayersIndex.vue'
+            ),
     },
     {
         path: '/admin/cosplayers/:slug/edit',
         name: 'admin.cosplayers.edit',
-        component: () => import(/* webpackChunkName: "cosplayersIndex" */  '../../../modules/Cosplayer/Resources/assets/js/components/admin/CosplayersEdit.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "cosplayersIndex" */ '../../../modules/Cosplayer/Resources/assets/js/components/admin/CosplayersEdit.vue'
+            ),
     },
     // Users
     {
         path: '/admin/users',
         name: 'admin.users.index',
-        component: () => import(/* webpackChunkName: "cosplayersIndex" */  '../../../modules/User/Resources/assets/js/UsersIndex.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "cosplayersIndex" */ '../../../modules/User/Resources/assets/js/UsersIndex.vue'
+            ),
     },
     // Settings
     {
         path: '/admin/settings',
         name: 'admin.settings.index',
-        component: () => import(/* webpackChunkName: "cosplayersIndex" */  '../../../modules/Core/Resources/assets/js/components/Settings.vue'),
-
+        component: () =>
+            import(
+                /* webpackChunkName: "cosplayersIndex" */ '../../../modules/Core/Resources/assets/js/components/Settings.vue'
+            ),
     },
     {
         path: '/admin/pages',
         name: 'admin.pages.index',
-        component: () => import(/* webpackChunkName: "cosplayersIndex" */   '../../../modules/Core/Resources/assets/js/components/pages/PagesIndex.vue'),
+        component: () =>
+            import(
+                /* webpackChunkName: "cosplayersIndex" */ '../../../modules/Core/Resources/assets/js/components/pages/PagesIndex.vue'
+            ),
     },
 ];
 
