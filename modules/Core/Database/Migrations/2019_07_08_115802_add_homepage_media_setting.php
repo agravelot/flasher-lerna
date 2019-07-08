ab9111166189f28c08efbba6cf5d73f42d21f015
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-07-08 14:06:03 +0200

    reformat

--- modules/Core/Database/Migrations/2019_07_08_115802_add_homepage_media_setting.php
+++ modules/Core/Database/Migrations/2019_07_08_115802_add_homepage_media_setting.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+/*
+ * (c) Antoine GRAVELOT <antoine.gravelot@hotmail.fr> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Antoine Gravelot <agravelot@hotmail.fr>
+ */
+
 use Illuminate\Database\Migrations\Migration;
 
 class AddHomepageMediaSetting extends Migration
