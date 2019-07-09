baef92b7d44ee090fc11d19120dcd726eb9e71e8
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-07-09 04:15:52 +0200

    move arrow navigable modals

--- resources/js/modal.ts
+++ resources/js/modal.ts
@@ -67,6 +67,74 @@
         return {
             init: function () {
                 events();
+
+                window.addEventListener(
+                    'keydown',
+                    function(event) {
+                        if (event.defaultPrevented) {
+                            return; // Do nothing if the event was already processed
+                        }
+
+                        switch (event.key) {
+                            case 'Left': // IE/Edge specific value
+                                previousModal();
+                                break;
+                            case 'ArrowLeft':
+                                previousModal();
+                                break;
+                            case 'Right': // IE/Edge specific value
+                                nextModal();
+                                break;
+                            case 'ArrowRight':
+                                nextModal();
+                                break;
+                            default:
+                                return; // Quit when this doesn't handle the key event.
+                        }
+
+                        // Cancel the default action to avoid it being handled twice
+                        event.preventDefault();
+                    },
+                    true
+                );
+
+                function previousModal() {
+                    let currentActiveModal: Element = getCurrentModal();
+                    let previousModal: Element = currentActiveModal.previousElementSibling;
+
+                    if (!previousModal || !previousModal.classList.contains('is-modal-navigable')) {
+                        return;
+                    }
+
+                    closeModal(currentActiveModal);
+                    openModal(previousModal);
+                }
+
+                function nextModal() {
+                    let currentActiveModal: Element = getCurrentModal();
+                    let nextModal: Element = currentActiveModal.nextElementSibling;
+
+                    if (!nextModal || !nextModal.classList.contains('is-modal-navigable')) {
+                        return;
+                    }
+
+                    closeModal(currentActiveModal);
+                    openModal(nextModal);
+                }
+
+                function getCurrentModal(): Element {
+                    return document.getElementsByClassName('is-modal-navigable is-active')[0];
+                }
+
+                function closeModal(modal: Element): void {
+                    modal.classList.remove('is-active');
+                }
+
+                function openModal(modal: Element): void {
+                    modal.classList.add('is-active');
+                }
+
+
             }
         }
     })();
