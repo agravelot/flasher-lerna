1c9da9029574b846c510886c66efca25385544b4
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-07-12 16:49:02 +0200

    add user impersonate

--- modules/User/Resources/assets/js/UsersIndex.vue
+++ modules/User/Resources/assets/js/UsersIndex.vue
@@ -58,6 +58,14 @@
                             user.row.role
                         }}</span>
                     </b-table-column>
+
+                    <b-table-column field="actions.impersonate" label="Impersonate" centered>
+                        <a :href="user.row.actions.impersonate">
+                             <span class="icon has-text-info">
+                                <i class="fas fa-sign-in-alt"></i>
+                            </span>
+                        </a>
+                    </b-table-column>
                 </template>
 
                 <template slot="empty">
--- resources/js/admin/fontawsome.ts
+++ resources/js/admin/fontawsome.ts
@@ -27,6 +27,7 @@ import {
     faTrashAlt,
     faUpload,
     faUserTag,
+    faLink,
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
@@ -56,7 +57,8 @@ library.add(
     faExclamationCircle,
     faBell,
     faStickyNote,
-    faEdit
+    faEdit,
+    faLink
 );
 
 // Replace any existing <i> tags with <svg> and set up a MutationObserver to
