85d741924d6f0093141a90541a2cf5f9c3d52293
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-07-16 14:49:56 +0200

    add album og tags

---
+++ resources/views/layouts/partials/_og_tags.blade.php
@@ -0,0 +1,35 @@
+{{--Namespace URI: http://ogp.me/ns/article#--}}
+
+
+@section('head')
+    @php
+        /** @var \Modules\Album\Entities\Album $album */
+    @endphp
+
+    <meta property="og:title" content="{{ $album->title }}"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="{{ request()->url() }}"/>
+
+    <meta property="og:locale" content="{{ app()->getLocale() }}" />
+{{--    <meta property="og:locale:alternate" content="{{ app()->getFallbackLocale() }}" />--}}
+
+
+    <meta property="article:published_time" content="{{ $album->published_at }}"/>
+    <meta property="article:modified_time" content="{{ $album->updated_at }}"/>
+    {{--<meta property="article:expiration_time" content="" />--}}
+    <meta property="article:author" content="{{ $album->user->name }}"/>
+    <meta property="article:section" content="Photography"/>
+    @foreach($album->categories()->pluck('name') as $categoryName)
+        <meta property="article:tag" content="{{ $categoryName }}"/>
+    @endforeach
+
+    {{--Image--}}
+    <meta property="og:image" content="{{ $album->cover->getUrl('thumb') }}"/>
+    <meta property="og:image:secure_url" content="{{ $album->cover->getUrl('thumb') }}"/>
+    <meta property="og:image:type" content="{{ $album->cover->mime_type }}"/>
+    <meta property="og:image:width" content="{{ $album->cover->width }}"/>
+    <meta property="og:image:height" content="{{ $album->cover->height }}"/>
+    <meta property="og:image:alt" content="{{ $album->cover->name }}"/>
+@stop
+
+
