03b122ea117868a4883c901417dbed6d85417b99
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-07-16 22:17:41 +0200

    setup sendgrid mail driver

--- config/services.php
+++ config/services.php
@@ -20,24 +20,28 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+    'sendgrid' => [
+        'api_key' => env('SENDGRID_API_KEY'),
     ],
 
-    'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => env('SES_REGION', 'us-east-1'),
-    ],
-
-    'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
-    ],
-
-    'stripe' => [
-        'model' => App\Models\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
-    ],
+//    'mailgun' => [
+//        'domain' => env('MAILGUN_DOMAIN'),
+//        'secret' => env('MAILGUN_SECRET'),
+//    ],
+//
+//    'ses' => [
+//        'key' => env('SES_KEY'),
+//        'secret' => env('SES_SECRET'),
+//        'region' => env('SES_REGION', 'us-east-1'),
+//    ],
+//
+//    'sparkpost' => [
+//        'secret' => env('SPARKPOST_SECRET'),
+//    ],
+//
+//    'stripe' => [
+//        'model' => App\Models\User::class,
+//        'key' => env('STRIPE_KEY'),
+//        'secret' => env('STRIPE_SECRET'),
+//    ],
 ];
