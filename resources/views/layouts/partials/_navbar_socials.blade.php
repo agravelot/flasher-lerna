0b986b0d10f5efe54b02787f2867854ea2982c0b
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-07-22 10:37:23 +0200

    footer in black

--- resources/views/layouts/partials/_navbar_socials.blade.php
+++ resources/views/layouts/partials/_navbar_socials.blade.php
@@ -1,5 +1,5 @@
 @foreach($socialMedias as $media)
-    <a class="navbar-item" href="{{ $media->url }}"
+    <a class="{{ $class }}" href="{{ $media->url }}"
        target="_blank" rel="noreferrer" aria-label="{{ $media->name }}">
         <span class="icon" style="color: {{ $media->color }};">
             @fa($media->icon)
