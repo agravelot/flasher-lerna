e6e5a3d75499abc246cc3d8999118076b52c01bb
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-07-26 11:47:20 +0200

    albums: add share button

---
+++ config/socials-networks.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * (c) Antoine GRAVELOT <antoine.gravelot@hotmail.fr> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Antoine Gravelot <agravelot@hotmail.fr>
+ */
+
+return [
+
+    'facebook' => [
+        'icon' => 'facebook-f',
+        'url' => 'https://www.facebook.com/sharer/sharer.php?u='
+    ],
+    'twitter' => [
+        'icon' => 'twitter',
+        'url' => 'https://twitter.com/intent/tweet?text='
+    ]
+
+];
