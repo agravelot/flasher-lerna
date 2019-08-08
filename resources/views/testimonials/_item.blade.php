f0dcae5bcc1aad370a6d201e83d3057eb0019115
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2019-08-08 22:34:49 +0200

    remove box around testimonial

--- resources/views/testimonials/_item.blade.php
+++ resources/views/testimonials/_item.blade.php
@@ -1,21 +1,19 @@
-<div class="box">
-    <article class="media">
-        <div class="media-content">
-            <div class="content">
+<article class="media">
+    <div class="media-content">
+        <div class="content">
 
-                <p class="is-italic has-text-centered is-family-secondary">
-                    <span class="icon is-inline-flex">
-                        @fas('quote-left')
-                    </span>
-                    {{ $goldenBookPost->body }}
-                    <span class="icon is-inline-flex">
-                        @fas('quote-right')
-                    </span>
-                </p>
+            <p class="is-italic has-text-centered is-family-secondary">
+                <span class="icon is-inline-flex">
+                    @fas('quote-left')
+                </span>
+                {{ $goldenBookPost->body }}
+                <span class="icon is-inline-flex">
+                    @fas('quote-right')
+                </span>
+            </p>
 
-                <p class="has-text-right has-text-weight-light">- {{ $goldenBookPost->name }}</p>
-                <p class="has-text-right has-text-weight-light">{{ $goldenBookPost->created_at->toFormattedDateString() }}</p>
-            </div>
+            <p class="has-text-right has-text-weight-light">- {{ $goldenBookPost->name }}</p>
+            <p class="has-text-right has-text-weight-light">{{ $goldenBookPost->created_at->toFormattedDateString() }}</p>
         </div>
-    </article>
-</div>
+    </div>
+</article>
