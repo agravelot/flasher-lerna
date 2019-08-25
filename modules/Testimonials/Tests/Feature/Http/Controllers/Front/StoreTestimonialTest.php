d4338ac177882d908cb8764508c2ae95b58753d7
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-08-25 21:23:02 +0200

    fix tests

--- modules/Testimonials/Tests/Feature/Http/Controllers/Front/StoreTestimonialTest.php
+++ modules/Testimonials/Tests/Feature/Http/Controllers/Front/StoreTestimonialTest.php
@@ -11,6 +11,7 @@ namespace Modules\Testimonial\Tests\Features\Http\Controllers\Front;
 
 use Tests\TestCase;
 use App\Models\Testimonial;
+use Anhskohbo\NoCaptcha\Facades\NoCaptcha;
 use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -21,46 +22,65 @@ class StoreTestimonialTest extends TestCase
     public function test_admin_can_store_a_testimonial(): void
     {
         $this->actingAsAdmin();
+        /** @var Testimonial $testimonial */
         $testimonial = factory(Testimonial::class)->make();
         $testimonial->name = 'Test title';
 
         $response = $this->storeTestimonial($testimonial);
 
-        $response->assertStatus(201)
-            ->assertSee($testimonial->title);
+        $this->followRedirects($response)
+            ->assertStatus(200)
+            ->assertDontSee($testimonial->name);
         $this->assertCount(1, Testimonial::all());
     }
 
     private function storeTestimonial(Testimonial $testimonial): TestResponse
     {
+        // prevent validation error on captcha
+        NoCaptcha::shouldReceive('verifyResponse')
+            ->once()
+            ->andReturn(true);
+
+        // provide hidden input for your 'required' validation
+        NoCaptcha::shouldReceive('display')
+            ->zeroOrMoreTimes()
+            ->andReturn('<input type="hidden" name="g-recaptcha-response" value="1" />');
+        session()->setPreviousUrl('/testimonials/create');
+
         return $this->post('/testimonials', [
             'name' => $testimonial->name,
-            'description' => $testimonial->description,
+            'email' => $testimonial->email,
+            'body' => $testimonial->body,
+            'g-recaptcha-response' => '1',
         ]);
     }
 
     public function test_user_can_store_a_testimonial(): void
     {
         $this->actingAsUser();
+        /** @var Testimonial $testimonial */
         $testimonial = factory(Testimonial::class)->make();
         $testimonial->name = 'Test title';
 
         $response = $this->storeTestimonial($testimonial);
 
-        $response->assertStatus(201)
-            ->assertSee($testimonial->title);
+        $this->followRedirects($response)
+            ->assertStatus(200)
+            ->assertDontSee($testimonial->name);
         $this->assertCount(1, Testimonial::all());
     }
 
     public function test_guest_can_store_a_testimonial(): void
     {
+        /** @var Testimonial $testimonial */
         $testimonial = factory(Testimonial::class)->make();
         $testimonial->name = 'Test title';
 
         $response = $this->storeTestimonial($testimonial);
 
-        $response->assertStatus(201)
-            ->assertSee($testimonial->title);
+        $this->followRedirects($response)
+            ->assertStatus(200)
+            ->assertDontSee($testimonial->name);
         $this->assertCount(1, Testimonial::all());
     }
 }
