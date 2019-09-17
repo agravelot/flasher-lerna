d78bb8fa4c15b94a8dfabb027483bb612d71edbc
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-09-17 15:43:08 +0200

    dusk(login): add more test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -31,4 +31,23 @@ class LoginTest extends DuskTestCase
                 ->assertAuthenticatedAs($admin);
         });
     }
+
+    public function test_admin_redirected_to_login_from_admin_and_can_login_and_redirected_to_admin(): void
+    {
+        $password = '123456789';
+        /** @var User $admin */
+        $admin = factory(User::class)->state('admin')->create([
+            'password' => $password,
+        ]);
+
+        $this->browse(function (Browser $browser) use ($admin, $password) {
+            $browser->visit('/admin')
+                ->assertPathIs('/login')
+                ->type('email', $admin->email)
+                ->type('password', $password)
+                ->press('Se connecter')
+                ->assertPathIs('/admin')
+                ->assertAuthenticatedAs($admin);
+        });
+    }
 }
