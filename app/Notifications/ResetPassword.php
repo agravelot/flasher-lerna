596ea1cd345d4f49769d5fcf5b9c2a44689cc875
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-01 22:40:34 +0200

    do not ses mail from dynamically

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -4,22 +4,9 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordBase;
 
 class ResetPassword extends ResetPasswordBase implements ShouldQueue
 {
     use Queueable;
-
-    /**
-     * Build the mail representation of the notification.
-     */
-    public function toMail($notifiable): MailMessage
-    {
-        $mail = parent::toMail($notifiable);
-        $from = settings()->get('email_from');
-        $name = settings()->get('app_name');
-
-        return $mail->from($from, $name);
-    }
 }
