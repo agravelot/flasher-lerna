879a9048a047dc55e153fe61ee353c7642a35241
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-04 14:15:55 +0200

    sidebar: rewrite with buefy

--- resources/js/components/admin/Sidebar.vue
+++ resources/js/components/admin/Sidebar.vue
@@ -1,91 +1,26 @@
 <template>
     <div class="column is-one-fifth" id="admin-menu">
+
+        <b-menu>
+            <b-menu-list label="Administration">
+                <b-menu-item label="Dashboard" icon="home" tag="router-link" :to="{ name: 'admin.dashboard' }" exact></b-menu-item>
+                <b-menu-item label="Albums" icon="images" tag="router-link" :to="{ name: 'admin.albums.index' }"></b-menu-item>
+                <b-menu-item label="Categories" icon="tags" tag="router-link" :to="{ name: 'admin.categories.index' }"></b-menu-item>
+                <b-menu-item label="Cosplayers" icon="user-tag" tag="router-link" :to="{ name: 'admin.cosplayers.index' }"></b-menu-item>
+                <b-menu-item label="Contacts" icon="pen-fancy" tag="router-link" :to="{ name: 'admin.contacts.index' }"></b-menu-item>
+                <b-menu-item label="Users" icon="user" tag="router-link" :to="{ name: 'admin.users.index' }"></b-menu-item>
+                <b-menu-item label="Testimonials" icon="book" tag="router-link" :to="{ name: 'admin.testimonials.index' }"></b-menu-item>
+                <b-menu-item label="Social medias" icon="external-link-alt" tag="a" to="/admin/social-medias"></b-menu-item>
+                <b-menu-item label="Settings" icon="cog" tag="router-link" :to="{ name: 'admin.settings.index' }"></b-menu-item>
+            </b-menu-list>
+
+            <b-menu-list label="Monitoring">
+                <b-menu-item label="Background tasks" icon="tasks" tag="a" to="/horizon"></b-menu-item>
+            </b-menu-list>
+        </b-menu>
+
         <div>
             <aside class="menu">
-                <ul class="menu-list has-margin-bottom-md">
-                    <li class="menu-label">
-                        Administration
-                    </li>
-
-                    <router-link :to="{ name: 'admin.dashboard' }" exact>
-                        <b-icon icon="home"></b-icon>
-                        Dashboard
-                    </router-link>
-
-                    <li>
-                        <router-link :to="{ name: 'admin.albums.index' }">
-                            <b-icon icon="images"></b-icon>
-                            Albums
-                        </router-link>
-                    </li>
-
-                    <li>
-                        <router-link :to="{ name: 'admin.categories.index' }">
-                            <b-icon icon="tags"></b-icon>
-                            Categories
-                        </router-link>
-                    </li>
-
-                    <li>
-                        <router-link :to="{ name: 'admin.cosplayers.index' }">
-                            <b-icon icon="user-tag"></b-icon>
-                            Cosplayers
-                        </router-link>
-                    </li>
-
-                    <li>
-                        <router-link :to="{ name: 'admin.contacts.index' }">
-                            <b-icon icon="pen-fancy"></b-icon>
-                            Contacts
-                        </router-link>
-                    </li>
-
-                    <li>
-                        <router-link :to="{ name: 'admin.users.index' }">
-                            <b-icon icon="user"></b-icon>
-                            Users
-                        </router-link>
-                    </li>
-
-                    <li>
-                        <router-link :to="{ name: 'admin.testimonials.index' }">
-                            <b-icon icon="book"></b-icon>
-                            Testimonials
-                        </router-link>
-                    </li>
-
-                    <li>
-                        <a href="/admin/social-medias">
-                            <b-icon icon="external-link-alt"></b-icon>
-                            Social medias
-                        </a>
-                    </li>
-
-                    <li>
-                        <router-link :to="{ name: 'admin.settings.index' }">
-                            <b-icon icon="cog"></b-icon>
-                            Settings
-                        </router-link>
-                    </li>
-                </ul>
-
-                <ul class="menu-list">
-                    <li class="menu-label">
-                        Monitoring
-                    </li>
-                    <li>
-                        <a href="#">
-                            <b-icon icon="tachometer-alt"></b-icon>
-                            Statistics
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/horizon">
-                            <b-icon icon="tasks"></b-icon>
-                            Background tasks
-                        </a>
-                    </li>
-                </ul>
 
                 <ul class="menu-list">
                     <li>
