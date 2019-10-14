02995bc4facd6471b57da5a57ab9555472999e15
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-14 13:50:03 +0200

    add more tests

--- tests/Feature/Http/Controller/Api/Admin/AlbumShare/ShareAlbumTest.php
+++ tests/Feature/Http/Controller/Api/Admin/AlbumShare/ShareAlbumTest.php
@@ -33,6 +33,7 @@ class ShareAlbumTest extends TestCase
                 return $mail->hasTo($contact->email);
             });
         });
+        Mail::assertSent(ShareAlbum::class, $contacts->count());
     }
 
     public function test_admin_can_not_share_unpublished_album()
@@ -51,11 +52,46 @@ class ShareAlbumTest extends TestCase
     {
         Mail::fake();
         $this->actingAsAdmin();
-        $album = factory(Album::class)->state('published')->create();
+        $album = factory(Album::class)->states(['published', 'password'])->create();
+        $contacts = collect([]);
 
         $response = $this->shareAlbum($album, collect([]));
 
         $response->assertStatus(201);
+        $contacts->each(static function ($contact) use ($album) {
+            Mail::assertSent(ShareAlbum::class, static function (Mailable $mail) use ($album, $contact) {
+                return $mail->album->id === $album->id;
+            });
+            // Assert a message was sent to the given users...
+            Mail::assertSent(ShareAlbum::class, static function (Mailable $mail) use ($album, $contact) {
+                return $mail->hasTo($contact->email);
+            });
+        });
+        Mail::assertSent(ShareAlbum::class, $contacts->count());
+    }
+
+    public function test_user_can_not_share_album()
+    {
+        Mail::fake();
+        $this->actingAsUser();
+        $album = factory(Album::class)->state('unpublished')->create();
+
+        $response = $this->shareAlbum($album, collect([]));
+
+        $response->assertStatus(403);
+        Mail::assertNothingSent();
+    }
+
+    public function test_guest_can_not_share_album()
+    {
+        Mail::fake();
+        $this->actingAsUser();
+        $album = factory(Album::class)->state('unpublished')->create();
+
+        $response = $this->shareAlbum($album, collect([]));
+
+        $response->assertStatus(401);
+        Mail::assertNothingSent();
     }
 
     private function shareAlbum(Album $album, Collection $contacts, string $message = ''): TestResponse
