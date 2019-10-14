12317a027ec5767532f7d96a48897d2fc118fdb1
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-14 11:57:21 +0200

    wip: add tests

---
+++ tests/Feature/Http/Controller/Api/Admin/AlbumShare/ShareAlbumTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature\Http\Controller\Api\AdminAlbum;
+
+use Tests\TestCase;
+use App\Models\Album;
+use Illuminate\Mail\Mailable;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ShareAlbumTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_admin_can_share_published_album()
+    {
+        Mail::fake();
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('published')->create();
+        $contacts = collect([]);
+
+        $response = $this->shareAlbum($album, $contacts);
+
+        $response->assertStatus(201);
+        $contacts->each(static function ($contact) use ($album) {
+            Mail::assertSent(ShareAlbum::class, static function (Mailable $mail) use ($album, $contact) {
+                return $mail->album->id === $album->id;
+            });
+            // Assert a message was sent to the given users...
+            Mail::assertSent(ShareAlbum::class, static function (Mailable $mail) use ($album, $contact) {
+                return $mail->hasTo($contact->email);
+            });
+        });
+    }
+
+    public function test_admin_can_not_share_unpublished_album()
+    {
+        Mail::fake();
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('unpublished')->create();
+
+        $response = $this->shareAlbum($album, collect([]));
+
+        $response->assertStatus(422);
+        Mail::assertNothingSent();
+    }
+
+    public function test_admin_can_share_published_album_with_password()
+    {
+        Mail::fake();
+        $this->actingAsAdmin();
+        $album = factory(Album::class)->state('published')->create();
+
+        $response = $this->shareAlbum($album, collect([]));
+
+        $response->assertStatus(201);
+    }
+
+    private function shareAlbum(Album $album, Collection $contacts, string $message = ''): TestResponse
+    {
+        return $this->json('post', "/api/admin/share-albums/{$album->slug}",
+            compact('contacts', 'message')
+        );
+    }
+}
