a1e3aea65623ad938144096ac38939766ad4579a
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-15 13:13:04 +0200

    opengraph: fix namespaces

--- resources/views/layouts/partials/_og_tags.blade.php
+++ resources/views/layouts/partials/_og_tags.blade.php
@@ -11,14 +11,14 @@
     <meta property="og:locale" content="{{ app()->getLocale() }}"/>
     {{--    <meta property="og:locale:alternate" content="{{ app()->getFallbackLocale() }}" />--}}
 
-    @if ($openGraph instanceof \App\Models\Contracts\ProfileOpenGraphable)
+    @if ($openGraph instanceof \App\Http\OpenGraphs\Contracts\ProfileOpenGraphable)
         <meta property="profile:username" content="{{ $openGraph->username() }}">
         <meta property="profile:first_name" content="{{ $openGraph->firstName() }}">
         <meta property="profile:last_name" content="{{ $openGraph->lastName() }}">
         <meta property="profile:gender" content="{{ $openGraph->gender() }}">
     @endif
 
-    @if ($openGraph instanceof \App\Models\Contracts\ArticleOpenGraphable)
+    @if ($openGraph instanceof \App\Http\OpenGraphs\Contracts\ArticleOpenGraphable)
         <meta property="article:published_time" content="{{ $openGraph->publishedAt() }}"/>
         <meta property="article:modified_time" content="{{ $openGraph->updatedAt() }}"/>
         {{--<meta property="article:expiration_time" content="" />--}}
@@ -29,7 +29,7 @@
         @endforeach
     @endif
 
-    @if ($openGraph instanceof \App\Models\Contracts\ImagesOpenGraphable)
+    @if ($openGraph instanceof \App\Http\OpenGraphs\Contracts\ImagesOpenGraphable)
         @foreach($openGraph->images() as $image)
             <meta property="og:image" content="{{ $image->getUrl('thumb') }}"/>
             <meta property="og:image:secure_url" content="{{ $image->getUrl('thumb') }}"/>
