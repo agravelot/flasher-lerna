fe15005c02681316a32c6e996ee4d63aca21b7b5
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-19 23:28:50 +0200

    extract favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,15 +25,7 @@
     <link rel="preconnect" href="{{ config('medialibrary.s3.domain') }}" crossorigin>
     <link rel="dns-prefetch" href="{{ config('medialibrary.s3.domain') }}">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
-    <link rel="shortcut icon" href="{{ asset('img/favicon/favicon.ico') }}">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="{{ asset('img/favicon/mstile-144x144.png') }}">
-    <meta name="msapplication-config" content="{{ asset('img/favicon/browserconfig.xml') }}">
-    <meta name="theme-color" content="#ffffff">
+    @include('layouts.partials._favicon')
 
     <!-- Styles -->
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
