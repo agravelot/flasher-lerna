295bb28cfcc8b5509ede190bc87198c3a6805007
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-22 13:51:04 +0200

    use str limit helper

--- app/Http/OpenGraphs/HomeOpenGraph.php
+++ app/Http/OpenGraphs/HomeOpenGraph.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\OpenGraphs;
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Collection;
 use App\Http\OpenGraphs\Contracts\OpenGraphable;
 use App\Http\OpenGraphs\Contracts\ImagesOpenGraphable;
@@ -23,7 +24,7 @@ class HomeOpenGraph implements OpenGraphable, ImagesOpenGraphable
 
     public function description(): string
     {
-        return settings()->get('seo_description');
+        return Str::limit(settings()->get('seo_description') ?? '', 150);
     }
 
     public function type(): string
