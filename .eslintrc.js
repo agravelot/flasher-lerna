010863ce716a137fb30b370d7f2ed36a4894031c
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-23 16:23:13 +0200

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,27 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['airbnb-base', 'plugin:vue/recommended'],
+  extends: [
+      'plugin:vue/recommended',
+
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  "parser": "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+      'parser': "@typescript-eslint/parser"
   },
   plugins: [
     'vue',
+      "@typescript-eslint",
   ],
   rules: {
+      // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
