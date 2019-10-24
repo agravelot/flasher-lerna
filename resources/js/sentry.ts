27737c84de634f726ae23e384450ab5173dee6c1
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-24 09:04:52 +0200

    remove vue from frontend

--- resources/js/sentry.ts
+++ resources/js/sentry.ts
@@ -1,8 +1,5 @@
-import Vue from 'vue';
 import * as Sentry from '@sentry/browser';
-import * as Integrations from '@sentry/integrations';
 
 Sentry.init({
   dsn: process.env.MIX_SENTRY_DSN_PUBLIC,
-  integrations: [new Integrations.Vue({ Vue, attachProps: true })],
 });
