a104a0f66128c78481d61ed93dfbb0f666eb9eab
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-10-24 16:19:03 +0200

    use @endauth with @auth

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -55,7 +55,7 @@
                             </form>
                         </div>
                     </div>
-                @endguest
+                @endauth
             </div>
         </div>
     </div>
