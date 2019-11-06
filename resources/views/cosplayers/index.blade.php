0696d5cc18c01bd24d48eb18e2fa6b2bbeb94ad9
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-11-06 12:48:05 +0100

    hide coslpayer double title

--- resources/views/cosplayers/index.blade.php
+++ resources/views/cosplayers/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     @include('layouts.partials._messages')
     <div class="container is-centered">
-        <h1 class="title">{{ __('Cosplayers') }}</h1>
+        {{--<h1 class="title">{{ __('Cosplayers') }}</h1>--}}
         <div class="columns is-multiline is-centered">
             @each('cosplayers.partials._cosplayer_item', $cosplayers, 'cosplayer', 'layouts.partials._empty')
         </div>
