88b7d5a18d3803b01fe4f370e818c4b24fab4c7c
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-11-09 02:38:54 +0100

    add column in contact page

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -5,87 +5,73 @@
 @section('content')
     <section class="section">
         <div class="container">
-            <div class="card-content">
-                @include('layouts.partials._messages')
+            <div class="columns">
+                <div class="column">
 
-                <form method="POST" action="{{ route('contact.store') }}">
-                    @csrf
+                    <h2 class="title is-3">Comment me contacter</h2>
 
-                    <div class="field is-horizontal">
-                        <div class="field-label">
+                    <div class="content">
+                        <p>
+                            Par téléphone au <a href="tel:0766648588">07 66 64 85 88</a>.
+                        </p>
+                        <p>
+                            Ou bien en remplissant le formulaire de contact.
+                        </p>
+                    </div>
+
+                </div>
+                <div class="column">
+                    <h2 class="title is-3">Formulaire de contact</h2>
+                    @include('layouts.partials._messages')
+
+                    <form method="POST" action="{{ route('contact.store') }}">
+                        @csrf
+
+                        <div class="field">
                             <label class="label">{{ __('Name') }}</label>
-                        </div>
-                        <div class="field-body">
-                            <div class="field">
-                                <div class="control">
-                                    <input class="input" id="name" type="text" name="name" value="{{ old('name') }}"
-                                           placeholder="{{ __('Name') }}" required autofocus>
-                                </div>
-                                @include('layouts.partials._form_errors', ['data' => 'name'])
+                            <div class="control">
+                                <input class="input" id="name" type="text" name="name" value="{{ old('name') }}"
+                                       placeholder="{{ __('Name') }}" required autofocus>
                             </div>
+                            @include('layouts.partials._form_errors', ['data' => 'name'])
                         </div>
-                    </div>
 
-                    <div class="field is-horizontal">
-                        <div class="field-label">
+                        <div class="field">
                             <label class="label">{{ __('Email') }}</label>
-                        </div>
-                        <div class="field-body">
-                            <div class="field">
-                                <div class="control">
-                                    <input class="input" id="email" type="text" name="email" value="{{ old('email') }}"
-                                           placeholder="{{ __('Email') }}" required autofocus>
-                                </div>
-                                @include('layouts.partials._form_errors', ['data' => 'email'])
+                            <div class="control">
+                                <input class="input" id="email" type="text" name="email"
+                                       value="{{ old('email') }}"
+                                       placeholder="{{ __('Email') }}" required autofocus>
                             </div>
+                            @include('layouts.partials._form_errors', ['data' => 'email'])
                         </div>
-                    </div>
 
-                    <div class="field is-horizontal">
-                        <div class="field-label">
+                        <div class="field">
                             <label class="label">{{ __('Message') }}</label>
-                        </div>
-                        <div class="field-body">
-                            <div class="field">
-                                <div class="control">
+                            <div class="control">
                                 <textarea class="textarea" id="message" type="text" name="message"
                                           rows="10"
                                           placeholder="{{ __('Your message here') }}">{{ old('message') }}</textarea>
-                                </div>
-                                @include('layouts.partials._form_errors', ['data' => 'message'])
                             </div>
+                            @include('layouts.partials._form_errors', ['data' => 'message'])
                         </div>
-                    </div>
 
-                    <div class="field is-horizontal">
-                        <div class="field-label">
-                            <!-- Left empty for spacing -->
-                        </div>
-                        <div class="field-body">
-                            <div class="field">
-                                <div class="control">
-                                    {!! NoCaptcha::display() !!}
-                                </div>
-                                @include('layouts.partials._form_errors', ['data' => 'g-recaptcha-response'])
+                        <div class="field">
+                            <div class="control">
+                                {!! NoCaptcha::display() !!}
                             </div>
+                            @include('layouts.partials._form_errors', ['data' => 'g-recaptcha-response'])
                         </div>
-                    </div>
 
-                    <div class="field is-horizontal">
-                        <div class="field-label">
-                            <!-- Left empty for spacing -->
-                        </div>
-                        <div class="field-body">
-                            <div class="field">
-                                <div class="control">
-                                    <button class="button is-primary">
-                                        {{ __('Send') }}
-                                    </button>
-                                </div>
+                        <div class="field">
+                            <div class="control">
+                                <button class="button is-primary">
+                                    {{ __('Send') }}
+                                </button>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </section>
