4328387c7f3077bb0ace7127cd4bc7368b702561
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-11-09 02:38:39 +0100

    update coplsayer layout

--- resources/views/cosplayers/index.blade.php
+++ resources/views/cosplayers/index.blade.php
@@ -3,11 +3,13 @@
 @section('pageTitle', __('Cosplayers'))
 
 @section('content')
-    @include('layouts.partials._messages')
-    <div class="container is-centered">
-        {{--<h1 class="title">{{ __('Cosplayers') }}</h1>--}}
-        <div class="columns is-multiline is-centered">
-            @each('cosplayers.partials._cosplayer_item', $cosplayers, 'cosplayer', 'layouts.partials._empty')
+    <section class="section">
+        @include('layouts.partials._messages')
+        <div class="container">
+            <div class="columns is-multiline is-centered">
+                @each('cosplayers.partials._cosplayer_item', $cosplayers, 'cosplayer', 'layouts.partials._empty')
+            </div>
+            {{ $cosplayers->links() }}
         </div>
-    </div>
+    </section>
 @endsection
