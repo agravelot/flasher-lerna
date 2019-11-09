37317734d74c01db2caed30fcfa6e71a25f8bf36
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-11-09 12:29:12 +0100

    fix cover if the first media

--- tests/Unit/Models/AlbumTest.php
+++ tests/Unit/Models/AlbumTest.php
@@ -4,8 +4,10 @@ namespace Tests\Unit\Models;
 
 use Carbon\Carbon;
 use App\Models\Album;
+use App\Models\Media;
 use Tests\ModelTestCase;
 use App\Scope\PublicScope;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -13,7 +15,7 @@ class AlbumTest extends ModelTestCase
 {
     use WithFaker, RefreshDatabase;
 
-    public function testRouteKeyName()
+    public function testRouteKeyName(): void
     {
         $album = new Album();
         $slug = $this->faker->slug;
@@ -23,7 +25,7 @@ class AlbumTest extends ModelTestCase
         $this->assertSame($$routeKey, $slug);
     }
 
-    public function testSlugAsRouteKeyName()
+    public function testSlugAsRouteKeyName(): void
     {
         $album = new Album();
         $routeKey = $album->getRouteKeyName();
@@ -33,7 +35,7 @@ class AlbumTest extends ModelTestCase
         $this->assertSame($excepted, $routeKey);
     }
 
-    public function testSlugSourceAsTitle()
+    public function testSlugSourceAsTitle(): void
     {
         $album = new Album();
         $slugSource = $album->sluggable()['slug']['source'];
@@ -43,7 +45,7 @@ class AlbumTest extends ModelTestCase
         $this->assertSame($excepted, $slugSource);
     }
 
-    public function testModelConfiguration()
+    public function testModelConfiguration(): void
     {
         $this->runConfigurationAssertions(new Album(), [
             'title', 'slug', 'body', 'published_at', 'user_id', 'private', 'notify_users_on_published',
@@ -54,7 +56,7 @@ class AlbumTest extends ModelTestCase
         ]);
     }
 
-    public function testBelongsToManyAlbumsRelationship()
+    public function testBelongsToManyAlbumsRelationship(): void
     {
         $album = new Album();
         $relation = $album->cosplayers();
@@ -62,35 +64,35 @@ class AlbumTest extends ModelTestCase
         $this->assertBelongsToManyRelation($relation, $album, new Album(), 'album_id');
     }
 
-    public function test_album_is_public()
+    public function test_album_is_public(): void
     {
         $album = factory(Album::class)->states(['published', 'passwordLess'])->make();
 
         $this->assertTrue($album->isPublic());
     }
 
-    public function test_album_is_private_when_not_published()
+    public function test_album_is_private_when_not_published(): void
     {
         $album = factory(Album::class)->states(['unpublished', 'passwordLess'])->make();
 
         $this->assertFalse($album->isPublic());
     }
 
-    public function test_album_is_private_when_password_is_defined()
+    public function test_album_is_private_when_password_is_defined(): void
     {
         $album = factory(Album::class)->states(['published', 'password'])->make();
 
         $this->assertFalse($album->isPublic());
     }
 
-    public function test_album_is_private_when_password_is_defined_and_not_published()
+    public function test_album_is_private_when_password_is_defined_and_not_published(): void
     {
         $album = factory(Album::class)->states(['unpublished', 'password'])->make();
 
         $this->assertFalse($album->isPublic());
     }
 
-    public function test_album_without_password_is_passwordLess()
+    public function test_album_without_password_is_passwordLess(): void
     {
         /** @var Album $album */
         $album = factory(Album::class)->states(['passwordLess'])->make();
@@ -98,7 +100,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($album->isPasswordLess());
     }
 
-    public function test_album_with_password_is_not_passwordLess()
+    public function test_album_with_password_is_not_passwordLess(): void
     {
         /** @var Album $album */
         $album = factory(Album::class)->states(['password'])->make();
@@ -106,7 +108,7 @@ class AlbumTest extends ModelTestCase
         $this->assertFalse($album->isPasswordLess());
     }
 
-    public function test_set_published_at_with_true_is_now()
+    public function test_set_published_at_with_true_is_now(): void
     {
         $knownDate = Carbon::create(2018, 5, 21, 12);
         Carbon::setTestNow($knownDate);
@@ -115,7 +117,7 @@ class AlbumTest extends ModelTestCase
         $this->assertSame($knownDate->format('Y-m-d H:i:s'), $album->published_at->format('Y-m-d H:i:s'));
     }
 
-    public function test_set_published_at_with_date()
+    public function test_set_published_at_with_date(): void
     {
         $knownDate = Carbon::create(2018, 5, 21, 12);
         Carbon::setTestNow($knownDate);
@@ -125,7 +127,7 @@ class AlbumTest extends ModelTestCase
         $this->assertSame($knownDate->format('Y-m-d H:i:s'), $album->published_at->format('Y-m-d H:i:s'));
     }
 
-    public function test_album_with_a_published_at_date_are_published()
+    public function test_album_with_a_published_at_date_are_published(): void
     {
         $publishedAlbums = factory(Album::class, 2)->states(['published', 'withUser'])->create();
 
@@ -136,7 +138,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($publishedAlbums->get(1)));
     }
 
-    public function test_album_with_unpublished_are_not_visible()
+    public function test_album_with_unpublished_are_not_visible(): void
     {
         factory(Album::class, 2)->states(['unpublished', 'passwordLess', 'withUser'])->create();
 
@@ -147,7 +149,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($albums->get(1)));
     }
 
-    public function test_album_with_a_password_are_not_visible()
+    public function test_album_with_a_password_are_not_visible(): void
     {
         factory(Album::class, 2)->states(['published', 'password', 'withUser'])->create();
 
@@ -158,7 +160,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($albums->get(1)));
     }
 
-    public function test_album_with_a_published_at_date_and_password_are_unpublished()
+    public function test_album_with_a_published_at_date_and_password_are_unpublished(): void
     {
         factory(Album::class, 2)->states(['published', 'password', 'withUser'])->create();
 
@@ -167,7 +169,7 @@ class AlbumTest extends ModelTestCase
         $this->assertSame(2, $albums->count());
     }
 
-    public function test_count_four_published_albums_should_be_four()
+    public function test_count_four_published_albums_should_be_four(): void
     {
         $publishedAlbums = factory(Album::class, 4)->states(['published', 'passwordLess', 'withUser'])->create();
 
@@ -180,7 +182,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($publishedAlbums->get(3)));
     }
 
-    public function test_count_four_unpublished_albums_should_be_zero()
+    public function test_count_four_unpublished_albums_should_be_zero(): void
     {
         factory(Album::class, 4)->states(['unpublished', 'passwordLess', 'withUser'])->create();
 
@@ -193,7 +195,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($albums->get(3)));
     }
 
-    public function test_album_with_a_published_at_date_are_published_without_public_criteria()
+    public function test_album_with_a_published_at_date_are_published_without_public_criteria(): void
     {
         $publishedAlbums = factory(Album::class, 2)->states(['published', 'withUser'])->create();
 
@@ -203,7 +205,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($publishedAlbums->get(1)));
     }
 
-    public function test_album_with_password_are_unpublished_without_public_criteria()
+    public function test_album_with_password_are_unpublished_without_public_criteria(): void
     {
         $publishedAlbums = factory(Album::class, 2)->states(['unpublished', 'withUser'])->create();
 
@@ -213,7 +215,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($publishedAlbums->get(1)));
     }
 
-    public function test_album_with_a_published_at_date_and_password_are_unpublished_without_public_criteria()
+    public function test_album_with_a_published_at_date_and_password_are_unpublished_without_public_criteria(): void
     {
         $publishedAlbums = factory(Album::class, 2)->states(['published', 'password', 'withUser'])->create();
 
@@ -223,7 +225,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($publishedAlbums->get(1)));
     }
 
-    public function test_count_four_published_albums_should_be_four_without_public_criteria()
+    public function test_count_four_published_albums_should_be_four_without_public_criteria(): void
     {
         $publishedAlbums = factory(Album::class, 4)->states(['published', 'passwordLess', 'withUser'])->create();
 
@@ -235,7 +237,7 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($publishedAlbums->get(3)));
     }
 
-    public function test_count_four_unpublished_albums_should_be_zero_without_public_criteria()
+    public function test_count_four_unpublished_albums_should_be_zero_without_public_criteria(): void
     {
         $publishedAlbums = factory(Album::class, 4)->states(['unpublished', 'passwordLess', 'withUser'])->create();
 
@@ -245,5 +247,20 @@ class AlbumTest extends ModelTestCase
         $this->assertTrue($albums->contains($publishedAlbums->get(1)));
         $this->assertTrue($albums->contains($publishedAlbums->get(2)));
         $this->assertTrue($albums->contains($publishedAlbums->get(3)));
+    }
+
+    public function test_mage_first_in_is_the_album_cover(): void
+    {
+        /** @var Album $album */
+        $album = factory(Album::class)->states('withUser')->create();
+        $exceptedName = 'shouldBeTheCover.jpg';
+
+        $album->addPicture(UploadedFile::fake()->image($exceptedName));
+        $album->addPicture(UploadedFile::fake()->image('second.jpg'));
+        $album->addPicture(UploadedFile::fake()->image('last.jpg'));
+
+        /** @var Media $cover */
+        $cover = $album->cover;
+        $this->assertSame($exceptedName, $cover->name.'.'.$cover->extension);
     }
 }
