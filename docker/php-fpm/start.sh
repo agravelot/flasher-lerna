af33de2ef25e3d1b182d02e3e159a5c5d6afef37
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-11-17 03:07:59 +0100

    php-fpm: optimize for all hp containers

--- docker/php-fpm/start.sh
+++ docker/php-fpm/start.sh
@@ -6,24 +6,35 @@ role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-production}
 cd /var/www/html
 
-if [[ "$role" = "app" ]]; then
-
-    echo "App role"
-    exec php-fpm
-
-elif [[ "$role" = "queue" ]]; then
-
-    echo "Queue role"
-    exec php artisan horizon
-
-elif [[ "$role" = "scheduler" ]]; then
-
-    echo "Scheduler role"
-    exec crond -f -c /etc/crontabs -d 8
-
-elif [[ "$role" = "publisher" ]]; then
+if [[ "$env" != "local" ]]; then
   php artisan db:wait-connection
   php artisan cache:clear-wait-connection
+  # Optimizing for production
+  # https://laravel.com/docs/6.0/deployment#optimization
+  echo "Caching configuration..."
+  php artisan view:clear
+  php artisan optimize
+  php artisan view:cache
+  php artisan event:cache
+fi
+
+if [[ "$role" == "app" ]]; then
+
+  echo "App role"
+  exec php-fpm
+
+elif [[ "$role" == "queue" ]]; then
+
+  echo "Queue role"
+  exec php artisan horizon
+
+elif [[ "$role" == "scheduler" ]]; then
+
+  echo "Scheduler role"
+  exec crond -f -c /etc/crontabs -d 8
+
+elif [[ "$role" == "publisher" ]]; then
+
   php artisan migrate --force
   php artisan passport:keys
 
@@ -31,16 +42,7 @@ elif [[ "$role" = "publisher" ]]; then
   # php artisan telescope:publish
   php artisan horizon:assets
 
-  if [[ "$env" != "local" ]]; then
-      # Optimizing for production
-      # https://laravel.com/docs/5.8/deployment#optimization
-      echo "Caching configuration..."
-      php artisan view:clear
-      php artisan optimize
-      php artisan view:cache
-      php artisan event:cache
-  fi
 else
-    echo "Could not match the container role \"$role\""
-    exit 1
+  echo "Could not match the container role \"$role\""
+  exit 1
 fi
