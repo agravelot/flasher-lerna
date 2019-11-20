7cdf64e74f5dbfb794832b1d861c7f7689903a0d
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-11-20 22:27:32 +0100

    make hmr working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 @section('js')
-    {{--<script src="{{ mix('/js/main/manifest.js') }}"></script>--}}
-    {{--<script src="{{ mix('/js/main/vendor.js') }}"></script>--}}
-    <script src="{{ mix('/js/main/app.js') }}"></script>
+    {{--<script src="{{ mix('/js/manifest.js') }}"></script>--}}
+    {{--<script src="{{ mix('/js/vendor.js') }}"></script>--}}
+    <script src="{{ mix('js/app.js') }}"></script>
 @endsection
 
 <!DOCTYPE html>
