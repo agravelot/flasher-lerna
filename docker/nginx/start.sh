d2c3dc060a83eeaa7f1f2a909dd1defb075dc4ea
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-11-29 11:04:40 +0100

    only envsubst given vars

--- docker/nginx/start.sh
+++ docker/nginx/start.sh
@@ -5,12 +5,14 @@ set -o pipefail  # trace ERR through pipes
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-# Update cnginx config from environments
-cat /etc/nginx/nginx.conf | envsubst \$ERROR_LOG_LEVEL > /etc/nginx/nginx.conf
-cat /etc/nginx/sites-enabled/default.conf | envsubst \$NGINX_HOST > /etc/nginx/sites-enabled/default.conf
+# Update nginx config from environments
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.template
+mv /etc/nginx/sites-enabled/default.conf /etc/nginx/sites-enabled/default.template
+envsubst \$ERROR_LOG_LEVEL < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+envsubst \$NGINX_HOST < /etc/nginx/sites-enabled/default.template > /etc/nginx/sites-enabled/default.conf
 
-cat /etc/nginx/nginx.conf /etc/nginx/sites-enabled/default.conf
-
+cat /etc/nginx/nginx.conf
+cat /etc/nginx/sites-enabled/default.conf
 # Validate nginx configuraition
 nginx -t
 
