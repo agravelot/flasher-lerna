cedfe1a0c2877e8af8bb7bdeb225444b2251548d
Antoine Gravelot <antoine?gravelot@hotmail.fr>
2019-12-29 22:00:22 +0100

    remove old reset password header

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,17 +4,6 @@
 
 @section('content')
 
-    <section class="hero is-primary">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
-                    {{ __('Reset Password') }}
-                </h1>
-            </div>
-        </div>
-    </section>
-
-
     <div class="columns is-marginless is-centered">
         <div class="column is-5">
             <div class="card">
