2da7ec4ff8f76894a5cdc0981bb2528829db0012
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2019-12-30 14:46:56 +0100

    remove old depencies from lock and import

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,7 +24,6 @@ use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Routing\Middleware\ValidateSignature;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Inspector\Laravel\Middleware\WebRequestMonitoring;
 use Laravel\Passport\Http\Middleware\CreateFreshApiToken;
 use Spatie\ResponseCache\Middlewares\DoNotCacheResponse;
 
