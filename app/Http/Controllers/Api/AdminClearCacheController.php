b1efd7b69f73a85ae295fdb6249948cd4b8d1f69
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2020-01-07 12:35:44 +0100

    api: add route cache clear route

---
+++ app/Http/Controllers/Api/AdminClearCacheController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Spatie\ResponseCache\Facades\ResponseCache;
+
+class AdminClearCacheController extends Controller
+{
+    public function __invoke(): JsonResponse
+    {
+        ResponseCache::clear();
+
+        return new JsonResponse();
+    }
+}
