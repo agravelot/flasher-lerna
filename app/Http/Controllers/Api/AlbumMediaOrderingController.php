8553f70b9dea76fd96427594770de1f5e5586e93
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-01-24 15:00:25 +0100

    clear cache on album media re-order
    Signed-off-by: Antoine GRAVELOT <antoine.gravelot@hotmail.fr>

--- app/Http/Controllers/Api/AlbumMediaOrderingController.php
+++ app/Http/Controllers/Api/AlbumMediaOrderingController.php
@@ -7,12 +7,14 @@ use App\Http\Requests\AlbumMediaOrderingRequest;
 use App\Http\Resources\AlbumShowResource;
 use App\Models\Album;
 use App\Models\Media;
+use Spatie\ResponseCache\Facades\ResponseCache;
 
 class AlbumMediaOrderingController extends Controller
 {
     public function __invoke(Album $album, AlbumMediaOrderingRequest $request): AlbumShowResource
     {
         Media::setNewOrder($request->media_ids);
+        ResponseCache::clear();
 
         return new AlbumShowResource($album);
     }
