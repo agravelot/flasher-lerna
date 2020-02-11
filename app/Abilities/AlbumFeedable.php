191cd2c5362c0a8133a424958f6f42275b414b9e
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2020-02-11 16:34:14 +0100

    add meta description in rss

--- app/Abilities/AlbumFeedable.php
+++ app/Abilities/AlbumFeedable.php
@@ -11,7 +11,7 @@ trait AlbumFeedable
         return FeedItem::create()
             ->id($this->id)
             ->title($this->title)
-            ->summary('')
+            ->summary($this->meta_description)
             ->updated($this->updated_at)
             ->link(route('albums.show', ['slug' => $this->slug]))
             ->author($this->user->name);
