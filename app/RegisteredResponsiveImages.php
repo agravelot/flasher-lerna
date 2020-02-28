722b19a30c10ac28356900977851d31c80e189c2
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-02-28 11:08:04 +0100

    rework with config

--- app/RegisteredResponsiveImages.php
+++ app/RegisteredResponsiveImages.php
@@ -7,32 +7,35 @@ namespace App;
 use Spatie\MediaLibrary\ResponsiveImages\RegisteredResponsiveImages as RegisteredResponsiveImagesBase;
 use Spatie\MediaLibrary\ResponsiveImages\ResponsiveImage;
 
-class RegisteredResponsiveImages extends RegisteredResponsiveImagesBase {
+class RegisteredResponsiveImages extends RegisteredResponsiveImagesBase
+{
 
     public function getSrcset(): string
     {
-        $url = 'https://d11vat99ofzpe5.cloudfront.net/';
+        $url = config('medialibrary.s3.domain');
+        //$url = '';
 
         $filesSrcset = $this->files
             ->map(function (ResponsiveImage $responsiveImage) use ($url) {
+                $path = $this->media->getPath();
+                if ($root = config('filesystems.disks.'.$this->media->disk.'.root')) {
+                    $path = $root.'/'.$path;
+                }
 
-                $data = [
-                    'bucket' => 'assets.jkanda.fr',
-                    'key' => 'local/'.$responsiveImage->media->getPath(),
-                    'edits' => [
-                        'normalize' => true,
-                        'sharpen' => true,
-                        'resize' => [
-                            'width' => $responsiveImage->width(),
+                $encodedData = base64_encode(json_encode(
+                    $data = [
+                        'bucket' => 'assets.jkanda.fr',
+                        'key' => $path,
+                        'edits' => [
+                            'normalize' => true,
+                            'sharpen' => true,
+                            'resize' => [
+                                'width' => $responsiveImage->width(),
+                            ],
                         ],
-                    ],
-                ];
-                $encodedData = base64_encode(json_encode($data));
+                    ]));
 
-                \Log::debug($encodedData);
-                \Log::debug(base64_decode($encodedData));
-
-                return "${url}${encodedData} {$responsiveImage->width()}w";
+                return "${url}/${encodedData} {$responsiveImage->width()}w";
             })
             ->implode(', ');
 
