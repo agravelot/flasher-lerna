128158438fd2d9c66707c084ef9b53da392c97dc
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-02-28 22:59:53 +0100

    update cosplayers views

--- resources/views/cosplayers/partials/_cosplayer_item.blade.php
+++ resources/views/cosplayers/partials/_cosplayer_item.blade.php
@@ -1,18 +1,18 @@
 <div class="column is-half-tablet is-one-third-widescreen">
     <a href="{{ route('cosplayers.show', compact('cosplayer')) }}">
-        <div class="card large">
-            <div class="card-image">
-                <figure class="image">
-                    {{ $cosplayer->getFirstMedia('avatar') }}
-                </figure>
-            </div>
-            <div class="card-content">
-                <div class="media">
-                    <div class="media-content">
+        <div class="box">
+            <article class="media">
+                <div class="media-left">
+                    <figure class="image is-64x64">
+                        {{ $cosplayer->getFirstMedia('avatar') }}
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <div class="content">
                         <h2 class="subtitle is-5">{{ $cosplayer->name }}</h2>
                     </div>
                 </div>
-            </div>
+            </article>
         </div>
     </a>
 </div>
