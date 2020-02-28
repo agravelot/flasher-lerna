2511fb4233ccdf2baabf1c19e6daaa238864415f
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-02-28 23:15:16 +0100

    remove size definitions

--- resources/views/cosplayers/partials/_cosplayer_avatar.blade.php
+++ resources/views/cosplayers/partials/_cosplayer_avatar.blade.php
@@ -1,5 +1,5 @@
 @if ($cosplayer->avatar)
-    <figure class="is-centered image is-64x64">
+    <figure class="is-centered image">
         {{ $cosplayer->getFirstMedia('avatar')('thumb', ['class' => 'is-rounded']) }}
     </figure>
 @else
