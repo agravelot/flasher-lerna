cf44d99e408aa80a698bb31c24f1d7d3a4593a5d
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-02-29 12:55:11 +0100

    add responsive lazy class

--- resources/views/vendor/medialibrary/responsiveImage.blade.php
+++ resources/views/vendor/medialibrary/responsiveImage.blade.php
@@ -1,2 +1,2 @@
-<img{!! $attributeString !!} srcset="{{ $media->getSrcset($conversion) }}" src="{{ $media->getUrl($conversion) }}" width="{{ $width }}" alt="{{ $media->model->title ?? $media->name }}">
+<img{!! $attributeString !!} class="responsive-media responsive-media-lazy" srcset="{{ $media->getSrcset($conversion) }}" src="{{ $media->getUrl($conversion) }}" width="{{ $width }}" alt="{{ $media->model->title ?? $media->name }}">
 {!! new \App\Http\SchemasOrg\MediaSchema($media) !!}
