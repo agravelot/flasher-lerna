7d76b98673086c2d74e667e473c650ddbd416900
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-02-29 17:57:23 +0100

    update favicon

--- resources/views/layouts/partials/_favicon.blade.php
+++ resources/views/layouts/partials/_favicon.blade.php
@@ -1,9 +1,9 @@
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
-<link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
-<link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="{{ asset('favicon/mstile-144x144.png') }}">
-<meta name="msapplication-config" content="{{ asset('favicon/browserconfig.xml') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png?v=dLnYvW4506">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png?v=dLnYvW4506">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png?v=dLnYvW4506">
+<link rel="manifest" href="/favicon/site.webmanifest?v=dLnYvW4506">
+<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg?v=dLnYvW4506" color="#3d3d3d">
+<link rel="shortcut icon" href="/favicon/favicon.ico?v=dLnYvW4506">
+<meta name="msapplication-TileColor" content="#000000">
+<meta name="msapplication-config" content="/favicon/browserconfig.xml?v=dLnYvW4506">
 <meta name="theme-color" content="#000000">
