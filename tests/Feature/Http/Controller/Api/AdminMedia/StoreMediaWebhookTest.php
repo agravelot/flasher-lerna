6f0abb0ef7ecd329e52d10c04966890a9ee85666
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-03-22 00:28:17 +0100

    rework tus webhook

--- tests/Feature/Http/Controller/Api/AdminMedia/StoreMediaWebhookTest.php
+++ tests/Feature/Http/Controller/Api/AdminMedia/StoreMediaWebhookTest.php
@@ -10,7 +10,7 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
-class StoreAdminAlbumMediaWebhookTest extends TestCase
+class StoreMediaWebhookTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
@@ -31,11 +31,11 @@ class StoreAdminAlbumMediaWebhookTest extends TestCase
         $exceptedFileName = "$album->slug.jpeg";
         Storage::disk('s3')->putFileAs($path, UploadedFile::fake()->image($exceptedFileName), $exceptedFileName);
 
-        $response = $this->sendMediaAddedWebhook($exceptedFileName, $album->id, );
+        $response = $this->sendMediaAddedWebhook($exceptedFileName, $album->id);
 
         $response->assertCreated()
-            ->assertJsonPath('data.mime_type', 'image/jpeg')
-            ->assertJsonPath('data.name', $album->slug.'.jpeg');
+            ->assertJsonPath('data.mime_type', 'image/png')
+            ->assertJsonPath('data.name', 'filename.png');
         //->assertJsonPath('data.path', '');
         $this->assertCount(1, $album->fresh()->media);
     }
@@ -48,21 +48,21 @@ class StoreAdminAlbumMediaWebhookTest extends TestCase
         $response = $this->sendMediaAddedWebhook('/image.png', 4242);
 
         $response->assertStatus(422)
-            ->assertJsonValidationErrors('album_id');
+            ->assertJsonValidationErrors('Upload.MetaData.modelId');
     }
 
-    /** @test */
-    public function send_webhook_with_invalid_url_will_return_422(): void
-    {
-        $this->actingAsAdmin();
-        /** @var Album $album */
-        $album = factory(Album::class)->create();
-
-        $response = $this->sendMediaAddedWebhook('nonValidName', $album->id);
-
-        $response->assertStatus(422)
-            ->assertJsonValidationErrors('media_name');
-    }
+//    /** @test */
+//    public function send_webhook_with_invalid_url_will_return_422(): void
+//    {
+//        $this->actingAsAdmin();
+//        /** @var Album $album */
+//        $album = factory(Album::class)->create();
+//
+//        $response = $this->sendMediaAddedWebhook('nonValidName', $album->id);
+//
+//        $response->assertStatus(422)
+//            ->assertJsonValidationErrors('media_name');
+//    }
 
     /** @test */
     public function user_cannot_send_webhook(): void
@@ -88,8 +88,30 @@ class StoreAdminAlbumMediaWebhookTest extends TestCase
     private function sendMediaAddedWebhook(string $mediaName, string $albumId): TestResponse
     {
         return $this->postJson('/api/admin/album-media-added', [
-            'media_name' => $mediaName,
-            'album_id' => $albumId,
+            'Upload' => [
+                'ID' => 'azeaze',
+                'Size' => 123,
+                'Offset' => 123,
+                'IsFinal' => true,
+                'IsPartial' => true,
+                'MetaData' => [
+                    'filename' => 'filename.png',
+                    'modelClass' => 'album',
+                    'modelId' => $albumId,
+                ],
+                'Storage' => [
+                    'Type' => 'filestore',
+                    'Path' => '/svr/files/14b1c4c77771671a8479bc0444bbc5ce',
+                ],
+            ],
+            'HTTPRequest' => [
+                'Method' => 'POST',
+                'URI' => '/600x400/000/fff',
+                'RemoteAddr' => 'https://dummyimage.com',
+                'Header' => [
+                    'test' => '',
+                ],
+            ],
         ]);
     }
 }
