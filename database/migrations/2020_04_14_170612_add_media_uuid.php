bf1b75b46106a86ef33eacc47c07e46703488c93
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-04-14 19:57:12 +0200

    update migration

--- database/migrations/2020_04_14_170612_add_media_uuid.php
+++ database/migrations/2020_04_14_170612_add_media_uuid.php
@@ -2,17 +2,20 @@
 
 use App\Models\Media;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Str;
 
 class AddMediaUuid extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
+        Schema::table('media', static function (Blueprint $table) {
+            $table->uuid('uuid')->nullable();
+            $table->string('conversions_disk')->nullable();
+        });
         Media::cursor()->each(
             static fn (Media $media) => $media->update(['uuid' => Str::uuid()])
         );
@@ -20,10 +23,8 @@ class AddMediaUuid extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         //
     }
