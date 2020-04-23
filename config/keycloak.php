be981dd3524c478b7cd520374a4e9e38064de510
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-04-23 23:00:20 +0200

    update keycloak config

--- config/keycloak.php
+++ config/keycloak.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-  'realm_public_key' => env('KEYCLOAK_REALM_PUBLIC_KEY', 'flasher'),
+  'realm_public_key' => env('KEYCLOAK_REALM_PUBLIC_KEY', null),
 
   'load_user_from_database' => env('KEYCLOAK_LOAD_USER_FROM_DATABASE', true),
 
-  'user_provider_credential' => env('KEYCLOAK_USER_PROVIDER_CREDENTIAL', 'username'),
+  'user_provider_credential' => env('KEYCLOAK_USER_PROVIDER_CREDENTIAL', 'name'),
 
   'token_principal_attribute' => env('KEYCLOAK_TOKEN_PRINCIPAL_ATTRIBUTE', 'preferred_username'),
 
-  'append_decoded_token' => env('KEYCLOAK_APPEND_DECODED_TOKEN', false),
+  'append_decoded_token' => env('KEYCLOAK_APPEND_DECODED_TOKEN', true),
 
-  'allowed_resources' => env('KEYCLOAK_ALLOWED_RESOURCES', null),
+  'allowed_resources' => env('KEYCLOAK_ALLOWED_RESOURCES', 'account'),
 ];
