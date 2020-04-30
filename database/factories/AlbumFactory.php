32d8945aa20da9445aa3cefb12553f89dc2f4018
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-04-30 23:55:47 +0200

    shorten akbul mete desc on factory to avoid validation errors

--- database/factories/AlbumFactory.php
+++ database/factories/AlbumFactory.php
@@ -15,7 +15,7 @@ $factory->define(Album::class, static function (Faker $faker) use (&$withMedias)
 
     return [
         'title' => $faker->sentence,
-        'meta_description' => $faker->text(154),
+        'meta_description' => $faker->text(120),
         'body' => $faker->randomHtml($faker->numberBetween(2, 6)),
         'published_at' => null,
         'private' => $faker->boolean,
