ce8ba0c04b5aebf1aebd65690d044d12bbbc8cf9
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-04-30 02:32:29 +0200

    disable keycloak provider for now

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         // App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
-        App\Providers\KeycloakServiceProvider::class,
+        // App\Providers\KeycloakServiceProvider::class,
 
         /*
          * Package Service Providers...
