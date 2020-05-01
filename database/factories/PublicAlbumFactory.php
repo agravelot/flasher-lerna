d6719d6d45399614a4528ca88696e5678a1c4f6f
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-05-02 01:30:20 +0200

    Revert "remove duplicate factories"
    This reverts commit 8e828c4477ab0dec4ed299228652a3090ea74825.

---
+++ database/factories/PublicAlbumFactory.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Models\Album;
+use App\Models\PublicAlbum;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+
+/* @var Factory $factory */
+
+$withMedias = false;
+
+$factory->define(PublicAlbum::class, static function (Faker $faker) use (&$withMedias) {
+    $withMedias = false;
+
+    return [
+        'title' => $faker->sentence,
+        'meta_description' => $faker->text(254),
+        'body' => $faker->paragraph($faker->numberBetween(42, 420)),
+        'sso_id' => \App\Facades\Keycloak::users()->all()[0]->id,
+        'published_at' => Carbon::now(),
+        'created_at' => Carbon::now(),
+        'private' => false,
+    ];
+});
+
+$factory->afterMaking(PublicAlbum::class, static function (Album $album, Faker $faker) use (&$withMedias) {
+    if ($withMedias) {
+        foreach (range(1, 5) as $i) {
+            $album->addMediaFromUrl($faker->imageUrl(640 * $faker->numberBetween(1, 3), 480 * $faker->numberBetween(1, 3)))
+                ->toMediaCollection(Album::PICTURES_COLLECTION);
+        }
+    }
+});
+
+$factory->state(PublicAlbum::class, 'withMedias', static function () use (&$withMedias) {
+    $withMedias = true;
+
+    return [];
+});
+
+$factory->state(PublicAlbum::class, 'published', static function () {
+    return [
+        'published_at' => Carbon::now(),
+    ];
+});
+
+$factory->state(PublicAlbum::class, 'unpublished', static function () {
+    return [
+        'published_at' => null,
+    ];
+});
+
+$factory->state(PublicAlbum::class, 'password', static function () {
+    return [
+        'private' => true,
+    ];
+});
+
+$factory->state(PublicAlbum::class, 'passwordLess', static function () {
+    return [
+        'private' => false,
+    ];
+});
+
+$factory->state(PublicAlbum::class, 'withUser', static function () {
+    return [
+        'sso_id' => Str::uuid(),
+    ];
+});
