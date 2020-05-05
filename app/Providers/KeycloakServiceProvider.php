cbda2f2807115d7552655366c0aaa18a88eef97e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-05-05 16:40:55 +0200

    remove boot from keycloak provider

--- app/Providers/KeycloakServiceProvider.php
+++ app/Providers/KeycloakServiceProvider.php
@@ -16,28 +16,4 @@ class KeycloakServiceProvider extends ServiceProvider
             return new Keycloak();
         });
     }
-
-    /**
-     * We sill fetch keycloak public key, and store it in cache.
-     */
-    public function boot(): void
-    {
-//        if (App::environment('testing')) {
-//            return;
-//        }
-//
-//        if (config('keycloak.realm_public_key') !== null) {
-//            return;
-//        }
-//
-//        $publicKey = Cache::get('keycloak.realm_public_key', null);
-//        if ($publicKey === null) {
-//            $protocol = config('keycloak.ssl') ? 'https' : 'http';
-//            $publicKey = Http::withOptions(['verify' => false])
-//                ->get($protocol.'://'.config('keycloak.uri').'/auth/realms/'.config('keycloak.realm'))
-//                ->json()['public_key'];
-//            Cache::add('keycloak.realm_public_key', $publicKey);
-//        }
-//        config(['keycloak.realm_public_key' => $publicKey]);
-    }
 }
