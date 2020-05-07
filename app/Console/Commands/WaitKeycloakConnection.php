2f5d818af72c1b0ddaf3acbaf40cb66c5473bc44
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-05-07 17:12:57 +0200

    enable debug on wait keycloak http calls

--- app/Console/Commands/WaitKeycloakConnection.php
+++ app/Console/Commands/WaitKeycloakConnection.php
@@ -27,7 +27,7 @@ class WaitKeycloakConnection extends Command
     public function handle(): void
     {
         $this->info('Waiting keycloak : '.config('keycloak-web.base_url'));
-        $response = Http::withOptions(['verify' => false])
+        $response = Http::withOptions(['verify' => false, 'debug' => true])
             ->retry(30, 1000)
             ->timeout(2)
             ->get(config('keycloak-web.base_url'));
