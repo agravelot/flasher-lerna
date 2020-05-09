d809bd5f1808fbefd3b0dd5cd1499133b432857d
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-05-09 22:38:03 +0200

    add search on users endpoint

--- app/Http/Controllers/Api/AdminUsersController.php
+++ app/Http/Controllers/Api/AdminUsersController.php
@@ -2,64 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\UserRequest;
-use App\Http\Resources\UserResource;
-use App\Models\User;
-use Exception;
+use App\Facades\Keycloak;
+use App\Services\Keycloak\UserQuery;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Spatie\QueryBuilder\QueryBuilder;
 
 class AdminUsersController extends Controller
 {
     /**
      * Display a listing of the resource.
-     */
-    public function index(): AnonymousResourceCollection
-    {
-        return UserResource::collection(
-            QueryBuilder::for(User::class)->allowedFilters('name')->paginate(15)
-        );
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(UserRequest $request): UserResource
-    {
-        $user = User::create($request->validated());
-
-        return new UserResource($user);
-    }
-
-    /**
-     * Show the specified resource.
-     */
-    public function show(User $user): UserResource
-    {
-        return new UserResource($user);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UserRequest $request, User $user): UserResource
-    {
-        $user->update($request->validated());
-
-        return new UserResource($user);
-    }
-
-    /**
-     * Remove the specified resource from storage.
      *
-     * @throws Exception
+     * @return JsonResponse
      */
-    public function destroy(User $user): JsonResponse
+    public function index(Request $request): JsonResponse
     {
-        $user->delete();
+        $query = new UserQuery();
+        $query->search = $request->get('search');
+        $query->max = 10;
+        $users = Keycloak::users()->all($query);
 
-        return response()->json(null, 204);
+        return new JsonResponse(['data' => $users]);
+    }
+
+
+    public function show(string $user): JsonResponse
+    {
+        return new JsonResponse(Keycloak::users()->find($user));
     }
 }
