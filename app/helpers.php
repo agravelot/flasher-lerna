61a8b9dc1bca112fde58dc87f7895dc626b08848
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-05-10 22:55:14 +0200

    remove useless helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,12 +1,5 @@
 <?php
 
-if (! function_exists('string_to_color')) {
-    function string_to_color(string $str): string
-    {
-        return '#00000';
-    }
-}
-
 if (! function_exists('settings')) {
     /**
      * Get the settings manager instance.
