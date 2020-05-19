6f00c0a363bbbb7b5e4efcafca4aea63286f416a
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-05-19 10:18:28 +0200

    do not show image if not available

--- resources/views/albums/partials/_index_item.blade.php
+++ resources/views/albums/partials/_index_item.blade.php
@@ -24,11 +24,13 @@
                     <a href="{{ route('albums.show', compact('album')) }}"
                        class="has-margin-right-md" aria-label="{{ $album->title }}">
                         <article class="card has-hover-zoom is-clipped">
-                            <div v-if="album.media" class="card-image">
-                                <figure class="image">
-                                    {{ $album->coverResponsive }}
-                                </figure>
-                            </div>
+                            @if ($album->coverResponsive)
+                                <div class="card-image">
+                                    <figure class="image">
+                                        {{ $album->coverResponsive }}
+                                    </figure>
+                                </div>
+                            @endif
                             <div class="card-content">
                                 <div class="content">
                                     <h3 class="title">{{ $album->title }}</h3>
@@ -60,11 +62,13 @@
                     <a href="{{ route('albums.show', compact('album')) }}"
                        class="has-margin-right-md" aria-label="{{ $album->title }}">
                         <article class="card has-hover-zoom is-clipped">
-                            <div v-if="album.media" class="card-image">
-                                <figure class="image">
-                                    {{ $album->coverResponsive }}
-                                </figure>
-                            </div>
+                            @if ($album->coverResponsive)
+                                <div class="card-image">
+                                    <figure class="image">
+                                        {{ $album->coverResponsive }}
+                                    </figure>
+                                </div>
+                            @endif
                             <div class="card-content">
                                 <div class="content">
                                     <h3 class="title">{{ $album->title }}</h3>
@@ -92,11 +96,13 @@
         <div class="has-margin-top-md">
             <a href="{{ route('albums.show', compact('album')) }}" aria-label="{{ $album->title }}">
                 <article class="card has-hover-zoom is-clipped">
-                    <div v-if="album.media" class="card-image">
-                        <figure class="image">
-                            {{ $album->coverResponsive }}
-                        </figure>
-                    </div>
+                    @if ($album->coverResponsive)
+                        <div class="card-image">
+                            <figure class="image">
+                                {{ $album->coverResponsive }}
+                            </figure>
+                        </div>
+                    @endif
                     <div class="card-content">
                         <div class="content">
                             <h3 class="title">{{ $album->title }}</h3>
