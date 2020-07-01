2a256c31eab60bb5b94f655098b47f7ba563b63d
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-07-02 01:30:38 +0200

    fix test

--- tests/Feature/Http/Controller/Front/Album/ShowAlbumTest.php
+++ tests/Feature/Http/Controller/Front/Album/ShowAlbumTest.php
@@ -106,7 +106,7 @@ class ShowAlbumTest extends TestCase
         $locale = App::getLocale();
 
         $response->assertSee("<meta property=\"og:title\" content=\"{$album->title}\"/>", false)
-            ->assertSee('<meta property="article:author" content="JKanda"/>')
+            ->assertSee('<meta property="article:author" content="JKanda"/>', false)
             ->assertSee('<meta property="article:section" content="Photography"/>', false)
             ->assertSee("<meta property=\"article:modified_time\" content=\"{$album->updated_at->toIso8601String()}\"/>", false)
             ->assertSee("<meta property=\"article:published_time\" content=\"{$album->published_at->toIso8601String()}\"/>", false)
@@ -128,7 +128,7 @@ class ShowAlbumTest extends TestCase
         $locale = App::getLocale();
 
         $response->assertSee("<meta property=\"og:title\" content=\"{$album->title}\"/>", false)
-            ->assertSee('<meta property="article:author" content="JKanda"/>')
+            ->assertSee('<meta property="article:author" content="JKanda"/>', false)
             ->assertSee('<meta property="article:section" content="Photography"/>', false)
             ->assertSee("<meta property=\"article:modified_time\" content=\"{$album->updated_at->toIso8601String()}\"/>", false)
             ->assertSee("<meta property=\"article:published_time\" content=\"{$album->published_at->toIso8601String()}\"/>", false)
