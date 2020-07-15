bae1354f61e87abd3b60e45536c505fe5c0bc151
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-07-15 20:51:42 +0200

    run linter

--- database/migrations/2020_07_15_171242_categorizable_must_not_have_duplicate.php
+++ database/migrations/2020_07_15_171242_categorizable_must_not_have_duplicate.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +13,7 @@ class CategorizableMustNotHaveDuplicate extends Migration
      */
     public function up(): void
     {
-        DB::table('categorizables')->get()->each(static function($categorizable) {
+        DB::table('categorizables')->get()->each(static function ($categorizable): void {
             DB::table('categorizables')->where((array) $categorizable)->delete();
             DB::table('categorizables')->insert((array) $categorizable);
         });
