071cd63e40149b6ddf48822b64078ab399929526
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-08-23 00:23:13 +0200

    add categories pagination

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
      */
     public function index(): AnonymousResourceCollection
     {
-        return CategoryResource::collection(Category::all());
+        return CategoryResource::collection(Category::paginate());
     }
 
     /**
