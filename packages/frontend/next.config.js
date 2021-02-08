a4f9d9b40a296223b19f0c527709519e96597416
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-02-08 22:30:47 +0100

    wip: only install prood deps

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,62 +1,75 @@
+const { withPlugins, optional } = require("next-compose-plugins");
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
 
+// TODO in withPuigin
 const withTM = require("next-transpile-modules")([
   "@flasher/common",
   "@flasher/models",
 ]); // pass the modules you would like to see transpiled
 
-module.exports = withTM(
-  (module.exports = withBundleAnalyzer(
-    (module.exports = withPWA({
-      async redirects() {
-        return [
-          {
-            source: "/contact",
-            destination: "/#contact",
-            permanent: true,
-          },
-          {
-            source: "/albums",
-            destination: "/albums/page/1",
-            permanent: true,
-          },
-          {
-            source: "/categories",
-            destination: "/categories/page/1",
-            permanent: true,
-          },
-          {
-            source: "/cosplayers",
-            destination: "/cosplayers/page/1",
-            permanent: true,
-          },
-        ];
+module.exports = withPlugins([
+  [
+    [
+      withTM,
+      withPWA,
+      optional(() => require("@next/bundle-analyzer")),
+      {
+        enabled: process.env.ANALYZE === "true",
       },
-      // future: { webpack5: true },
-      pwa: {
-        dest: "public",
-        runtimeCaching,
-        disable: process.env.NODE_ENV !== "production",
-        mode: process.env.NODE_ENV,
-      },
-      reactStrictMode: true,
-      env: {
-        baseUrl: "baseUrl",
-      },
-      images: {
-        domains: [
-          "assets.jkanda.fr",
-          "s3.fr-par.scw.cloud",
-          "assets-jkanda.s3.fr-par.scw.cloud",
-        ],
-      },
-      poweredByHeader: false,
-      compress: false,
-    }))
-  ))
-);
+      [PHASE_DEVELOPMENT_SERVER],
+    ],
+  ],
+  {
+    async redirects() {
+      return [
+        {
+          source: "/contact",
+          destination: "/#contact",
+          permanent: true,
+        },
+        {
+          source: "/albums",
+          destination: "/albums/page/1",
+          permanent: true,
+        },
+        {
+          source: "/categories",
+          destination: "/categories/page/1",
+          permanent: true,
+        },
+        {
+          source: "/cosplayers",
+          destination: "/cosplayers/page/1",
+          permanent: true,
+        },
+      ];
+    },
+    // future: { webpack5: true },
+    pwa: {
+      dest: "public",
+      runtimeCaching,
+      disable: process.env.NODE_ENV !== "production",
+      mode: process.env.NODE_ENV,
+    },
+    reactStrictMode: true,
+    env: {
+      baseUrl: "baseUrl",
+    },
+    images: {
+      domains: [
+        "assets.jkanda.fr",
+        "s3.fr-par.scw.cloud",
+        "assets-jkanda.s3.fr-par.scw.cloud",
+      ],
+    },
+    poweredByHeader: false,
+    compress: false,
+  },
+]);
