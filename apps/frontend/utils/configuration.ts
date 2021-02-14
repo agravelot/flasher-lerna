5c823b14c9542d05a95edd14a1aaabcce29b312e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-02-14 18:54:30 +0100

    hotfix: disable env check

--- apps/frontend/utils/configuration.ts
+++ apps/frontend/utils/configuration.ts
@@ -1,4 +1,4 @@
-import joi from "joi";
+// import joi from "joi";
 
 interface KeycloakConfig {
   realm: string;
@@ -26,60 +26,60 @@ export interface ConfigurationInterface {
   administration: string;
 }
 
-const envVarsSchema = joi
-  .object()
-  .keys({
-    // NODE_ENV: joi
-    //   .string()
-    //   .valid("production", "development", "test")
-    //   .required(),
-    NEXT_PUBLIC_APP_URL: joi
-      .string()
-      .required()
-      .uri()
-      .description("Public app url"),
-    NEXT_PUBLIC_API_URL: joi
-      .string()
-      .required()
-      .uri()
-      .description("Data source api"),
-    NEXT_PUBLIC_KEYCLOAK_REALM: joi.string().required(),
-    NEXT_PUBLIC_KEYCLOAK_CLIENT_ID: joi.string().required(),
-    NEXT_PUBLIC_KEYCLOAK_URL: joi.string().uri().required(),
-    NEXT_PUBLIC_ALGOLIA_APP_ID: joi.string().allow(""), // require in prod
-    NEXT_PUBLIC_ALGOLIA_API_KEY: joi.string().allow(""), // require in prod
-    NEXT_PUBLIC_GOOGLE_ANALYTICS_UA: joi.string().allow(""), // require in prod
-    NEXT_PUBLIC_GOOGLE_ANALYTICS_DEBUG: joi.boolean(),
-    NEXT_PUBLIC_ADMINISTRATION_URL: joi.string().uri().required(),
-  })
-  .unknown();
+// const envVarsSchema = joi
+//   .object()
+//   .keys({
+//     // NODE_ENV: joi
+//     //   .string()
+//     //   .valid("production", "development", "test")
+//     //   .required(),
+//     NEXT_PUBLIC_APP_URL: joi
+//       .string()
+//       .required()
+//       .uri()
+//       .description("Public app url"),
+//     NEXT_PUBLIC_API_URL: joi
+//       .string()
+//       .required()
+//       .uri()
+//       .description("Data source api"),
+//     NEXT_PUBLIC_KEYCLOAK_REALM: joi.string().required(),
+//     NEXT_PUBLIC_KEYCLOAK_CLIENT_ID: joi.string().required(),
+//     NEXT_PUBLIC_KEYCLOAK_URL: joi.string().uri().required(),
+//     NEXT_PUBLIC_ALGOLIA_APP_ID: joi.string().allow(""), // require in prod
+//     NEXT_PUBLIC_ALGOLIA_API_KEY: joi.string().allow(""), // require in prod
+//     NEXT_PUBLIC_GOOGLE_ANALYTICS_UA: joi.string().allow(""), // require in prod
+//     NEXT_PUBLIC_GOOGLE_ANALYTICS_DEBUG: joi.boolean(),
+//     NEXT_PUBLIC_ADMINISTRATION_URL: joi.string().uri().required(),
+//   })
+//   .unknown();
 
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: "key" } })
-  .validate(process.env);
+// const { error } = envVarsSchema
+//   .prefs({ errors: { label: "key" } })
+//   .validate(process.env);
 
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
+// if (error) {
+//   throw new Error(`Config validation error: ${error.message}`);
+// }
 
 export const configuration: ConfigurationInterface = {
-  appUrl: envVars.NEXT_PUBLIC_APP_URL,
-  baseUrl: envVars.NEXT_PUBLIC_API_URL,
+  appUrl: process.env.NEXT_PUBLIC_APP_URL ?? "",
+  baseUrl: process.env.NEXT_PUBLIC_API_URL ?? "",
   keycloak: {
-    realm: envVars.NEXT_PUBLIC_KEYCLOAK_REALM,
-    clientId: envVars.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
-    url: envVars.NEXT_PUBLIC_KEYCLOAK_URL,
+    realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM ?? "",
+    clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID ?? "",
+    url: process.env.NEXT_PUBLIC_KEYCLOAK_URL ?? "",
   },
   algolia: {
-    appId: envVars.NEXT_PUBLIC_ALGOLIA_APP_ID,
-    apiKey: envVars.NEXT_PUBLIC_ALGOLIA_API_KEY,
+    appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? "",
+    apiKey: process.env.NEXT_PUBLIC_ALGOLIA_API_KEY ?? "",
   },
   googleAnalytics: {
-    ua: envVars.NEXT_PUBLIC_GOOGLE_ANALYTICS_UA,
-    debug: envVars.NEXT_PUBLIC_GOOGLE_ANALYTICS_DEBUG
-      ? envVars.NEXT_PUBLIC_GOOGLE_ANALYTICS_DEBUG === "true"
-      : envVars.NODE_ENV !== "production",
+    ua: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_UA ?? "",
+    debug: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_DEBUG
+      ? process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_DEBUG === "true"
+      : process.env.NODE_ENV !== "production",
   },
   twitter: "JujunneK",
-  administration: envVars.NEXT_PUBLIC_ADMINISTRATION_URL,
+  administration: process.env.NEXT_PUBLIC_ADMINISTRATION_URL ?? "",
 };
