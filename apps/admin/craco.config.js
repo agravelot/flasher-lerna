416568e1b49539b3716154f3964ecd8c7db69f5b
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-06-03 15:41:07 +0200

    build(admin): can import monorepo deps

--- apps/admin/craco.config.js
+++ apps/admin/craco.config.js
@@ -1,3 +1,14 @@
+// crago.config.js
+// see: https://github.com/sharegate/craco
+
+const path = require('path')
+const fs = require('fs')
+
+const cracoBabelLoader = require('craco-babel-loader')
+// Handle relative paths to sibling packages
+const appDirectory = fs.realpathSync(process.cwd())
+const resolvePackage = relativePath => path.resolve(appDirectory, relativePath)
+
 // craco.config.js
 module.exports = {
     style: {
@@ -8,4 +19,16 @@ module.exports = {
         ],
       },
     },
+    plugins: [
+      {
+        plugin: cracoBabelLoader,
+        options: {
+          includes: [
+            // No "unexpected token" error importing components from these lerna siblings:
+            resolvePackage('../common'),
+            resolvePackage('../models'),
+          ],
+        },
+      },
+    ],
   }
