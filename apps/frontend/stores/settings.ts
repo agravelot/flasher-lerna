06b81012401d6d846011ae5449f95f44725735e6
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-11-02 00:31:09 +0100

    chore(frontend): remove console.log

--- apps/frontend/stores/settings.ts
+++ apps/frontend/stores/settings.ts
@@ -27,27 +27,6 @@ export const getSettings = async (): Promise<Settings> => {
         return setting;
       };
 
-      console.log({
-        homepageTitle: getSetting(SettingKeys.HomepageTitle).value as string,
-        homepageSubtitle: getSetting(SettingKeys.HomepageSubtitle)
-          .value as string,
-        footerContent: getSetting(SettingKeys.FooterContent).value as string,
-        profilePictureHomepage: getSetting(SettingKeys.ProfilePictureHomepage)
-          .value as SettingMedia,
-        backgroundPictureHomepage: getSetting(
-          SettingKeys.BackgroundPictureHomepage
-        ).value as SettingMedia,
-        defaultPageTitle: getSetting(SettingKeys.DefaultPageTitle)
-          .value as string,
-        homepageHeaderSubtitle: getSetting(SettingKeys.HomepageHeaderSubtitle)
-          .value as string,
-        appName: getSetting(SettingKeys.AppName).value as string,
-        homepageDescription: getSetting(SettingKeys.HomepageDescription)
-          .value as string,
-        seoDescription: getSetting(SettingKeys.SeoDescription).value as string,
-      });
-      
-
       return {
         homepageTitle: getSetting(SettingKeys.HomepageTitle).value as string,
         homepageSubtitle: getSetting(SettingKeys.HomepageSubtitle)
