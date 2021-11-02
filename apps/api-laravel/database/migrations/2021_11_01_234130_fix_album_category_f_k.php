a672044a39b6f3e9ebfbb58713671490f6f72f07
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-11-02 01:04:46 +0100

    fix(api-laraval): unable delete album with categories

---
+++ apps/api-laravel/database/migrations/2021_11_01_234130_fix_album_category_f_k.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixAlbumCategoryFK extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('album_category', static function (Blueprint $table): void {
+            $table->dropForeign(['album_id']);
+            $table->dropForeign(['category_id']);
+        });
+
+        Schema::table('album_category', static function (Blueprint $table): void {
+            $table->foreign('album_id')->references('id')->on('albums')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        }); 
+    }
+
+    public function down(): void
+    {
+        Schema::table('album_category', static function (Blueprint $table): void {
+            $table->dropForeign(['album_id']);
+            $table->dropForeign(['category_id']);
+        });
+
+        Schema::table('album_category', static function (Blueprint $table): void {
+            $table->foreign('album_id')->references('id')->on('albums')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+        });
+    }
+}
