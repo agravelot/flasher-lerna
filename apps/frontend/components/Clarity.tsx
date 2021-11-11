114e29dcce49d476e59c7c53e6c2970aee58e4f5
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-11-11 02:45:11 +0100

    fix(frontend): clarity loading

--- apps/frontend/components/Clarity.tsx
+++ apps/frontend/components/Clarity.tsx
@@ -11,5 +11,11 @@ const config: Config = {
 };
 
 export const Clarity = (): JSX.Element => {
-  return config.enable ? (<Script src={`https://www.clarity.ms/tag/${config.id}`} strategy="afterInteractive" async />) : (<></>);
+  return config.enable ? (<Script id="clarity-script" dangerouslySetInnerHTML={{ __html: `
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${config.id}");
+  `}} strategy="afterInteractive"  />) : (<></>);
 };
