35e36d503c6e8ea5c993cc2d5ce71bff85449f8f
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2022-03-31 22:20:59 +0200

    chore(api-go): remove old gen orm script

--- apps/api-go/gen-orm.sh
+++
@@ -1,5 +0,0 @@
-
-
-# gen --sqltype=postgres --database=flasher --connstr "host=localhost user=flasher password=flasher dbname=flasher port=5432 sslmode=disable" --gorm --module=example.com/examplegen --table=albums 
-
-gentool -db postgres -dsn "host=localhost user=flasher password=flasher dbname=flasher port=5432 sslmode=disable" -outPath=gormQuery/ -fieldNullable -fieldWithTypeTag -fieldWithIndexTag
