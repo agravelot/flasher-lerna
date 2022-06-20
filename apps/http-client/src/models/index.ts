07a3e24688b9c050bd1a673fe95a29e14edf979c
Antoine GRAVELOT <a.gravelot@hotmail.fr>
2022-06-20 23:08:37 +0200

    build(http-client): add new generated http client lib

---
+++ apps/http-client/src/models/index.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AlbumServiceUpdateRequest';
+export * from './ProtobufAny';
+export * from './RpcStatus';
+export * from './V1AlbumResponse';
+export * from './V1ArticleResponse';
+export * from './V1Category';
+export * from './V1CreateRequest';
+export * from './V1CreateResponse';
+export * from './V1DeleteResponse';
+export * from './V1GetBySlugResponse';
+export * from './V1IndexResponse';
+export * from './V1UpdateRequest';
+export * from './V1UpdateResponse';
