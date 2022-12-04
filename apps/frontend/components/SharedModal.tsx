68a2afbc4609d4949242a76c68995eba3aa5cbc6
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2022-12-04 14:34:58 +0100

    feat(frontend): add image loader

--- apps/frontend/components/SharedModal.tsx
+++ apps/frontend/components/SharedModal.tsx
@@ -92,25 +92,58 @@ export default function SharedModal({
                                 exit="exit"
                                 className="absolute"
                             >
-                                <Image
-                                    src={currentImage.url}
-                                    width={currentImage.width}
-                                    height={currentImage.height}
-                                    sizes={
-                                        screenSize.height > screenSize.width || currentImage.width > currentImage.height
-                                            ? "100vw" // Image will be full width on mobile and landscape images.
-                                            : `${Math.ceil((screenSize.height / currentImage.width) * currentImage.height)}px` // Determine wanted size
-                                    }
-                                    draggable={false}
-                                    priority
-                                    alt="TODO"
-                                    onLoadingComplete={() => setLoaded(true)}
-                                    style={{
-                                        objectFit: "contain",
-                                        height: "calc(100vh - 100px)",
-                                        marginBottom: "100px"
-                                    }}
-                                />
+                               <div className="relative">
+                                  {!loaded && <svg className="absolute inset-1/2" width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg">
+                                      <defs>
+                                          <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+                                              <stop stopColor="#fff" stopOpacity="0" offset="0%"/>
+                                              <stop stopColor="#fff" stopOpacity=".631" offset="63.146%"/>
+                                              <stop stopColor="#fff" offset="100%"/>
+                                          </linearGradient>
+                                      </defs>
+                                      <g fill="none" fillRule="evenodd">
+                                          <g transform="translate(1 1)">
+                                              <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" strokeWidth="2">
+                                                  <animateTransform
+                                                      attributeName="transform"
+                                                      type="rotate"
+                                                      from="0 18 18"
+                                                      to="360 18 18"
+                                                      dur="0.9s"
+                                                      repeatCount="indefinite" />
+                                              </path>
+                                              <circle fill="#fff" cx="36" cy="18" r="1">
+                                                  <animateTransform
+                                                      attributeName="transform"
+                                                      type="rotate"
+                                                      from="0 18 18"
+                                                      to="360 18 18"
+                                                      dur="0.9s"
+                                                      repeatCount="indefinite" />
+                                              </circle>
+                                          </g>
+                                      </g>
+                                  </svg>}
+                                  <Image
+                                      src={currentImage.url}
+                                      width={currentImage.width}
+                                      height={currentImage.height}
+                                      sizes={
+                                          screenSize.height > screenSize.width || currentImage.width > currentImage.height
+                                              ? "100vw" // Image will be full width on mobile and landscape images.
+                                              : `${Math.ceil((screenSize.height / currentImage.width) * currentImage.height)}px` // Determine wanted size
+                                      }
+                                      draggable={false}
+                                      priority
+                                      alt="TODO"
+                                      onLoadingComplete={() => setLoaded(true)}
+                                      style={{
+                                          objectFit: "contain",
+                                          height: "calc(100vh - 100px)",
+                                          marginBottom: "100px"
+                                      }}
+                                  />
+                              </div>
                             </motion.div>
                         </AnimatePresence>
                     </div>
