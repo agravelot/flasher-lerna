a18580c014b4fd7dc0d6dc2b2fbcc525b226d957
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2022-12-27 20:55:10 +0100

    feat(api-go): import article from local md contents

--- apps/api-go/cmd/import_articles/import_articles.go
+++ apps/api-go/cmd/import_articles/import_articles.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	articlesgrpc "api-go/gen/go/proto/articles/v2"
+	"api-go/infrastructure/keycloak"
 	"bufio"
+	"context"
 	"fmt"
 	"github.com/kr/pretty"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+	"log"
 	"os"
 	"strings"
 	"time"
@@ -21,19 +28,14 @@ type Post struct {
 
 const blogContentPath = "../frontend/content/blog/"
 
-func processFile(entry os.DirEntry) Post {
+func getPostFromFile(entry os.DirEntry) Post {
 	slug := strings.ReplaceAll(entry.Name(), ".mdx", "")
 
 	file, err := os.Open(fmt.Sprintf("%s%s", blogContentPath, entry.Name()))
 	if err != nil {
 		panic(err)
 	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(file)
+	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	beginHeaderRead := false
@@ -100,6 +102,20 @@ func processFile(entry os.DirEntry) Post {
 }
 
 func main() {
+	conn, err := grpc.Dial("localhost:3100", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO extract in env
+	url := "https://accounts.jkanda.fr"
+	realm := "jkanda"
+	clientID := "service-account"
+	clientSecret := "2d5e4db4-9785-4919-9dbf-39964aebcc4f"
+
+	kc, err := keycloak.New(url, realm, clientID, clientSecret)
+	s := articlesgrpc.NewArticleServiceClient(conn)
+
 	files, err := os.ReadDir(blogContentPath)
 	if err != nil {
 		panic(err)
@@ -111,11 +127,38 @@ func main() {
 		}
 
 		if !strings.HasSuffix(entry.Name(), ".mdx") {
-			return
+			continue
 		}
 
-		post := processFile(entry)
+		post := getPostFromFile(entry)
 
-		pretty.Log(post)
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		// TODO Fix defer in loop
+		defer cancel()
+
+		pretty.Log(post.Slug)
+		data := articlesgrpc.CreateRequest{
+			Slug:    post.Slug,
+			Name:    post.Title,
+			Content: post.Content,
+			//PublishedAt:     post.CreatedAt,
+			MetaDescription: post.MetaDescription,
+		}
+
+		at, err := kc.GetAccessToken()
+		if err != nil {
+			log.Fatal(fmt.Errorf("unable to get access token: %w", err))
+		}
+
+		ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("authorization", fmt.Sprintf("Bearer %s", at)))
+
+		r, err := s.Create(ctx, &data)
+		if err != nil {
+			log.Fatal(fmt.Errorf("unable to create article: %w", err))
+		}
+
+		log.Println(r.Id)
+
 	}
 }
