f2d9e0510c8e221bcaa265e334a82bfa6fea224d
Hikari-rom <romainfpro@gmail.com>
2023-02-11 23:46:24 +0100

    fix: :butterfly: Remove export default from the SearchOpener component

--- apps/frontend/components/SearchOpener.tsx
+++ apps/frontend/components/SearchOpener.tsx
@@ -2,7 +2,7 @@ import { useSearch } from "contexts/AppContext";
 import { useRouter } from "next/router";
 import { FunctionComponent, useEffect } from "react";
 
-const SearchOpener: FunctionComponent = () => {
+export const SearchOpener: FunctionComponent = () => {
     const { open, setGoogleSearch } = useSearch();
     const {query, isReady} = useRouter();
     
@@ -21,5 +21,3 @@ const SearchOpener: FunctionComponent = () => {
     }, [open, query, isReady, setGoogleSearch]);
     return null;
 };
-
-export default SearchOpener;
