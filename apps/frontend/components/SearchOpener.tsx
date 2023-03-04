c9228c2bb89d34cab5c16b215cdd6a49cf23e226
Hikari-rom <romainfpro@gmail.com>
2023-03-04 21:34:27 +0100

    style: apply linter

--- apps/frontend/components/SearchOpener.tsx
+++ apps/frontend/components/SearchOpener.tsx
@@ -19,31 +19,29 @@ export const SearchOpener: FunctionComponent = () => {
         type: string;
     }
 
-    
+
     const { open, setGoogleSearch } = useSearch();
-    const {query, isReady} = useRouter();
-    const reducer = (state : State, action: Action) => {
+    const { query, isReady } = useRouter();
+    const reducer = (state: State, action: Action) => {
         switch (action.type) {
             case "showResults":
                 setGoogleSearch(action.search.toString());
                 open();
-                return {status: true, search: action.search.toString()};
-            default: 
+                return { status: true, search: action.search.toString() };
+            default:
                 return state;
         }
-        
+
     };
     const [state, dispatch] = useReducer(reducer, initialState);
     useEffect(() => {
-        if(!isReady)
-        {
+        if (!isReady) {
             return;
         }
         const search = query.search;
-        
-        if(search != undefined && !state.status)
-        {
-            dispatch({type: "showResults", search: search.toString()});
+
+        if (search != undefined && !state.status) {
+            dispatch({ type: "showResults", search: search.toString() });
         }
     }, [open, query, isReady, setGoogleSearch, state.status]);
     return null;
