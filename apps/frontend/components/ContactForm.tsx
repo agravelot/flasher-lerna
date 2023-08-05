1759e84307fc359773c0db0d00bb24302cb51244
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2023-08-05 22:36:39 +0200

    Change contact form input field to textarea
    The message input field in the contact form has been converted from a standard input field to a textarea for better user experience. This allows users to enter multiline messages more conveniently. Furthermore, the condition to display a certain SVG icon was simplified by removing redundant comparisons to false.

--- apps/frontend/components/ContactForm.tsx
+++ apps/frontend/components/ContactForm.tsx
@@ -132,14 +132,13 @@ const ContactForm: FunctionComponent = () => {
         >
           Message
         </label>
-        <input
+        <textarea
           {...register("message", { required: true })}
           name="message"
           type="text"
           id="contact_message"
           required={true}
-          // rows={4}
-          // cols={80}
+          rows={8}
           className="w-full rounded bg-white px-3 py-3 text-sm text-gray-700 placeholder-gray-400 shadow focus:outline-none focus:ring"
           placeholder="Ecrivez votre message..."
         />
@@ -198,20 +197,19 @@ const ContactForm: FunctionComponent = () => {
               />
             </svg>
           )}
-          {(isSubmitSuccessful === false && isSubmitting === false) ||
-            (isValid === false && (
+          {(!isSubmitSuccessful && !isSubmitting) || (!isValid && (
               <svg
-                className="-ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+                  className="-ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 />
               </svg>
             ))}
