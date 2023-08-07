a125f6758aa8e7ad4fbddc55cfba7a97f021b12f
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2023-08-08 00:05:53 +0200

    fix: type contact form

--- apps/frontend/components/ContactForm.tsx
+++ apps/frontend/components/ContactForm.tsx
@@ -135,7 +135,6 @@ const ContactForm: FunctionComponent = () => {
         <textarea
           {...register("message", { required: true })}
           name="message"
-          type="text"
           id="contact_message"
           required={true}
           rows={8}
@@ -212,7 +211,7 @@ const ContactForm: FunctionComponent = () => {
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 />
               </svg>
-            ))}
+          ))}
           <span>Envoyer</span>
         </button>
       </div>
