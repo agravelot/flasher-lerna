c1c91b63f5d82221812a6316864eb6303c987bb2
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2023-08-19 00:47:47 +0200

    feat(frontend): new homepage

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,28 +1,23 @@
-import Link from "next/link";
 import Layout from "../components/Layout";
 import Image from "next/image";
 import Header from "../components/Header";
 import { TestimonialList } from "../components/TestimonialList";
-import AlbumList from "../components/album/AlbumList";
 import { GetStaticProps, GetStaticPropsResult, NextPage } from "next";
 import { getGlobalProps, GlobalProps } from "../stores";
-import Separator from "../components/Separator";
 import { NextSeo, SiteLinksSearchBoxJsonLd } from "next-seo";
-import {Album, Category, Testimonial} from "@flasher/models";
+import { Category, Testimonial} from "@flasher/models";
 import { api, PaginatedReponse, sizes } from "@flasher/common";
 import { configuration } from "utils/configuration";
 import { ContactSection } from "../components/ContactSection";
 import { SearchOpener } from "components/SearchOpener";
-import CategoryItem from "../components/category/CategoryItem";
+import CategoryList from "../components/category/CategoryList";
 
 type Props = {
-  albums: Album[];
   testimonials: Testimonial[];
   categories: Category[];
 } & GlobalProps;
 
 const IndexPage: NextPage<Props> = ({
-  albums,
   testimonials,
   defaultPageTitle,
   appName,
@@ -69,38 +64,18 @@ const IndexPage: NextPage<Props> = ({
         ]}
       />
       <div>
-        <Header title={appName} separatorClass="text-gray-300" />
-        <section className="bg-gray-300 pb-20">
-          <div className="container mx-auto">
-            <div className="-mt-24 flex flex-wrap overflow-x-hidden md:-mx-3">
-              <Link
-                href="/galerie"
-                tabIndex={0}
-                className="order-last mx-auto mt-12 rounded-lg bg-gradient-to-r from-blue-700 to-red-700 py-3 px-10 font-semibold text-white hover:from-pink-500 hover:to-orange-500"
-              >
-                <h2>Découvrez mes derniers albums</h2>
-              </Link>
-              <AlbumList albums={albums} className="-mt-24" />
-            </div>
-          </div>
-        </section>
-        <section className="container mx-auto px-4 py-8">
-          <div className="py-8">
+        <Header title={appName} separatorClass="text-white" />
+         <section className="container mx-auto py-8">
+          <div className="prose max-w-none text-justify leading-relaxed">
             <p>Mes photos reflètent une harmonie subtile de diverses émotions, faisant de chacune d'elles une véritable expression de mon style unique. J'apprécie particulièrement jouer avec ces différentes thématiques pour laisser libre cours à mon imagination débordante. Laissez vous emporter par la magie de mes clichés qui racontent des histoires uniques et captivantes.</p>
           </div>
           <div className="flex flex-wrap px--4">
-            {categories.filter(c => c.cover).map(( category ,i) => (
-                <div key={i} className="px-4 w-1/2">
-                  <CategoryItem category={category} />
-                </div>
-            )
-            )}
+            <CategoryList categories={categories} />
           </div>
         </section>
        
-        <section className="relative py-20">
-          <Separator separatorClass="text-white" position="top" />
-          <div className="md:container md:mx-auto px-4">
+        <section>
+          <div className="container mx-auto">
             <div className="block items-center md:flex md:flex-wrap">
               {profilePictureHomepage && (
                 <div className="w-full md:w-1/2 md:order-2">
@@ -108,10 +83,10 @@ const IndexPage: NextPage<Props> = ({
                     className="mb-8 w-full object-cover lg:mb-0"
                     alt={appName}
                     src={profilePictureHomepage.url}
-                    width={2000}
-                    height={2000}
+                    width={profilePictureHomepage.width ?? 10}
+                    height={profilePictureHomepage.height ?? 10}
                     draggable={false}
-                    sizes={sizes(3, "container")}
+                    sizes={sizes(2, "container")}
                   />
                 </div>
               )}
@@ -137,7 +112,7 @@ const IndexPage: NextPage<Props> = ({
         <section id="testimonials" className="p-12 lg:pb-24">
           <div className="container mx-auto">
             <div className="flex flex-wrap justify-center text-center">
-              <div className="w-full px-4 lg:w-6/12">
+              <div className="w-full lg:w-6/12">
                 <h2 className="text-4xl font-semibold">
                   <span>
                     {"Ils sont "}
@@ -183,11 +158,6 @@ export default IndexPage;
 export const getStaticProps: GetStaticProps = async ({
   params,
 }): Promise<GetStaticPropsResult<Props>> => {
-  const albums = await api<PaginatedReponse<Album[]>>(
-    `/albums?page=${params?.page ?? 1}`
-  )
-    .then((res) => res.json())
-    .then((res) => res.data.slice(0, 3));
   const testimonials = await api<PaginatedReponse<Testimonial[]>>(
     `/testimonials?page=${params?.page ?? 1}`
   )
@@ -201,5 +171,5 @@ export const getStaticProps: GetStaticProps = async ({
 
   const global = await getGlobalProps();
 
-  return { props: { albums, testimonials, categories,...global }, revalidate: 60 };
+  return { props: { testimonials, categories,...global }, revalidate: 60 };
 };
