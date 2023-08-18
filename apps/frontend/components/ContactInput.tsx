7d937b0c529253a98327f14d5fd2e52a4d4dbe8d
Hikari-rom <romainfpro@gmail.com>
2023-08-18 19:35:21 +0200

    feat: add checkbox Input implementation in ContactInput.tsx v2

--- apps/frontend/components/ContactInput.tsx
+++ apps/frontend/components/ContactInput.tsx
@@ -19,15 +19,17 @@ interface Props {
 
 export const ContactInput: FC<Props> = ({ register, errors, idForm, idHtml, label, tag = "input", inputType = "text", required = true, placeholder = "", checkboxTitle = false, checkboxValue = "", checkboxLabel = "" }) => {
     return (
-        <div className="relative mb-3 w-full">
+        <div className={(tag != "checkbox") ? "relative mb-3 w-full" : "relative w-full"}>
+            {(tag != "checkbox" || checkboxTitle) &&
             <label
                 className="mb-2 block text-xs font-bold uppercase text-gray-700"
                 htmlFor={idHtml}
             >
                 {label}{(required) ? <div className="inline-block pl-0.5 text-red-700">*</div> : ""}
             </label>
+            }
             {
-                (balise === "textarea") ?
+                (tag === "textarea") &&
                     <textarea
                         {...register(`${idForm}`, { required: required })}
                         name={idForm}
@@ -38,11 +40,35 @@ export const ContactInput: FC<Props> = ({ register, errors, idForm, idHtml, labe
                         className="w-full rounded bg-white px-3 py-3 text-sm text-gray-700 placeholder-gray-400 shadow focus:outline-none focus:ring"
                         placeholder={placeholder}
                     />
-                    :
+            }
+            {
+                (tag === "checkbox") &&
+                <div className="relative flex">
                     <input
                         {...register(`${idForm}`, { required: required })}
                         name={idForm}
+                        type={tag}
+                        value={checkboxValue}
+                        id={idHtml}
+                        required={required}
+                        autoComplete={"true"}
+                        className="rounded bg-white mb-1 text-sm text-gray-700 placeholder-gray-400"
+                        style={{ transition: "all 0.15s ease 0s" }}
+                    />
+                    <label
+                        className="mb-2 block text-xs pl-2 font-bold text-gray-700"
+                        htmlFor={idHtml}
+                    >
+                        {checkboxLabel}
+                    </label>
+                </div>
             }
+            {
+                (tag === "input") &&
+                <input
+                    {...register(`${idForm}`, { required: required })}
+                    name={idForm}
+                    type={inputType}
                         id={idHtml}
                         required={required}
                         autoComplete={"true"}
