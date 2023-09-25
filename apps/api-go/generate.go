0568010b31f2a8d8623f61be12603a0213a887be
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2023-09-26 00:30:36 +0200

    chore(api-go): upgrade

--- apps/api-go/generate.go
+++ apps/api-go/generate.go
@@ -1,5 +1,5 @@
 //go:generate go run github.com/bufbuild/buf/cmd/buf@v1.25.0 generate
-//go:generate cp gen/openapiv2/all.swagger.json pkg/openapi/third_party/OpenAPI/swagger.json
+//go:generate cp gen/openapiv2/all.swagger.json infrastructure/openapi/third_party/OpenAPI/swagger.json
 //go:generate go run github.com/go-swagger/go-swagger/cmd/swagger@master generate markdown -f gen/openapiv2/all.swagger.json --output API.md
 //  // go:generate go run cmd/generate-orm/generate.go
 
