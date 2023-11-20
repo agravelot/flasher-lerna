d28512adca630feab1507d1cee8ffefb66ccfb9d
Hikari-rom <romainfpro@gmail.com>
2023-11-20 18:16:42 +0100

    test(api-laravel): Change from Notification to Mail in StoreContact Test

--- apps/api-laravel/tests/Feature/Http/Controller/Api/Contact/StoreContactTest.php
+++ apps/api-laravel/tests/Feature/Http/Controller/Api/Contact/StoreContactTest.php
@@ -11,7 +11,7 @@ use App\Models\Contact;
 use App\Mail\ContactSent;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Notifications\AnonymousNotifiable;
-use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
@@ -22,27 +22,27 @@ class StoreContactTest extends TestCase
     public function test_guest_can_store_a_contact_and_admins_are_notified(): void
     {
         $this->withoutExceptionHandling();
-        Notification::fake();
+        Mail::fake();
         $admin1 = UserRepresentation::factory(['groups' => ['admin']]);
         $admin2 = UserRepresentation::factory(['groups' => ['admin']]);
+        $admin3 = UserRepresentation::factory();
         $mockGroup = new GroupRepresentation();
         $mockGroup->id = '';
         Keycloak::shouldReceive('groups->first')->andReturn($mockGroup);
         Keycloak::shouldReceive('groups->members')->once()->andReturn([$admin1->toUser(), $admin2->toUser()]);
         $contact = factory(Contact::class)->make();
         $this->assertCount(0, Contact::all());
-        Notification::assertNotSentTo([$admin1->toUser(), $admin2->toUser()], ContactSent::class);
+        Mail::assertNotSent(ContactSent::class);
 
         $response = $this->storeContact($contact);
 
         $response->assertCreated();
         $this->assertCount(1, Contact::all());
 
-        Notification::assertSentTo(
-            new AnonymousNotifiable(),
+        Mail::assertQueued(
             ContactSent::class,
-            static function ($notification, $channels, $notifiable) use ($admin1, $admin2) {
-                return $notifiable->routes['mail'] === [$admin1->email, $admin2->email];
+            function (ContactSent $mail) use ($admin1, $admin2, $admin3) {
+                return $mail->hasTo($admin1) && $mail->hasTo($admin2) && !$mail->hasTo($admin3);
             }
         );
     }
