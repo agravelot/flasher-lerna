56ebaf460986017456f211a2a722ccca1cd9459a
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-02-25 15:29:24 +0100

    chore: upgrade deps with cotrect types

--- apps/frontend/types/next-auth.d.ts
+++ apps/frontend/types/next-auth.d.ts
@@ -1,11 +1,14 @@
-import { CustomSession, Role } from "../pages/api/auth/types";
-import { DefaultUser } from "next-auth/src/core/types";
+import { Role } from "../pages/api/auth/types";
+import { DefaultUser } from "next-auth/core/types";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  interface Session extends CustomSession {}
+  interface Session {
+    user?: DefaultSession["user"] & { role?: Role };
+  }
 
   interface User extends DefaultUser {
     role?: Role;
